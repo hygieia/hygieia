4992f528100e69dced8bf70687d8af22d359059a
Damick <rvl994@6c4008a5dd24.cof.ds.capitalone.com>
2015-08-03 17:13:24 -0400

    changed from maven to gradle, along with embedded mongo to enable testing added sample properties file for clarity updated README, for "hudson" as it is now looking for "Jenkins" properties
    TODO: normalize names of directories to match and avoid confusion

--- api/src/main/java/com/capitalone/dashboard/request/AuthenticationRequest.java
+++ api/src/main/java/com/capitalone/dashboard/request/AuthenticationRequest.java
@@ -5,37 +5,37 @@ import javax.validation.constraints.NotNull;
 import com.capitalone.dashboard.model.Authentication;
 
 public class AuthenticationRequest {
-	
-@NotNull
-private String username;
 
-@NotNull
-private String password;
+    @NotNull
+    private String username;
 
-public String getUsername() {
-	return username;
-}
+    @NotNull
+    private String password;
 
-public void setUsername(String username) {
-	this.username = username;
-}
+    public String getUsername() {
+        return username;
+    }
 
-public String getPassword() {
-	return password;
-}
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-public void setPassword(String password) {
-	this.password = password;
-}
+    public String getPassword() {
+        return password;
+    }
 
-public Authentication toAuthentication() {
-    return new Authentication(username, password);
-}
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-public Authentication copyTo(Authentication authentication) {
-    Authentication updated = toAuthentication();
-    updated.setId(authentication.getId());
-    return updated;
-}
+    public Authentication toAuthentication() {
+        return new Authentication(username, password);
+    }
+
+    public Authentication copyTo(Authentication authentication) {
+        Authentication updated = toAuthentication();
+        updated.setId(authentication.getId());
+        return updated;
+    }
 
 }
