4fa45d3ee3d6e670ecb80babc84cdd912a66804d
tabladrum <tapabrata.pal@capitalone.com>
2015-08-05 12:41:39 -0400

    Support for github.com. Still will not support access behind proxy.

--- GitHubSourceCodeCollector/src/main/java/com/capitalone/dashboard/collector/DefaultGitHubClient.java
+++ GitHubSourceCodeCollector/src/main/java/com/capitalone/dashboard/collector/DefaultGitHubClient.java
@@ -41,7 +41,8 @@ public class DefaultGitHubClient implements GitHubClient {
 
 	private final RestOperations restOperations;
 	private final String SEGMENT_API = "/api/v3/repos/";
-
+	private final String PUBLIC_GITHUB_REPO_HOST = "api.github.com/repos/";
+	private final String PUBLIC_GITHUB_HOST_NAME =  "github.com";
 
 	@Autowired
 	public DefaultGitHubClient(GitHubSettings settings,
@@ -54,28 +55,39 @@ public class DefaultGitHubClient implements GitHubClient {
 	public List<Commit> getCommits(GitHubRepo repo) {
 
 		List<Commit> commits = new ArrayList<>();
-		
-		String repoUrl = (String)repo.getOptions().get("url");
-		URL url=null;
-		String hostName="";
-		String protocol="";
+
+		String repoUrl = (String) repo.getOptions().get("url");
+
+		URL url = null;
+		String hostName = "";
+		String protocol = "";
 		try {
-			url=new URL(repoUrl);
-			hostName =url.getHost();
-			protocol= url.getProtocol();
+			url = new URL(repoUrl);
+			hostName = url.getHost();
+			protocol = url.getProtocol();
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
 			LOG.error(e.getMessage());
 		}
 		String hostUrl = protocol + "://" + hostName + "/";
-		String repoName= repoUrl.substring(hostUrl.length(), repoUrl.length());
-	
-		String apiUrl = protocol + "://" + hostName +SEGMENT_API+repoName;
-		
-		DateTime dt = repo.getLastUpdateTime().minusMinutes(10); //randomly chosen 10 minutes. Need to refactor
+		String repoName = repoUrl.substring(hostUrl.length(), repoUrl.length());
+		String apiUrl = "";
+		if (hostName.startsWith(PUBLIC_GITHUB_HOST_NAME)) {
+			apiUrl = protocol + "://" + PUBLIC_GITHUB_REPO_HOST + repoName;
+		} else {
+			 apiUrl = protocol + "://" + hostName + SEGMENT_API
+					+ repoName;
+		}
+
+		DateTime dt = repo.getLastUpdateTime().minusMinutes(10000); // randomly
+																	// chosen 10
+																	// minutes.
+																	// Need to
+																	// refactor
 		DateTimeFormatter fmt = ISODateTimeFormat.dateHourMinuteSecond();
 		String strDt = fmt.print(dt);
-		String queryUrl = apiUrl.concat("/commits?branch=" + repo.getBranch() + "&since=" + strDt);
+		String queryUrl = apiUrl.concat("/commits?branch=" + repo.getBranch()
+				+ "&since=" + strDt);
 		for (Object item : paresAsArray(makeRestCall(queryUrl))) {
 			JSONObject jsonObject = (JSONObject) item;
 			String sha = str(jsonObject, "sha");
@@ -83,22 +95,23 @@ public class DefaultGitHubClient implements GitHubClient {
 			JSONObject authorObject = (JSONObject) commitObject.get("author");
 			String message = str(commitObject, "message");
 			String author = str(authorObject, "name");
-			long timestamp = new DateTime(str(authorObject, "date")).getMillis();
-            Commit commit = new Commit();
-            commit.setTimestamp(System.currentTimeMillis());
-            commit.setScmUrl(repo.getRepoUrl());
-            commit.setScmRevisionNumber(sha);
-            commit.setScmAuthor(author);
-            commit.setScmCommitLog(message);
-            commit.setScmCommitTimestamp(timestamp);
-            commit.setNumberOfChanges(1);
-            commits.add(commit);
+			long timestamp = new DateTime(str(authorObject, "date"))
+					.getMillis();
+			Commit commit = new Commit();
+			commit.setTimestamp(System.currentTimeMillis());
+			commit.setScmUrl(repo.getRepoUrl());
+			commit.setScmRevisionNumber(sha);
+			commit.setScmAuthor(author);
+			commit.setScmCommitLog(message);
+			commit.setScmCommitTimestamp(timestamp);
+			commit.setNumberOfChanges(1);
+			commits.add(commit);
 		}
 		return commits;
 	}
 
 	private ResponseEntity<String> makeRestCall(String url) {
-		//Not using github auth now. Assuming all public repos. 
+		// Not using github auth now. Assuming all public repos.
 		return restOperations.exchange(url, HttpMethod.GET, null, String.class);
 	}
 
