184f5d82b3ebcf43f4c189bf17d169c6fd3b152c
tabladrum <tapabrata.pal@capitalone.com>
2015-08-08 18:50:24 -0400

    Added encrypt and decrypt methods to take String key. Added related tests.

--- common/src/main/java/com/capitalone/dashboard/util/Encryption.java
+++ common/src/main/java/com/capitalone/dashboard/util/Encryption.java
@@ -8,6 +8,8 @@ import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.SecretKeySpec;
 
 import org.apache.commons.codec.binary.Base64;
 
@@ -34,6 +36,42 @@ public class Encryption {
 	public static String decryptString(String encryptedMessage, SecretKey key) throws EncryptionException{
 		String decryptedMessage = "";
 		try {
+			Cipher decipher = Cipher.getInstance(ALGO);
+			decipher.init(Cipher.DECRYPT_MODE, key);
+			byte[] messageToDecrypt = Base64.decodeBase64(encryptedMessage);
+			byte[] decryptedBytes = decipher.doFinal(messageToDecrypt);
+			decryptedMessage = new String(decryptedBytes);
+
+		} catch (NoSuchAlgorithmException | NoSuchPaddingException
+				| InvalidKeyException | IllegalBlockSizeException
+				| BadPaddingException | NullPointerException | IllegalArgumentException e) {
+			throw new EncryptionException("Cannot decrypt this message" + '\n' + e.getMessage());
+		}
+		return decryptedMessage;
+	}
+	
+	public static String encryptString(String message, String aKey) throws EncryptionException {
+		String encryptedMessage = "";
+		try {
+			byte[] encodedKey = Base64.decodeBase64(aKey);
+		    SecretKey key = new SecretKeySpec(encodedKey, 0, encodedKey.length, ALGO);
+			Cipher cipher = Cipher.getInstance(ALGO);
+			cipher.init(Cipher.ENCRYPT_MODE, key);
+			byte[] encryptedBytes = cipher.doFinal(message.getBytes());
+			encryptedMessage = Base64.encodeBase64String(encryptedBytes);
+		} catch (IllegalBlockSizeException | BadPaddingException
+				| InvalidKeyException | NoSuchAlgorithmException
+				| NoSuchPaddingException | NullPointerException e) {
+			throw new EncryptionException("Cannot encrypt this message" + '\n' + e.getMessage());
+		}
+		return encryptedMessage;
+	}
+
+	public static String decryptString(String encryptedMessage, String aKey) throws EncryptionException{
+		String decryptedMessage = "";
+		try {
+			byte[] encodedKey     = Base64.decodeBase64(aKey);
+		    SecretKey key = new SecretKeySpec(encodedKey, 0, encodedKey.length, ALGO);
 			Cipher decipher = Cipher.getInstance(ALGO);
 			decipher.init(Cipher.DECRYPT_MODE, key);
 			byte[] messageToDecrypt = Base64.decodeBase64(encryptedMessage);
