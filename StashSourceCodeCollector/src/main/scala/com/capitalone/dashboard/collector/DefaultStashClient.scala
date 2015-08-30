fa8874f063d6486680c3645387b72f4715c1937b
srid99 <srid99@gmail.com>
2015-08-30 22:39:36 +0200

    Revert "Initial commit for Stash Collector"
    This reverts commit 80ee850ef815697cb420fc131be5a84ecf979cbe.

--- StashSourceCodeCollector/src/main/scala/com/capitalone/dashboard/collector/DefaultStashClient.scala
+++
@@ -1,9 +0,0 @@
-package com.capitalone.dashboard.collector
-
-import com.capitalone.dashboard.model.{Commit, StashRepo}
-import org.springframework.stereotype.Component
-
-@Component
-class DefaultStashClient extends StashClient {
-  override def getCommits(repo: StashRepo): List[Commit] = ???
-}
--- StashSourceCodeCollector/src/main/scala/com/capitalone/dashboard/collector/StashClient.scala
+++
@@ -1,7 +0,0 @@
-package com.capitalone.dashboard.collector
-
-import com.capitalone.dashboard.model.{Commit, StashRepo}
-
-trait StashClient {
-  def getCommits(repo: StashRepo): List[Commit]
-}
