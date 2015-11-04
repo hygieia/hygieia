f82099c3b79cd7190a526326e4cdb3a8a4e397ea
jdamick <jeffreydamick@gmail.com>
2015-11-04 11:13:52 -0500

    fixed version to be greater than the largest released component (versionone was 1.3.0) added more logging, added version change script & cleaned up poms

--- core/src/main/java/com/capitalone/dashboard/collector/CollectorTask.java
+++ core/src/main/java/com/capitalone/dashboard/collector/CollectorTask.java
@@ -2,6 +2,8 @@ package com.capitalone.dashboard.collector;
 
 import com.capitalone.dashboard.model.Collector;
 import com.capitalone.dashboard.repository.BaseCollectorRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.support.CronTrigger;
@@ -13,18 +15,19 @@ import javax.annotation.PreDestroy;
 /**
  * Base class for Collector task implementation which provides subclasses with
  * the following:
- *
+ * <p>
  * <ol>
- *     <li>Creates a Collector instance the first time the collector runs.</li>
- *     <li>Uses TaskScheduler to schedule the job based on the provided cron when the process starts.</li>
- *     <li>Saves the last execution time on the collector when the collection run finishes.</li>
- *     <li>Sets the collector online/offline when the collector process starts/stops</li>
+ * <li>Creates a Collector instance the first time the collector runs.</li>
+ * <li>Uses TaskScheduler to schedule the job based on the provided cron when the process starts.</li>
+ * <li>Saves the last execution time on the collector when the collection run finishes.</li>
+ * <li>Sets the collector online/offline when the collector process starts/stops</li>
  * </ol>
  *
  * @param <T> Class that extends Collector
  */
 @Component
 public abstract class CollectorTask<T extends Collector> implements Runnable {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CollectorTask.class);
 
     private final TaskScheduler taskScheduler;
     private final String collectorName;
@@ -37,21 +40,21 @@ public abstract class CollectorTask<T extends Collector> implements Runnable {
 
     @Override
     public final void run() {
+        LOGGER.info("Running Collector: {}", collectorName);
         T collector = getCollectorRepository().findByName(collectorName);
         if (collector == null) {
             // Register new collector
             collector = getCollectorRepository().save(getCollector());
-        } 
-        else {
-        	// In case the collector options changed via collectors properties setup.
-        	// We want to keep the existing collectors ID same as it ties to collector items.
-        	T newCollector = getCollector();
-        	newCollector.setId(collector.getId());
-        	newCollector.setEnabled(collector.isEnabled());
-        	newCollector.setCollectorType(collector.getCollectorType());
-        	newCollector.setLastExecuted(collector.getLastExecuted());
-        	newCollector.setName(collector.getName());
-        	collector = getCollectorRepository().save(newCollector); 	
+        } else {
+            // In case the collector options changed via collectors properties setup.
+            // We want to keep the existing collectors ID same as it ties to collector items.
+            T newCollector = getCollector();
+            newCollector.setId(collector.getId());
+            newCollector.setEnabled(collector.isEnabled());
+            newCollector.setCollectorType(collector.getCollectorType());
+            newCollector.setLastExecuted(collector.getLastExecuted());
+            newCollector.setName(collector.getName());
+            collector = getCollectorRepository().save(newCollector);
         }
 
         if (collector.isEnabled()) {
@@ -76,8 +79,11 @@ public abstract class CollectorTask<T extends Collector> implements Runnable {
     }
 
     public abstract T getCollector();
+
     public abstract BaseCollectorRepository<T> getCollectorRepository();
+
     public abstract String getCron();
+
     public abstract void collect(T collector);
 
     private void setOnline(boolean online) {
