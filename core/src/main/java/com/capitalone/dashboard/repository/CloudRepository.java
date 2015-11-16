1caad432766c201ca12a9ee033b7347cbf19ebdb
tabladrum <tapabrata.pal@capitalone.com>
2015-11-16 14:48:15 -0500

    Removed unused imports.

--- core/src/main/java/com/capitalone/dashboard/repository/CloudRepository.java
+++ core/src/main/java/com/capitalone/dashboard/repository/CloudRepository.java
@@ -1,11 +1,9 @@
 package com.capitalone.dashboard.repository;
 
+import com.capitalone.dashboard.model.Cloud;
 import org.bson.types.ObjectId;
 import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.data.repository.CrudRepository;
-
-import com.capitalone.dashboard.model.Cloud;
-import com.capitalone.dashboard.model.CloudComputeData;
 
 public interface CloudRepository extends
 		CrudRepository<Cloud, ObjectId>,
