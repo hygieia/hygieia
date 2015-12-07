f86647f3012ad4b7ace1912219896e8e285732bf
amitmawkin <amit.mawkin@capitalone.com>
2015-12-07 16:46:04 -0500

    Added ChatOps Collector

---
+++ ChatOpsCollector/README.md
@@ -0,0 +1,4 @@
+GitHubSourceCodeCollector
+=========================
+
+Collect source code details from GitHub based on URL and branch
---
+++ UI/src/app/dashboard/core/data-factories/chatops-data.js
@@ -0,0 +1,36 @@
+/**
+ * Gets code repo related data
+ */
+(function () {
+    'use strict';
+
+    angular
+        .module('devops-dashboard.core')
+        .factory('chatOpsData', chatOpsData);
+
+    function chatOpsData($http) {
+        var testDetailRoute = 'test-data/chatops-hipchat.json';
+        var testImgDetailRoute = 'test-data/chatops-hipchat-img.json';
+
+
+
+        return {
+            details: details
+        };
+
+        function details(serviceUrl) {
+            console.log("ServiceURl:"+serviceUrl);
+            serviceUrl="http://"+serviceUrl;
+            return $http.get(serviceUrl).then(function (response) {
+
+                    return response.data;
+                },function(response){
+                console.log("Error occured:"+JSON.stringify(response));
+            });
+        }
+
+
+
+    }
+
+})();
