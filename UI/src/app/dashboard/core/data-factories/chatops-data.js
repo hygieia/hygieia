952801a9ce5c1db905bdede3141ff5bb7f515e4d
amitmawkin <amit.mawkin@capitalone.com>
2015-12-11 00:05:16 -0500

    removed un-necessary debug statements

--- UI/src/app/dashboard/core/data-factories/chatops-data.js
+++ UI/src/app/dashboard/core/data-factories/chatops-data.js
@@ -10,25 +10,18 @@
 
     function chatOpsData($http) {
         var testDetailRoute = 'test-data/chatops-hipchat.json';
-        var testImgDetailRoute = 'test-data/chatops-hipchat-img.json';
-
-
-
         return {
             details: details
         };
 
         function details(serviceUrl) {
-            console.log("ServiceURl:"+serviceUrl);
             return $http.get(serviceUrl).then(function (response) {
 
-                    return response.data;
-                },function(response){
-                console.log("Error occured:"+JSON.stringify(response));
+                return response.data;
+            }, function (response) {
                 return response.data;
             });
         }
-
 
 
     }
