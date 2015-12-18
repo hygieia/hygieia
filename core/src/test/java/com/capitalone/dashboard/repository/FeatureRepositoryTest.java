e87b1fcb17740414b4aea354be2aea08d9488eda
amp5208 <arnoldpelka@gmail.com>
2015-12-18 16:12:07 -0500

    Created base testing class for proving feature repository core functionality

---
+++ core/src/test/java/com/capitalone/dashboard/repository/FeatureRepositoryTest.java
@@ -0,0 +1,227 @@
+package com.capitalone.dashboard.repository;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+import com.capitalone.dashboard.config.MongoConfig;
+import com.capitalone.dashboard.model.Feature;
+
+import org.bson.types.ObjectId;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+
+@ContextConfiguration(classes = { MongoConfig.class })
+@RunWith(SpringJUnit4ClassRunner.class)
+@DirtiesContext
+public class FeatureRepositoryTest {
+	static Feature mockV1Feature;
+	static Feature mockJiraFeature;
+	static final String generalUseDate = "2015-11-01T00:00:00Z";
+	static DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+	static Calendar cal = Calendar.getInstance();
+	static final String maxDateWinner = df.format(new Date());
+	static String maxDateLoser = new String();
+	static String currentSprintEndDate = new String();
+	static final ObjectId jiraCollectorId = new ObjectId();
+	static final ObjectId v1CollectorId = new ObjectId();
+
+	@ClassRule
+	public static final EmbeddedMongoDBRule RULE = new EmbeddedMongoDBRule();
+
+	@Autowired
+	private FeatureRepository featureRepo;
+
+	@Before
+	public void setUp() {
+		// Date-time modifications
+		cal.setTime(new Date());
+		cal.add(Calendar.DAY_OF_YEAR,-1);
+		maxDateLoser = df.format(cal.getTime());
+		cal.add(Calendar.DAY_OF_YEAR,+13);
+		currentSprintEndDate = df.format(cal.getTime());
+		
+		// Helper mock data
+		List<String> sOwnerNames = new ArrayList<String>();
+		sOwnerNames.add("Goku");
+		sOwnerNames.add("Gohan");
+		sOwnerNames.add("Picolo");
+		List<String> sOwnerDates = new ArrayList<String>();
+		sOwnerNames.add(generalUseDate);
+		sOwnerNames.add(generalUseDate);
+		sOwnerNames.add(generalUseDate);
+		List<String> sOwnerStates = new ArrayList<String>();
+		sOwnerNames.add("Active");
+		sOwnerNames.add("Active");
+		sOwnerNames.add("Deleted");
+		List<String> sOwnerIds = new ArrayList<String>();
+		sOwnerNames.add("9001");
+		sOwnerNames.add("8999");
+		sOwnerNames.add("7999");
+		List<String> sOwnerBools = new ArrayList<String>();
+		sOwnerNames.add("True");
+		sOwnerNames.add("False");
+		sOwnerNames.add("True");
+
+		// VersionOne Mock Feature
+		mockV1Feature = new Feature();
+		mockV1Feature.setCollectorId(jiraCollectorId);
+		mockV1Feature.setIsDeleted("True");
+		mockV1Feature.setChangeDate(generalUseDate);
+		mockV1Feature.setsEpicAssetState("Active");
+		mockV1Feature.setsEpicBeginDate(generalUseDate);
+		mockV1Feature.setsEpicChangeDate(generalUseDate);
+		mockV1Feature.setsEpicEndDate(generalUseDate);
+		mockV1Feature.setsEpicHPSMReleaseID("CO312615921");
+		mockV1Feature.setsEpicID("E-12345");
+		mockV1Feature.setsEpicIsDeleted("False");
+		mockV1Feature.setsEpicName("Test Epic 1");
+		mockV1Feature.setsEpicNumber("12938715");
+		mockV1Feature.setsEpicPDD(generalUseDate);
+		mockV1Feature.setsEpicType("Portfolio Feature");
+		mockV1Feature.setsEstimate("5");
+		mockV1Feature.setsId("B-12345");
+		mockV1Feature.setsName("Test Story 1");
+		mockV1Feature.setsNumber("12345416");
+		mockV1Feature.setsOwnersChangeDate(sOwnerDates);
+		mockV1Feature.setsOwnersFullName(sOwnerNames);
+		mockV1Feature.setsOwnersID(sOwnerIds);
+		mockV1Feature.setsOwnersIsDeleted(sOwnerBools);
+		mockV1Feature.setsOwnersShortName(sOwnerNames);
+		mockV1Feature.setsOwnersState(sOwnerStates);
+		mockV1Feature.setsOwnersUsername(sOwnerNames);
+		mockV1Feature.setsProjectBeginDate(generalUseDate);
+		mockV1Feature.setsProjectChangeDate(generalUseDate);
+		mockV1Feature.setsProjectEndDate(generalUseDate);
+		mockV1Feature.setsProjectID("Scope:231870");
+		mockV1Feature.setsProjectIsDeleted("False");
+		mockV1Feature.setsProjectName("Test Scope 1");
+		mockV1Feature.setsProjectPath("Top -> Middle -> Bottome -> "
+				+ mockV1Feature.getsProjectName());
+		mockV1Feature.setsProjectState("Active");
+		mockV1Feature.setsSoftwareTesting("True");
+		mockV1Feature.setsSprintAssetState("Inactive");
+		mockV1Feature.setsSprintBeginDate(generalUseDate);
+		mockV1Feature.setsSprintChangeDate(generalUseDate);
+		mockV1Feature.setsSprintEndDate(maxDateWinner);
+		mockV1Feature.setsSprintID("Timebox:12781205");
+		mockV1Feature.setsSprintIsDeleted("False");
+		mockV1Feature.setsSprintName("Test Sprint 1");
+		mockV1Feature.setsState("Inactive");
+		mockV1Feature.setsStatus("Accepted");
+		mockV1Feature.setsTeamAssetState("Active");
+		mockV1Feature.setsTeamChangeDate(generalUseDate);
+		mockV1Feature.setsTeamID("Team:124127");
+		mockV1Feature.setsTeamIsDeleted("False");
+		mockV1Feature.setsTeamName("Protectors of Earth");
+
+		// Jira Mock Feature
+		mockJiraFeature = new Feature();
+		mockJiraFeature.setCollectorId(jiraCollectorId);
+		mockJiraFeature.setIsDeleted("False");
+		mockJiraFeature.setChangeDate(maxDateWinner);
+		mockJiraFeature.setsEpicAssetState("Active");
+		mockJiraFeature.setsEpicBeginDate("");
+		mockJiraFeature.setsEpicChangeDate(maxDateWinner);
+		mockJiraFeature.setsEpicEndDate("");
+		mockJiraFeature.setsEpicHPSMReleaseID("");
+		mockJiraFeature.setsEpicID("32112345");
+		mockJiraFeature.setsEpicIsDeleted("");
+		mockJiraFeature.setsEpicName("Test Epic 1");
+		mockJiraFeature.setsEpicNumber("12938715");
+		mockJiraFeature.setsEpicPDD("");
+		mockJiraFeature.setsEpicType("");
+		mockJiraFeature.setsEstimate("40");
+		mockJiraFeature.setsId("0812345");
+		mockJiraFeature.setsName("Test Story 2");
+		mockJiraFeature.setsNumber("12345416");
+		mockJiraFeature.setsOwnersChangeDate(sOwnerDates);
+		mockJiraFeature.setsOwnersFullName(sOwnerNames);
+		mockJiraFeature.setsOwnersID(sOwnerIds);
+		mockJiraFeature.setsOwnersIsDeleted(sOwnerBools);
+		mockJiraFeature.setsOwnersShortName(sOwnerNames);
+		mockJiraFeature.setsOwnersState(sOwnerStates);
+		mockJiraFeature.setsOwnersUsername(sOwnerNames);
+		mockJiraFeature.setsProjectBeginDate(maxDateWinner);
+		mockJiraFeature.setsProjectChangeDate(maxDateWinner);
+		mockJiraFeature.setsProjectEndDate(maxDateWinner);
+		mockJiraFeature.setsProjectID("583482");
+		mockJiraFeature.setsProjectIsDeleted("False");
+		mockJiraFeature.setsProjectName("Saiya-jin Warriors");
+		mockJiraFeature.setsProjectPath("");
+		mockJiraFeature.setsProjectState("Active");
+		mockJiraFeature.setsSoftwareTesting("");
+		mockJiraFeature.setsSprintAssetState("Active");
+		mockJiraFeature.setsSprintBeginDate(maxDateLoser);
+		mockJiraFeature.setsSprintChangeDate(maxDateWinner);
+		mockJiraFeature.setsSprintEndDate(currentSprintEndDate);
+		mockJiraFeature.setsSprintID("1232512");
+		mockJiraFeature.setsSprintIsDeleted("False");
+		mockJiraFeature.setsSprintName("Test Sprint 2");
+		mockJiraFeature.setsState("Active");
+		mockJiraFeature.setsStatus("In Progress");
+		mockJiraFeature.setsTeamAssetState("Active");
+		mockJiraFeature.setsTeamChangeDate(maxDateWinner);
+		mockJiraFeature.setsTeamID("08374321");
+		mockJiraFeature.setsTeamIsDeleted("False");
+		mockJiraFeature.setsTeamName("Saiya-jin Warriors");
+	}
+
+	@After
+	public void tearDown() {
+		mockV1Feature = null;
+		mockJiraFeature = null;
+		featureRepo.deleteAll();
+	}
+
+	@Test
+	public void validateConnectivity_HappyPath() {
+		featureRepo.save(mockV1Feature);
+		featureRepo.save(mockJiraFeature);
+		
+		assertTrue("Happy-path MongoDB connectivity validation for the FeatureRepository has failed",featureRepo.findAll().iterator().hasNext());
+	}
+	
+	@Test
+	public void testGetFeatureIdById_HappyPath() {
+		featureRepo.save(mockV1Feature);
+		featureRepo.save(mockJiraFeature);
+		String testStoryId = "0812345";
+		
+		assertEquals("Expected feature ID did not match actual feature ID",testStoryId,featureRepo.getFeatureIdById(testStoryId).get(0).getId().toString());
+	}
+	
+	@Test
+	public void testGetFeatureMaxChangeDate_HappyPath() {
+		featureRepo.save(mockV1Feature);
+		featureRepo.save(mockJiraFeature);
+		
+		assertEquals("Expected feature max change date did not match actual feature max change date",maxDateWinner,featureRepo.getFeatureMaxChangeDate(jiraCollectorId,maxDateLoser).get(0).toString());
+	}
+	
+	@Test
+	public void testGetCurrentSprintDetail_HappyPath() {
+		featureRepo.save(mockV1Feature);
+		featureRepo.save(mockJiraFeature);
+		String testTeamId = "08374321";
+		String testSprintName = "Test Sprint 2";
+		assertEquals("Expected current sprint detail did not match actual current sprint detail",testSprintName,featureRepo.getCurrentSprintDetail(testTeamId,maxDateWinner).get(0).getsSprintName());
+	}
+	
+	
+}
