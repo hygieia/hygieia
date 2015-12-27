df2e2c95ce8d8b37ffc9d8e23f394e4a72ee064d
srid99 <srid99@gmail.com>
2015-12-27 14:22:23 +0100

    Add moco stub server  as dependency

---
+++ stash-scm-collector/src/test/java/com/capitalone/dashboard/collector/DefaultStashClientTest.java
@@ -0,0 +1,12 @@
+package com.capitalone.dashboard.collector;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class DefaultStashClientTest {
+
+    @Test
+    public void getCommits() {
+
+    }
+}
