a7c01d75270400a39aa555bcde9924ade0053107
Jim Zucker <jim@stratengllc.com>
2016-01-06 11:51:14 -0800

    Updated to address feedback to make all options configurable, added git-settings.env to make it consistent with the jira setup.

--- github-scm-collector/docker/git-properties-builder.sh
+++ github-scm-collector/docker/git-properties-builder.sh
@@ -2,34 +2,36 @@
 
 # mongo container provides the HOST/PORT
 # api container provided DB Name, ID & PWD
-PROP_FILE=hygieia-github-scm-collector.properties
+
+if [ "$TEST_SCRIPT" != "" ]
+then
+        #for testing locally
+        PROP_FILE=application.properties
+else 
+	PROP_FILE=hygieia-github-scm-collector.properties
+fi
+  
+if [ ! -d logs ]
+then
+        mkdir logs
+fi
 LOG=logs/$PROP_FILE.log
 
-
-# if we are linked, use that info
 if [ "$MONGO_PORT" != "" ]; then
   # Sample: MONGO_PORT=tcp://172.17.0.20:27017
-  export SPRING_DATA_MONGODB_HOST=`echo $MONGO_PORT|sed 's;.*://\([^:]*\):\(.*\);\1;'`
-  export SPRING_DATA_MONGODB_PORT=`echo $MONGO_PORT|sed 's;.*://\([^:]*\):\(.*\);\2;'`
-else
-	env >>$LOG
-	echo "ERROR: MONGO_PORT not defined" > logs/$0.log
-	exit 1
+  MONGODB_HOST=`echo $MONGO_PORT|sed 's;.*://\([^:]*\):\(.*\);\1;'`
+  MONGODB_PORT=`echo $MONGO_PORT|sed 's;.*://\([^:]*\):\(.*\);\2;'`
 fi
-
-echo "SPRING_DATA_MONGODB_HOST: $SPRING_DATA_MONGODB_HOST"
-echo "SPRING_DATA_MONGODB_PORT: $SPRING_DATA_MONGODB_PORT"
-
 
 cat > $PROP_FILE <<EOF
 #Database Name
 database=${HYGIEIA_API_ENV_SPRING_DATA_MONGODB_DATABASE:-dashboard}
 
 #Database HostName - default is localhost
-dbhost=${SPRING_DATA_MONGODB_HOST:-10.0.1.1}
+dbhost=${MONGODB_HOST:-10.0.1.1}
 
 #Database Port - default is 27017
-dbport=${SPRING_DATA_MONGODB_PORT:-9999}
+dbport=${MONGODB_PORT:-9999}
 
 #Database Username - default is blank
 dbusername=${HYGIEIA_API_ENV_SPRING_DATA_MONGODB_USERNAME:-db}
@@ -38,22 +40,19 @@ dbusername=${HYGIEIA_API_ENV_SPRING_DATA_MONGODB_USERNAME:-db}
 dbpassword=${HYGIEIA_API_ENV_SPRING_DATA_MONGODB_PASSWORD:-dbpass}
 
 #Collector schedule (required)
-github.cron=0 0/5 * * * *
+github.cron=${GITHUB_CRON:-"0 0/5 * * * *"}
 
-github.host=github.com
+github.host=${GITHUB_HOST:-github.com}
 
 #Maximum number of days to go back in time when fetching commits
-github.commitThresholdDays=15
+github.commitThresholdDays=${GITHUB_COMMIT_THREASHOLD_DAYS:-15}
 
 EOF
-
-cp $PROP_FILE application.properties
 
 echo "
 
 ===========================================
 Properties file created:  $PROP_FILE
-`cat $PROP_FILE`
 ===========================================
 
  " >>$LOG
--- jira-feature-collector/docker/jira-properties-builder.sh
+++ jira-feature-collector/docker/jira-properties-builder.sh
@@ -2,6 +2,7 @@
 
 # mongo container provides the HOST/PORT
 # api container provided DB Name, ID & PWD
+
 if [ "$TEST_SCRIPT" != "" ]
 then
 	#for testing locally
@@ -16,19 +17,12 @@ then
 fi
 LOG=logs/$PROP_FILE.log
 
-# if we are linked, use that info
+
 if [ "$MONGO_PORT" != "" ]; then
   # Sample: MONGO_PORT=tcp://172.17.0.20:27017
-  export SPRING_DATA_MONGODB_HOST=`echo $MONGO_PORT|sed 's;.*://\([^:]*\):\(.*\);\1;'`
-  export SPRING_DATA_MONGODB_PORT=`echo $MONGO_PORT|sed 's;.*://\([^:]*\):\(.*\);\2;'`
-else
-	env >>$LOG
-	echo "ERROR: MONGO_PORT not defined" > $LOG
-	exit 1
+  MONGODB_HOST=`echo $MONGO_PORT|sed 's;.*://\([^:]*\):\(.*\);\1;'`
+  MONGODB_PORT=`echo $MONGO_PORT|sed 's;.*://\([^:]*\):\(.*\);\2;'`
 fi
-
-echo "SPRING_DATA_MONGODB_HOST: $SPRING_DATA_MONGODB_HOST"
-echo "SPRING_DATA_MONGODB_PORT: $SPRING_DATA_MONGODB_PORT"
 
 
 cat > $PROP_FILE <<EOF
@@ -36,10 +30,10 @@ cat > $PROP_FILE <<EOF
 database=${HYGIEIA_API_ENV_SPRING_DATA_MONGODB_DATABASE:-dashboard}
 
 #Database HostName - default is localhost
-dbhost=${SPRING_DATA_MONGODB_HOST:-10.0.1.1}
+dbhost=${MONGODB_HOST:-10.0.1.1}
 
 #Database Port - default is 27017
-dbport=${SPRING_DATA_MONGODB_PORT:-9999}
+dbport=${MONGODB_PORT:-9999}
 
 #Database Username - default is blank
 dbusername=${HYGIEIA_API_ENV_SPRING_DATA_MONGODB_USERNAME:-db}
@@ -48,10 +42,10 @@ dbusername=${HYGIEIA_API_ENV_SPRING_DATA_MONGODB_USERNAME:-db}
 dbpassword=${HYGIEIA_API_ENV_SPRING_DATA_MONGODB_PASSWORD:-dbpass}
 
 #Collector schedule (required)
-feature.cron=0 * * * * *
+feature.cron=${JIRA_CRON:-"0 * * * * *"}
 
 #Page size for data calls (Jira maxes at 1000)
-feature.pageSize=1000
+feature.pageSize=${JIRA_PAGE_SIZE:-1000}
 
 #In-built folder housing prepared REST queries (required)
 feature.queryFolder=jiraapi-queries
@@ -66,35 +60,39 @@ feature.teamQuery=teaminfo
 feature.trendingQuery=trendinginfo
 
 #Jira Connection Details
-feature.jiraProxyUrl=${SPRING_DATA_JIRA_PROXY_URL}
-feature.jiraProxyPort=${SPRING_DATA_JIRA_PROXY_PORT}
+feature.jiraProxyUrl=${JIRA_PROXY_URL}
+feature.jiraProxyPort=${JIRA_PROXY_PORT}
 
 # Trending Query:  Number of days in a sprint (not-required)
-feature.sprintDays=${SPRING_DATA_SPRINT_DAYS:-60}
+feature.sprintDays=${JIRA_SPRINT_DAYS:-60}
 
 # Trending Query:  Length of sprint week (not-required)
-feature.sprintEndPrior=${SPRING_DATA_SPRINT_END_PRIOR:-7}
+feature.sprintEndPrior=${JIRA_SPRINT_END_PRIOR:-7}
 
 #Scheduled Job prior minutes to recover data created during execution time (usually, 2 minutes is enough)
-feature.scheduledPriorMin=2
+feature.scheduledPriorMin=${JIRA_SCHEDULED_PRIOR_MIN:-2}
 
 #Delta change date that modulates the collector item task - should be about as far back as possible, in ISO format (required)
-feature.deltaCollectorItemStartDate=2008-01-01T00:00:00.000000
+feature.deltaCollectorItemStartDate=${JIRA_DELTA_COLLECTOR_ITEM_START_DATE:-2008-01-01T00:00:00.000000}
 
 #Jira Connection Details
-feature.jiraBaseUrl=${SPRING_DATA_JIRA_BASE_URL:-https://jira.atlassian.com}
-feature.jiraQueryEndpoint=rest/api/2/
-#64-bit encoded credentials with the pattern username:password
-feature.jiraCredentials=${SPRING_DATA_JIRA_CREDENTIALS:-YWxsIHlvdXIgYmFzZSBhcmUgYmVsb25nIHRvIHVzOiB5b3UgYXJlIG9uIHRoZSB3YXkgdG8gZGVzdHJ1Y3Rpb246IG1ha2UgeW91ciB0aW1l}
+feature.jiraBaseUrl=${JIRA_BASE_URL:-https://jira.atlassian.com}
+feature.jiraQueryEndpoint=${JIRA_QUERY_ENDPOINT:-rest/api/2/}
+
+#64-bit encoded credentials with the pattern username:password 
+#on a mac you con create them with : echo "username:password" | base64
+#reference:  https://www.base64decode.org/ 
+feature.jiraCredentials=${JIRA_CREDENTIALS}
+
 #OAuth2.0 token credentials (currently not supported in this version)
-feature.jiraOauthAuthtoken=sdfghjkl==
-feature.jiraOauthRefreshtoken=sdfagheh==
-feature.jiraOauthRedirecturi=uri.this.is.test:uri
-feature.jiraOauthExpiretime=234567890987
+feature.jiraOauthAuthtoken=${JIRA_OAUTH_AUTH_TOKEN:-sdfghjkl==}
+feature.jiraOauthRefreshtoken=${JIRA_OAUTH_REFRESH_TOKEN:-sdfagheh==}
+feature.jiraOauthRedirecturi=${JIRA_OAUTH_REDIRECT_URL:-uri.this.is.test:uri}
+feature.jiraOauthExpiretime=${JIRA_OAUTH_EXPIRE_TIME:-234567890987}
 
 #Start dates from which to begin collector data, if no other data is present - usually, a month back is appropriate (required)
-feature.deltaStartDate=2015-03-01T00:00:00.000000
-feature.masterStartDate=2008-01-01T00:00:00.000000
+feature.deltaStartDate=${JIRA_DELTA_START_DATE:-2015-03-01T00:00:00.000000}
+feature.masterStartDate=${JIRA_MASTER_START_DATE:-2008-01-01T00:00:00.000000}
 
 EOF
 
@@ -102,7 +100,6 @@ echo "
 
 ===========================================
 Properties file created:  $PROP_FILE
-`cat $PROP_FILE`
 ===========================================
 
  " >>$LOG
