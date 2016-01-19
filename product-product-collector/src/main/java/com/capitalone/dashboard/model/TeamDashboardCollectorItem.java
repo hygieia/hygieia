b810ef21f0ff093eb82aeb61a8d52d892d17a746
John Curd <johnkangcurd@gmail.com>
2016-01-19 13:39:09 -0500

    implemented hashcode()

--- product-product-collector/src/main/java/com/capitalone/dashboard/model/TeamDashboardCollectorItem.java
+++ product-product-collector/src/main/java/com/capitalone/dashboard/model/TeamDashboardCollectorItem.java
@@ -2,17 +2,31 @@ package com.capitalone.dashboard.model;
 
 public class TeamDashboardCollectorItem extends CollectorItem {
     private static final String DASHBOARD_ID = "dashboardId";
+    private static final String DATE_ENABLED = "dateEnabled";
 
     public String getDashboardId() {
         return (String) getOptions().get(DASHBOARD_ID);
     }
 
     public void setDashboardId(String dashboardId) {
-        this.getOptions().put(DASHBOARD_ID, dashboardId);
+        getOptions().put(DASHBOARD_ID, dashboardId);
+    }
+
+    public Long getDateEnabled(){
+        return (Long) getOptions().get(DATE_ENABLED);
+    }
+
+    public void setDateEnabled(Long dateEnabledTimestamp){
+        getOptions().put(DATE_ENABLED, dateEnabledTimestamp);
     }
 
     @Override
     public boolean equals(Object obj) {
         return ((TeamDashboardCollectorItem)obj).getDashboardId().equals(getDashboardId());
+    }
+
+    @Override
+    public int hashCode() {
+        return getDashboardId().hashCode() + getDateEnabled().hashCode();
     }
 }
