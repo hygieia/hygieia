ddc370362d1c2e95d63487dd86e5d95649c7aaab
Jim Zucker <jim@stratengllc.com>
2016-02-08 16:22:11 -0800

    #239 - Created containers to provide examples for sonar & cucumber

--- test-servers/jenkins/jenkins_entrypoint.sh
+++ test-servers/jenkins/jenkins_entrypoint.sh
@@ -2,8 +2,9 @@
 
 #we have to do this in an entrypoint becuase the jenkins image has defined $JENKINS_HOME as a volume
 #this was the recommend approach on the docker site
-sudo chown -R jenkins:jenkins /var/jenkins_home/jobs
+chown -R jenkins:jenkins /var/jenkins_home/
 
 #call the original entry point
-/bin/tini -- /usr/local/bin/jenkins.sh
+#we use gosu as this is the recommended best practice
+exec gosu jenkins /usr/local/bin/jenkins.sh
 
