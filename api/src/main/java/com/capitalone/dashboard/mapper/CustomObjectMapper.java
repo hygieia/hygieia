34caae38df772c930b22a86457048727a4f4f925
Jim Zucker <jim@stratengllc.com>
2016-02-21 17:26:44 -0800

    reference #347 - added UID - Eclipse Warnings Cleanup - serializable does not declare a static final serialVersionUID

--- api/src/main/java/com/capitalone/dashboard/mapper/CustomObjectMapper.java
+++ api/src/main/java/com/capitalone/dashboard/mapper/CustomObjectMapper.java
@@ -8,8 +8,9 @@ import org.bson.types.ObjectId;
  * Custom object mapper that knows how to serialize Mongo ObjectIds.
  */
 public class CustomObjectMapper extends ObjectMapper {
+	private static final long serialVersionUID = 2035695746790240402L;
 
-    public CustomObjectMapper() {
+	public CustomObjectMapper() {
         SimpleModule module = new SimpleModule("ObjectIdModule");
         module.addSerializer(ObjectId.class, new ObjectIdSerializer());
         this.registerModule(module);
