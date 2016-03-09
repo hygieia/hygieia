841226adeae9f8d5a9ff772a62df8b9e656fd961
tabladrum <tapabrata.pal@capitalone.com>
2016-03-09 16:37:32 -0500

    More changes after review.

--- core/src/main/java/com/capitalone/dashboard/model/RequestLog.java
+++ core/src/main/java/com/capitalone/dashboard/model/RequestLog.java
@@ -9,7 +9,11 @@ public class RequestLog extends BaseModel {
     private String endpoint;
     private String method;
     private String parameter;
+    private long requestSize;
+    private String requestContentType;
     private Object requestBody;
+    private long responseSize;
+    private String responseContentType;
     private Object responseBody;
     private int responseCode;
     private long timestamp;
@@ -76,5 +80,37 @@ public class RequestLog extends BaseModel {
 
     public void setResponseCode(int responseCode) {
         this.responseCode = responseCode;
+    }
+
+    public long getRequestSize() {
+        return requestSize;
+    }
+
+    public void setRequestSize(long requestSize) {
+        this.requestSize = requestSize;
+    }
+
+    public long getResponseSize() {
+        return responseSize;
+    }
+
+    public void setResponseSize(long responseSize) {
+        this.responseSize = responseSize;
+    }
+
+    public String getRequestContentType() {
+        return requestContentType;
+    }
+
+    public void setRequestContentType(String requestContentType) {
+        this.requestContentType = requestContentType;
+    }
+
+    public String getResponseContentType() {
+        return responseContentType;
+    }
+
+    public void setResponseContentType(String responseContentType) {
+        this.responseContentType = responseContentType;
     }
 }
