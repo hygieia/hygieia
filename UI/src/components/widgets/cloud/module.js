64da88b69a1626d2a824f92412d13f988ec10107
Noah <hyw912@capitalone.com>
2016-04-12 22:01:03 -0400

    Some broken tests, but dont want to lose what I have done so far

---
+++ UI/src/components/widgets/cloud/module.js
@@ -0,0 +1,44 @@
+/**
+ * Created by hyw912 on 4/12/16.
+ */
+
+(function () {
+    'use strict';
+
+    var widget_state,
+        config = {
+            view: {
+                defaults: {
+                    title: 'Cloud' // widget title
+                },
+                controller: 'BuildWidgetViewController',
+                controllerAs: 'buildView',
+                templateUrl: 'components/widgets/build/view.html'
+            },
+            config: {
+                controller: 'BuildWidgetConfigController',
+                controllerAs: 'buildConfig',
+                templateUrl: 'components/widgets/build/config.html'
+            },
+            getState: getState,
+            collectors: ['build']
+        };
+
+    angular
+        .module(HygieiaConfig.module)
+        .config(register);
+
+    register.$inject = ['widgetManagerProvider', 'WidgetState'];
+    function register(widgetManagerProvider, WidgetState) {
+        widget_state = WidgetState;
+        widgetManagerProvider.register('cloud', config);
+    }
+
+    function getState(config) {
+        // make sure config values are set
+        return HygieiaConfig.local || (config.id && config.options.buildDurationThreshold && config.options.consecutiveFailureThreshold) ?
+            widget_state.READY :
+            widget_state.CONFIGURE;
+    }
+})();
+
---
+++ UI/src/components/widgets/cloud/view.js
@@ -0,0 +1,25 @@
+/**
+ * Created by hyw912 on 4/12/16.
+ */
+
+
+/**
+ * View controller for the build widget
+ */
+(function () {
+    'use strict';
+
+    angular
+        .module(HygieiaConfig.module)
+        .controller('CloudWidgetViewController', CloudWidgetViewController);
+
+    CloudWidgetViewController.$inject = ['$scope', '$modal', 'cloudData'];
+
+    function CloudWidgetViewController($scope, $modal, cloudData) {
+
+        var ctrl = this;
+        ctrl.load = function() {
+            return cloudData.cloudData;
+        }
+    }
+})();
