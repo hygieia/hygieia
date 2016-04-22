ef3024d963a63df7fe8ac05d6669015668284403
Noah <hyw912@capitalone.com>
2016-04-22 14:37:11 -0400

    More view work

---
+++ UI/src/components/widgets/cloud/directives/directives.js
@@ -0,0 +1,18 @@
+(function () {
+    'use strict';
+
+    var app = angular
+        .module(HygieiaConfig.module)
+        .directive('metricCategory',metricCategory);
+
+    function metricCategory() {
+        return {
+            restrict: 'E',
+            scope: { data: '=data'},
+            templateUrl: 'components/widgets/cloud/directives/metricCategory.html'
+        };
+    }
+
+})();
+
+
