78b90af9d0b83c2bb58c2ec3a64ea710d6438209
Noah <hyw912@capitalone.com>
2016-04-29 14:50:40 -0400

    sort logic tests

--- UI/test/components/feature/widgets/cloud/view.test.js
+++ UI/test/components/feature/widgets/cloud/view.test.js
@@ -6,8 +6,6 @@
 describe('CloudWidgetViewController', function () {
 
 
-
-
     var controller;
     var scope;
     var cloudData;
@@ -30,21 +28,25 @@ describe('CloudWidgetViewController', function () {
         var currentDate = new Date();
         currentDate.setDate(currentDate.getDate() + dayOffset);
         var dd = currentDate.getDate();
-        var mm = currentDate.getMonth()+1;
+        var mm = currentDate.getMonth() + 1;
         var yyyy = currentDate.getFullYear();
 
-        if(dd<10) { dd='0'+dd }
-        if(mm<10) { mm='0'+mm }
+        if (dd < 10) {
+            dd = '0' + dd
+        }
+        if (mm < 10) {
+            mm = '0' + mm
+        }
 
-        return mm+'/'+dd+'/'+yyyy;
+        return mm + '/' + dd + '/' + yyyy;
     }
 
     // load the controller's module
     beforeEach(module(HygieiaConfig.module));
     beforeEach(module(HygieiaConfig.module + '.core'));
 
-    beforeEach(module(function($provide) {
-        $provide.factory('cloudData', function() {
+    beforeEach(module(function ($provide) {
+        $provide.factory('cloudData', function () {
 
             return {
                 getAWSGlobalData: getAWSGlobalData,
@@ -58,12 +60,13 @@ describe('CloudWidgetViewController', function () {
             function getAWSInstancesByTag() {
                 return null;
             }
-        })}));
+        })
+    }));
 
 
     // inject the required services and instantiate the controller
     beforeEach(
-        function() {
+        function () {
             inject(function ($rootScope, cloudData, $controller) {
                 scope = $rootScope.$new();
 
@@ -77,80 +80,156 @@ describe('CloudWidgetViewController', function () {
                     $scope: scope,
                     cloudData: cloudData
                 });
-            })});
+            })
+        });
 
-
-    /*describe('checkImageAgeStatus()', function() {
-        describe('When I call checkImageAgeStatus', function () {
-            describe('And the expiration date is earlier than today', function () {
-                it('Then I expect "fail" to be returned', function() {
+    describe('changeSortDirection()', function () {
+        describe('When I call changeSortDirection', function () {
+            describe('And no sort key has been created', function () {
+                it('Then I expect to have one sort key added to sortType with a "-" at the front', function () {
 
                     //Arrange
-                    var expirationDate = retrieveTestDate(-10);
-                    var expected = "fail";
+                    var key = 'AMI';
+                    var expected = ['-AMI'];
 
                     //Act
-                    var actual = controller.checkImageAgeStatus(expirationDate);
+                    controller.changeSortDirection(key);
 
                     //Assert
-                    expect(actual).toBe(expected);
+                    expect(angular.equals(controller.sortType, expected)).toBeTruthy();
                 });
             });
 
-            describe('And the expiration date is today', function () {
-                it('Then I expect "warn" to be returned', function() {
+            describe('And one non-matching sort key exists', function () {
+                it('Then I expect to have a new sort key added to the front of sortType with a "-" at the front', function () {
 
                     //Arrange
-                    var expirationDate = retrieveTestDate(0);
-                    var expected = "warn";
+                    var firstKey = 'instanceID';
+                    var secondKey = 'AMI';
+                    var expected = ['-AMI','-instanceID'];
 
                     //Act
-                    var actual = controller.checkImageAgeStatus(expirationDate);
+                    controller.changeSortDirection(firstKey);
+                    controller.changeSortDirection(secondKey);
 
                     //Assert
-                    expect(actual).toBe(expected);
+                    expect(angular.equals(controller.sortType, expected)).toBeTruthy();
                 });
             });
 
-            describe('And the expiration date is 15 days from now', function () {
-                it('Then I expect "warn" to be returned', function() {
+            describe('And one non-matching sort key exists first and a matching sort key exists second with a "-" in front', function () {
+                it('Then I expect to have the matching sort key moved to the first spot with a '+' in front', function () {
 
                     //Arrange
-                    var expirationDate = retrieveTestDate(15);
-                    var expected = "warn";
+                    var firstKey = 'instanceID';
+                    var secondKey = 'AMI';
+                    var thirdKey = 'instanceID';
+                    var expected = ['+instanceID','-AMI'];
 
                     //Act
-                    var actual = controller.checkImageAgeStatus(expirationDate);
+                    controller.changeSortDirection(firstKey);
+                    controller.changeSortDirection(secondKey);
+                    controller.changeSortDirection(thirdKey);
 
                     //Assert
-                    expect(actual).toBe(expected);
+                    expect(angular.equals(controller.sortType, expected)).toBeTruthy();
                 });
             });
 
-            describe('And the expiration date is 16 days from now', function () {
-                it('Then I expect "pass" to be returned', function() {
+
+            describe('And one matching sort key exists with a "-" in the front', function () {
+                it('Then I expect to the sort key updated to have "+" at the front', function () {
 
                     //Arrange
-                    var expirationDate = retrieveTestDate(16);
-                    var expected = "pass";
+                    var key = 'AMI';
+                    var expected = ['+AMI'];
 
                     //Act
-                    var actual = controller.checkImageAgeStatus(expirationDate);
+                    controller.changeSortDirection(key);
+                    controller.changeSortDirection(key);
 
                     //Assert
-                    expect(actual).toBe(expected);
+                    expect(angular.equals(controller.sortType, expected)).toBeTruthy();
                 });
             });
         });
-    });*/
+    });
 
-    describe('checkNOTTDisabledStatus()', function() {
+
+    /*describe('checkImageAgeStatus()', function() {
+     describe('When I call checkImageAgeStatus', function () {
+     describe('And the expiration date is earlier than today', function () {
+     it('Then I expect "fail" to be returned', function() {
+
+     //Arrange
+     var expirationDate = retrieveTestDate(-10);
+     var expected = "fail";
+
+     //Act
+     var actual = controller.checkImageAgeStatus(expirationDate);
+
+     //Assert
+     expect(actual).toBe(expected);
+     });
+     });
+
+     describe('And the expiration date is today', function () {
+     it('Then I expect "warn" to be returned', function() {
+
+     //Arrange
+     var expirationDate = retrieveTestDate(0);
+     var expected = "warn";
+
+     //Act
+     var actual = controller.checkImageAgeStatus(expirationDate);
+
+     //Assert
+     expect(actual).toBe(expected);
+     });
+     });
+
+     describe('And the expiration date is 15 days from now', function () {
+     it('Then I expect "warn" to be returned', function() {
+
+     //Arrange
+     var expirationDate = retrieveTestDate(15);
+     var expected = "warn";
+
+     //Act
+     var actual = controller.checkImageAgeStatus(expirationDate);
+
+     //Assert
+     expect(actual).toBe(expected);
+     });
+     });
+
+     describe('And the expiration date is 16 days from now', function () {
+     it('Then I expect "pass" to be returned', function() {
+
+     //Arrange
+     var expirationDate = retrieveTestDate(16);
+     var expected = "pass";
+
+     //Act
+     var actual = controller.checkImageAgeStatus(expirationDate);
+
+     //Assert
+     expect(actual).toBe(expected);
+     });
+     });
+     });
+     });*/
+
+    describe('checkNOTTDisabledStatus()', function () {
         describe('When I call checkNOTTDisabledStatus', function () {
             describe('And the NOTT value is set to "exclude"', function () {
-                it('Then I expect "true" to be returned', function() {
+                it('Then I expect "true" to be returned', function () {
 
                     //Arrange
-                    var tags = [{"name":"Owner","value":"joe.doe@email.com"},{"name":"visigoths:nott","value":"exclude"}];
+                    var tags = [{"name": "Owner", "value": "joe.doe@email.com"}, {
+                        "name": "visigoths:nott",
+                        "value": "exclude"
+                    }];
                     var expected = true;
 
                     //Act
@@ -162,10 +241,10 @@ describe('CloudWidgetViewController', function () {
             });
 
             describe('And the NOTT value is not set', function () {
-                it('Then I expect "false" to be returned', function() {
+                it('Then I expect "false" to be returned', function () {
 
                     //Arrange
-                    var tags = [{"name":"Owner","value":"joe.doe@email.com"}];
+                    var tags = [{"name": "Owner", "value": "joe.doe@email.com"}];
                     var expected = false;
 
                     //Act
@@ -180,10 +259,10 @@ describe('CloudWidgetViewController', function () {
         });
     });
 
-    describe('checkMonitoredStatus()', function() {
+    describe('checkMonitoredStatus()', function () {
         describe('When I call checkMonitoredStatus', function () {
             describe('And the status is "true"', function () {
-                it('Then I expect "pass" to be returned', function() {
+                it('Then I expect "pass" to be returned', function () {
 
                     //Arrange
                     var status = true;
@@ -198,7 +277,7 @@ describe('CloudWidgetViewController', function () {
             });
 
             describe('And the status is "false"', function () {
-                it('Then I expect "fail" to be returned', function() {
+                it('Then I expect "fail" to be returned', function () {
 
                     //Arrange
                     var status = false;
@@ -216,10 +295,10 @@ describe('CloudWidgetViewController', function () {
         });
     });
 
-    describe('checkUtilizationStatus()', function() {
+    describe('checkUtilizationStatus()', function () {
         describe('When I call checkUtilizationStatus', function () {
             describe('And the status is greater than 30', function () {
-                it('Then I expect "pass" to be returned', function() {
+                it('Then I expect "pass" to be returned', function () {
 
                     //Arrange
                     var status = 31;
@@ -234,7 +313,7 @@ describe('CloudWidgetViewController', function () {
             });
 
             describe('And the status is less than 30', function () {
-                it('Then I expect "fail" to be returned', function() {
+                it('Then I expect "fail" to be returned', function () {
 
                     //Arrange
                     var status = 29;
@@ -249,7 +328,7 @@ describe('CloudWidgetViewController', function () {
             });
 
             describe('And the status is 30', function () {
-                it('Then I expect "fail" to be returned', function() {
+                it('Then I expect "fail" to be returned', function () {
 
                     //Arrange
                     var status = 30;
@@ -266,22 +345,22 @@ describe('CloudWidgetViewController', function () {
         });
     });
 
-    describe('load()', function() {
+    describe('load()', function () {
         describe('When I call load', function () {
-            it('Then I expect AMI data to be retrieved into awsOverview', function() {
+            it('Then I expect AMI data to be retrieved into awsOverview', function () {
 
                 //Act-Arrange
 
                 //Assert
-                var result = angular.equals( controller.awsOverview,AWSGlobalData );
+                var result = angular.equals(controller.awsOverview, AWSGlobalData);
                 expect(result).toBeTruthy();
             });
         });
     });
 
-    describe('toggleView()', function() {
+    describe('toggleView()', function () {
         describe('When I call toggleView and isDetail is false', function () {
-            it('Then I expect isDetail to change to true', function() {
+            it('Then I expect isDetail to change to true', function () {
 
                 //Arrange
                 controller.isDetail = false;
@@ -296,7 +375,7 @@ describe('CloudWidgetViewController', function () {
         });
 
         describe('When I call toggleView and isDetail is true', function () {
-            it('Then I expect isDetail to change to false', function() {
+            it('Then I expect isDetail to change to false', function () {
 
                 //Arrange
                 controller.isDetail = true;
