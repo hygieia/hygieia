4baf1d2ef5e91542a901b27b3cf106e84b66ac48
Noah <hyw912@capitalone.com>
2016-04-29 12:40:09 -0400

    Cleaned up commit data

---
+++ UI/src/components/widgets/cloud/directives/metricCategory.js
@@ -0,0 +1,21 @@
+(function () {
+    'use strict';
+
+    var app = angular
+        .module(HygieiaConfig.module)
+        .directive('metricCategory',metricCategory);
+
+    function metricCategory() {
+        return {
+            restrict: 'E',
+            scope: {
+                data: '=data',
+                category: '@category'
+            },
+            templateUrl: 'components/widgets/cloud/directives/metricCategory.html'
+        };
+    }
+
+})();
+
+
