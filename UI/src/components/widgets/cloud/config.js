d043fa81fc3f27b2c1f177f427c2d3a3b06b44de
Noah <hyw912@capitalone.com>
2016-05-04 16:19:59 -0400

    Added save logic

--- UI/src/components/widgets/cloud/config.js
+++ UI/src/components/widgets/cloud/config.js
@@ -12,33 +12,67 @@
         .module(HygieiaConfig.module)
         .controller('CloudWidgetConfigController', CloudWidgetConfigController);
 
-    CloudWidgetConfigController.$inject = ['modalData', '$scope', 'cloudData', '$modalInstance'];
-    function CloudWidgetConfigController(modalData, $scope, cloudData, $modalInstance) {
+    CloudWidgetConfigController.$inject = ['modalData', '$scope', 'collectorData', '$modalInstance'];
+    function CloudWidgetConfigController(modalData, $scope, collectorData, $modalInstance) {
 
+
+        //private properties/methods
         var ctrl = this;
         var widgetConfig = modalData.widgetConfig;
 
-        // public variables
+
+        function createCloudConfigItem(accountNumber,tagName,tagValue) {
+            var item = {
+                collectorId: _.findWhere(ctrl.collectors, {collectorType: 'Cloud'}).id,
+                options: {
+                    accountNumber: accountNumber,
+                    tagName: tagName,
+                    tagValue: tagValue
+                }
+            };
+
+            return collectorData.createCollectorItem(item);
+        }
+
+        function passDataToView() {
+            var postObj = {};
+
+            var form = document.cloudConfigForm;
+            var postObj = {
+                name: 'cloud',
+                options: {
+                    id: widgetConfig.options.id,
+                    accountNumber: ctrl.accountNumber,
+                    tagName: ctrl.tagName,
+                    tagValue: ctrl.tagValue
+                },
+                componentId: modalData.dashboard.application.components[0].id
+            };
+
+            // pass this new config to the modal closing so it's saved
+            $modalInstance.close(postObj);
+        }
+
+        function processCollectorsResponse(data) {
+            ctrl.collectors = data;
+        }
+
+
+        // Request collecters
+        collectorData.collectorsByType('Cloud').then(processCollectorsResponse);
+
+
+        // public properties/methods
         ctrl.accountNumber = undefined;
+        ctrl.tagName = undefined;
+        ctrl.tagValue = undefined;
+        ctrl.collectors = [];
 
         // public methods
         ctrl.submit = function (valid) {
             if (valid) {
-
-                var postObj = {};
-
-                var form = document.cloudConfigForm;
-                var postObj = {
-                    name: 'cloud',
-                    options: {
-                        id: widgetConfig.options.id,
-                        accountNumber: ctrl.accountNumber
-                    },
-                    componentId: modalData.dashboard.application.components[0].id
-                };
-
-                // pass this new config to the modal closing so it's saved
-                $modalInstance.close(postObj);
+                createCloudConfigItem(ctrl.accountNumber, ctrl.tagName, ctrl.tagValue).
+                then(passDataToView());
             }
         }
 
