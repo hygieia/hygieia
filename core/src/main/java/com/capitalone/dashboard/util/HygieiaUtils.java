89855aa6c109508efcc686180342e017b8286a2a
tabladrum <tapabrata.pal@capitalone.com>
2016-05-06 00:36:09 -0400

    Added instance history collection, api, model etc. Fixed a bug with subnet upsert.

--- core/src/main/java/com/capitalone/dashboard/util/HygieiaUtils.java
+++ core/src/main/java/com/capitalone/dashboard/util/HygieiaUtils.java
@@ -4,7 +4,6 @@ package com.capitalone.dashboard.util;
 import org.apache.commons.beanutils.BeanUtilsBean;
 
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 
 public class HygieiaUtils {
 
@@ -19,33 +18,5 @@ public class HygieiaUtils {
                 }
             }
         }.copyProperties(dest, source);
-    }
-
-
-    public void merge(Object obj, Object update){
-        if(!obj.getClass().isAssignableFrom(update.getClass())){
-            return;
-        }
-
-        Method[] methods = obj.getClass().getMethods();
-
-        for(Method fromMethod: methods){
-            if(fromMethod.getDeclaringClass().equals(obj.getClass())
-                    && fromMethod.getName().startsWith("get")){
-
-                String fromName = fromMethod.getName();
-                String toName = fromName.replace("get", "set");
-
-                try {
-                    Method toMetod = obj.getClass().getMethod(toName, fromMethod.getReturnType());
-                    Object value = fromMethod.invoke(update, (Object[])null);
-                    if(value != null){
-                        toMetod.invoke(obj, value);
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }
     }
 }
