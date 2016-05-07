b6ecc4a11654b2d643a6a61dad7ea56587b98377
tabladrum <tapabrata.pal@capitalone.com>
2016-05-07 01:30:28 -0400

    Simplied cloud/instance/details and cloud/subnet/details api.

--- core/src/main/java/com/capitalone/dashboard/repository/CloudVirtualNetworkRepository.java
+++ core/src/main/java/com/capitalone/dashboard/repository/CloudVirtualNetworkRepository.java
@@ -28,7 +28,8 @@ public interface CloudVirtualNetworkRepository extends
     @Query(value = "{ 'tags.name' : ?0, 'tags.value' : ?1 }")
     Collection<CloudVirtualNetwork> findByTagNameAndValue(String name, String value);
 
-    Collection<CloudVirtualNetwork> findBySubnetIdIn(List<String> virtualNetworkId);
+    Collection<CloudVirtualNetwork> findByvirtualNetworkIdIn(List<String> virtualNetworkId);
+
     @Query(value = "{accountNumber : ?0}")
     Collection<CloudVirtualNetwork> findByAccountNumber(String accountNumber);
 
