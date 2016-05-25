3b92cdeb64641f877995b3fc0141483db6e2b717
amp5208 <arnoldpelka@gmail.com>
2016-05-25 18:16:50 -0400

    updated VersionOne collector to use new kanban api logic; collector working and passing tests, but having issues with date-time difference for kanban

--- versionone-feature-collector/src/main/java/com/capitalone/dashboard/collector/StoryDataClient.java
+++ versionone-feature-collector/src/main/java/com/capitalone/dashboard/collector/StoryDataClient.java
@@ -8,6 +8,7 @@ import com.capitalone.dashboard.repository.FeatureCollectorRepository;
 import com.capitalone.dashboard.repository.FeatureRepository;
 import com.capitalone.dashboard.util.DateUtil;
 import com.capitalone.dashboard.util.FeatureCollectorConstants;
+
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.slf4j.Logger;
@@ -27,248 +28,269 @@ import java.util.List;
  * @author kfk884
  */
 public class StoryDataClient extends BaseClient {
-    private static final Logger LOGGER = LoggerFactory.getLogger(StoryDataClient.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(StoryDataClient.class);
 
-    private final FeatureSettings featureSettings;
-    private final FeatureCollectorRepository featureCollectorRepository;
-    private final FeatureRepository featureRepo;
+	private final FeatureSettings featureSettings;
+	private final FeatureCollectorRepository featureCollectorRepository;
+	private final FeatureRepository featureRepo;
 
-    /**
-     * Extends the constructor from the super class.
-     */
-    public StoryDataClient(FeatureSettings featureSettings, FeatureRepository featureRepository,
-                           FeatureCollectorRepository featureCollectorRepository,
-                           VersionOneDataFactoryImpl vOneApi) {
-        super(vOneApi, featureSettings);
-        LOGGER.debug("Constructing data collection for the feature widget, story-level data...");
+	/**
+	 * Extends the constructor from the super class.
+	 */
+	public StoryDataClient(FeatureSettings featureSettings, FeatureRepository featureRepository,
+			FeatureCollectorRepository featureCollectorRepository,
+			VersionOneDataFactoryImpl vOneApi) {
+		super(vOneApi, featureSettings);
+		LOGGER.debug("Constructing data collection for the feature widget, story-level data...");
 
-        this.featureSettings = featureSettings;
-        this.featureRepo = featureRepository;
-        this.featureCollectorRepository = featureCollectorRepository;
-    }
+		this.featureSettings = featureSettings;
+		this.featureRepo = featureRepository;
+		this.featureCollectorRepository = featureCollectorRepository;
+	}
 
-    /**
-     * Updates the MongoDB with a JSONArray received from the source system
-     * back-end with story-based data.
-     *
-     * @param tmpMongoDetailArray A JSON response in JSONArray format from the source system
-     *
-     */
-    @Override
-    @SuppressWarnings({"unchecked", "PMD.ExcessiveMethodLength", "PMD.NPathComplexity"})
-    protected void updateMongoInfo(JSONArray tmpMongoDetailArray) {
-        for (Object obj : tmpMongoDetailArray) {
-            JSONObject dataMainObj = (JSONObject) obj;
-            Feature feature = new Feature();
+	/**
+	 * Updates the MongoDB with a JSONArray received from the source system
+	 * back-end with story-based data.
+	 *
+	 * @param tmpMongoDetailArray
+	 *            A JSON response in JSONArray format from the source system
+	 *
+	 */
+	@Override
+	@SuppressWarnings({ "unchecked", "PMD.ExcessiveMethodLength", "PMD.NPathComplexity" })
+	protected void updateMongoInfo(JSONArray tmpMongoDetailArray) {
+		for (Object obj : tmpMongoDetailArray) {
+			DateUtil dateUtil = new DateUtil();
+			JSONObject dataMainObj = (JSONObject) obj;
+			Feature feature = new Feature();
 
-            removeExistingEntity(getJSONString(dataMainObj, "_oid"));
+			removeExistingEntity(getJSONString(dataMainObj, "_oid"));
 
-            // collectorId
-            feature.setCollectorId(featureCollectorRepository
-                    .findByName(FeatureCollectorConstants.VERSIONONE).getId());
+			// collectorId
+			feature.setCollectorId(featureCollectorRepository
+					.findByName(FeatureCollectorConstants.VERSIONONE).getId());
 
-            // ID
-            feature.setsId(getJSONString(dataMainObj, "_oid"));
+			// ID
+			feature.setsId(getJSONString(dataMainObj, "_oid"));
 
-            // sNumber
-            feature.setsNumber(getJSONString(dataMainObj, "Number"));
+			// sNumber
+			feature.setsNumber(getJSONString(dataMainObj, "Number"));
 
-            // sName
-            feature.setsName(getJSONString(dataMainObj, "Name"));
+			// sName
+			feature.setsName(getJSONString(dataMainObj, "Name"));
 
-            // sStatus
-            feature.setsStatus(getJSONString(dataMainObj, "Status.Name"));
+			// sStatus
+			feature.setsStatus(getJSONString(dataMainObj, "Status.Name"));
 
-            // sState
-            feature.setsState(getJSONString(dataMainObj, "AssetState"));
+			// sState
+			feature.setsState(getJSONString(dataMainObj, "AssetState"));
 
-            // sEstimate
-            feature.setsEstimate(getJSONString(dataMainObj, "Estimate"));
+			// sEstimate
+			feature.setsEstimate(getJSONString(dataMainObj, "Estimate"));
 
-            // sChangeDate
-            feature.setChangeDate(getJSONString(dataMainObj, "ChangeDate"));
+			// sChangeDate
+			feature.setChangeDate(getJSONString(dataMainObj, "ChangeDate"));
 
-            // IsDeleted
-            feature.setIsDeleted(getJSONString(dataMainObj, "IsDeleted"));
+			// IsDeleted
+			feature.setIsDeleted(getJSONString(dataMainObj, "IsDeleted"));
 
-            // sProjectID
-            JSONObject tmpObj = (JSONObject) dataMainObj.get("Scope.ID");
-            if (tmpObj.containsKey("_oid")) {
-                feature.setsProjectID(getJSONString(tmpObj, "_oid"));
-            }
+			// sProjectID
+			JSONObject tmpObj = (JSONObject) dataMainObj.get("Scope.ID");
+			if (tmpObj.containsKey("_oid")) {
+				feature.setsProjectID(getJSONString(tmpObj, "_oid"));
+			}
 
-            // sProjectName
-            feature.setsProjectName(getJSONString(dataMainObj, "Scope.Name"));
+			// sProjectName
+			feature.setsProjectName(getJSONString(dataMainObj, "Scope.Name"));
 
-            // sProjectBeginDate
-            feature.setsProjectBeginDate(getJSONString(dataMainObj, "Scope.BeginDate"));
+			// sProjectBeginDate
+			feature.setsProjectBeginDate(getJSONString(dataMainObj, "Scope.BeginDate"));
 
-            // sProjectEndDate
-            feature.setsProjectEndDate(getJSONString(dataMainObj, "Scope.EndDate"));
+			// sProjectEndDate
+			feature.setsProjectEndDate(getJSONString(dataMainObj, "Scope.EndDate"));
 
-            // sProjectChangeDate
-            feature.setsProjectChangeDate(getJSONString(dataMainObj, "Scope.ChangeDate"));
+			// sProjectChangeDate
+			feature.setsProjectChangeDate(getJSONString(dataMainObj, "Scope.ChangeDate"));
 
-            // sProjectState
-            feature.setsProjectState(getJSONString(dataMainObj, "Scope.AssetState"));
+			// sProjectState
+			feature.setsProjectState(getJSONString(dataMainObj, "Scope.AssetState"));
 
-            // sProjectIsDeleted
-            feature.setsProjectIsDeleted(getJSONString(dataMainObj, "Scope.IsDeleted"));
+			// sProjectIsDeleted
+			feature.setsProjectIsDeleted(getJSONString(dataMainObj, "Scope.IsDeleted"));
 
-            // sProjectPath
-            String projPath = feature.getsProjectName();
-            List<String> projList = (List<String>) dataMainObj.get("Scope.ParentAndUp.Name");
-            if (!CollectionUtils.isEmpty(projList)) {
-                for (String proj : projList) {
-                    projPath = proj + "-->" + projPath;
-                }
-                projPath = "All-->" + projPath;
-            } else {
-                projPath = "All-->" + projPath;
-            }
-            feature.setsProjectPath(sanitizeResponse(projPath));
+			// sProjectPath
+			String projPath = feature.getsProjectName();
+			List<String> projList = (List<String>) dataMainObj.get("Scope.ParentAndUp.Name");
+			if (!CollectionUtils.isEmpty(projList)) {
+				for (String proj : projList) {
+					projPath = proj + "-->" + projPath;
+				}
+				projPath = "All-->" + projPath;
+			} else {
+				projPath = "All-->" + projPath;
+			}
+			feature.setsProjectPath(sanitizeResponse(projPath));
 
-            // sEpicID
-            tmpObj = (JSONObject) dataMainObj.get("Super.ID");
-            if (tmpObj.containsKey("_oid")) {
-                feature.setsEpicID(getJSONString(tmpObj, "_oid"));
-            }
+			// sEpicID
+			tmpObj = (JSONObject) dataMainObj.get("Super.ID");
+			if (tmpObj.containsKey("_oid")) {
+				feature.setsEpicID(getJSONString(tmpObj, "_oid"));
+			}
 
-            // sEpicNumber
-            feature.setsEpicNumber(getJSONString(dataMainObj, "Super.Number"));
+			// sEpicNumber
+			feature.setsEpicNumber(getJSONString(dataMainObj, "Super.Number"));
 
-            // sEpicName
-            feature.setsEpicName(getJSONString(dataMainObj, "Super.Name"));
+			// sEpicName
+			feature.setsEpicName(getJSONString(dataMainObj, "Super.Name"));
 
-            // sEpicBeginDate
-            feature.setsEpicBeginDate(getJSONString(dataMainObj, "Super.PlannedStart"));
+			// sEpicBeginDate
+			feature.setsEpicBeginDate(getJSONString(dataMainObj, "Super.PlannedStart"));
 
-            // sEpicEndDate
-            feature.setsEpicEndDate(getJSONString(dataMainObj, "Super.PlannedEnd"));
+			// sEpicEndDate
+			feature.setsEpicEndDate(getJSONString(dataMainObj, "Super.PlannedEnd"));
 
-            // sEpicType
-            feature.setsEpicType(getJSONString(dataMainObj, "Super.Category.Name"));
+			// sEpicType
+			feature.setsEpicType(getJSONString(dataMainObj, "Super.Category.Name"));
 
-            // sEpicAssetState
-            feature.setsEpicAssetState(getJSONString(dataMainObj, "Super.AssetState"));
+			// sEpicAssetState
+			feature.setsEpicAssetState(getJSONString(dataMainObj, "Super.AssetState"));
 
-            // sEpicChangeDate
-            feature.setsEpicChangeDate(getJSONString(dataMainObj, "Super.ChangeDate"));
+			// sEpicChangeDate
+			feature.setsEpicChangeDate(getJSONString(dataMainObj, "Super.ChangeDate"));
 
-            // sEpicIsDeleted
-            feature.setsEpicIsDeleted(getJSONString(dataMainObj, "Super.IsDeleted"));
+			// sEpicIsDeleted
+			feature.setsEpicIsDeleted(getJSONString(dataMainObj, "Super.IsDeleted"));
 
-            // sSprintID
-            tmpObj = (JSONObject) dataMainObj.get("Timebox.ID");
-            feature.setsSprintID(getJSONString(tmpObj, "_oid"));
+			if (dateUtil.evaluateSprintLength(getJSONString(dataMainObj, "Timebox.StartDate"),
+					getJSONString(dataMainObj, "Timebox.EndDate"))) {
+				// sSprintID
+				tmpObj = (JSONObject) dataMainObj.get("Timebox.ID");
+				feature.setsSprintID(getJSONString(tmpObj, "_oid"));
 
-            // sSprintName
-            feature.setsSprintName(getJSONString(dataMainObj, "Timebox.Name"));
+				// sSprintName
+				feature.setsSprintName(getJSONString(dataMainObj, "Timebox.Name"));
 
-            // sSprintBeginDate
-            feature.setsSprintBeginDate(getJSONString(dataMainObj, "Timebox.BeginDate"));
+				// sSprintBeginDate
+				feature.setsSprintBeginDate(getJSONString(dataMainObj, "Timebox.BeginDate"));
 
-            // sSprintEndDate
-            feature.setsSprintEndDate(getJSONString(dataMainObj, "Timebox.EndDate"));
+				// sSprintEndDate
+				feature.setsSprintEndDate(getJSONString(dataMainObj, "Timebox.EndDate"));
 
-            // sSprintAssetState
-            feature.setsSprintAssetState(getJSONString(dataMainObj, "Timebox.AssetState"));
+				// sSprintAssetState
+				feature.setsSprintAssetState(getJSONString(dataMainObj, "Timebox.AssetState"));
 
-            // sSprintChangeDate
-            feature.setsSprintChangeDate(getJSONString(dataMainObj, "Timebox.ChangeDate"));
+				// sSprintChangeDate
+				feature.setsSprintChangeDate(getJSONString(dataMainObj, "Timebox.ChangeDate"));
 
-            // sSprintIsDeleted
-            feature.setsSprintIsDeleted(getJSONString(dataMainObj, "Timebox.IsDeleted"));
+				// sSprintIsDeleted
+				feature.setsSprintIsDeleted(getJSONString(dataMainObj, "Timebox.IsDeleted"));
+			} else {
+				/*
+				 * For Kanban, associate a generic, never-ending kanban 'sprint'
+				 */
+				feature.setsSprintID(FeatureCollectorConstants.KANBAN_SPRINT_ID);
+				feature.setsSprintName(FeatureCollectorConstants.KANBAN_SPRINT_ID);
+				feature.setsSprintBeginDate(FeatureCollectorConstants.KANBAN_START_DATE);
+				feature.setsSprintEndDate(FeatureCollectorConstants.KANBAN_END_DATE);
+				feature.setsSprintAssetState(getJSONString(dataMainObj, "Timebox.AssetState"));
+			}
 
-            // sTeamID
-            tmpObj = (JSONObject) dataMainObj.get("Team.ID");
-            feature.setsTeamID(getJSONString(tmpObj, "_oid"));
+			// sTeamID
+			tmpObj = (JSONObject) dataMainObj.get("Team.ID");
+			feature.setsTeamID(getJSONString(tmpObj, "_oid"));
 
-            // sTeamName
-            feature.setsTeamName(getJSONString(dataMainObj, "Team.Name"));
+			// sTeamName
+			feature.setsTeamName(getJSONString(dataMainObj, "Team.Name"));
 
-            // sTeamChangeDate
-            feature.setsTeamChangeDate(getJSONString(dataMainObj, "Team.ChangeDate"));
+			// sTeamChangeDate
+			feature.setsTeamChangeDate(getJSONString(dataMainObj, "Team.ChangeDate"));
 
-            // sTeamAssetState
-            feature.setsTeamAssetState(getJSONString(dataMainObj, "Team.AssetState"));
+			// sTeamAssetState
+			feature.setsTeamAssetState(getJSONString(dataMainObj, "Team.AssetState"));
 
-            // sTeamIsDeleted
-            feature.setsTeamIsDeleted(getJSONString(dataMainObj, "Team.IsDeleted"));
+			// sTeamIsDeleted
+			feature.setsTeamIsDeleted(getJSONString(dataMainObj, "Team.IsDeleted"));
 
-            // sOwnersID
-            List<String> ownersIdList = new ArrayList<>();
-            for (Object ownersID : (JSONArray) dataMainObj.get("Owners.ID")) {
-                ownersIdList.add(getJSONString((JSONObject) ownersID, "_oid"));
-            }
-            feature.setsOwnersID(ownersIdList);
+			// sOwnersID
+			List<String> ownersIdList = new ArrayList<>();
+			for (Object ownersID : (JSONArray) dataMainObj.get("Owners.ID")) {
+				ownersIdList.add(getJSONString((JSONObject) ownersID, "_oid"));
+			}
+			feature.setsOwnersID(ownersIdList);
 
-            // sOwnersShortName
-            feature.setsOwnersShortName(toCanonicalList((List<String>) dataMainObj.get("Owners.Nickname")));
+			// sOwnersShortName
+			feature.setsOwnersShortName(
+					toCanonicalList((List<String>) dataMainObj.get("Owners.Nickname")));
 
-            // sOwnersFullName
-            feature.setsOwnersFullName(toCanonicalList((List<String>) dataMainObj.get("Owners.Name")));
+			// sOwnersFullName
+			feature.setsOwnersFullName(
+					toCanonicalList((List<String>) dataMainObj.get("Owners.Name")));
 
-            // sOwnersUsername
-            feature.setsOwnersUsername(toCanonicalList((List<String>) dataMainObj.get("Owners.Username")));
+			// sOwnersUsername
+			feature.setsOwnersUsername(
+					toCanonicalList((List<String>) dataMainObj.get("Owners.Username")));
 
-            // sOwnersState
-            feature.setsOwnersState(toCanonicalList((List<String>) dataMainObj.get("Owners.AssetState")));
+			// sOwnersState
+			feature.setsOwnersState(
+					toCanonicalList((List<String>) dataMainObj.get("Owners.AssetState")));
 
-            // sOwnersChangeDate
-            feature.setsOwnersChangeDate(toCanonicalList((List<String>) dataMainObj.get("Owners.ChangeDate")));
+			// sOwnersChangeDate
+			feature.setsOwnersChangeDate(
+					toCanonicalList((List<String>) dataMainObj.get("Owners.ChangeDate")));
 
-            // sOwnersIsDeleted
-            feature.setsOwnersIsDeleted(toCanonicalList((List<String>) dataMainObj.get("Owners.IsDeleted")));
+			// sOwnersIsDeleted
+			feature.setsOwnersIsDeleted(
+					toCanonicalList((List<String>) dataMainObj.get("Owners.IsDeleted")));
 
-            featureRepo.save(feature);
-        }
-    }
+			featureRepo.save(feature);
+		}
+	}
 
-    /**
-     * Explicitly updates queries for the source system, and initiates the
-     * update to MongoDB from those calls.
-     */
-    public void updateStoryInformation() throws HygieiaException {
-        String returnDate = this.featureSettings.getDeltaStartDate();
-        if (!StringUtils.isEmpty(getMaxChangeDate())) {
-            returnDate = getMaxChangeDate();
-        }
-        returnDate = DateUtil.getChangeDateMinutePrior(returnDate,
-                this.featureSettings.getScheduledPriorMin());
-        String queryName = this.featureSettings.getStoryQuery();
-        updateObjectInformation(getQuery(returnDate, queryName));
-    }
+	/**
+	 * Explicitly updates queries for the source system, and initiates the
+	 * update to MongoDB from those calls.
+	 */
+	public void updateStoryInformation() throws HygieiaException {
+		String returnDate = this.featureSettings.getDeltaStartDate();
+		if (!StringUtils.isEmpty(getMaxChangeDate())) {
+			returnDate = getMaxChangeDate();
+		}
+		returnDate = DateUtil.getChangeDateMinutePrior(returnDate,
+				this.featureSettings.getScheduledPriorMin());
+		String queryName = this.featureSettings.getStoryQuery();
+		updateObjectInformation(getQuery(returnDate, queryName));
+	}
 
-    /**
-     * Validates current entry and removes new entry if an older item exists in
-     * the repo
-     *
-     * @param localId local repository item ID (not the precise mongoID)
-     */
-    protected void removeExistingEntity(String localId) {
-        if (StringUtils.isEmpty(localId))
-            return;
-        List<Feature> listOfFeature = featureRepo.getFeatureIdById(localId);
+	/**
+	 * Validates current entry and removes new entry if an older item exists in
+	 * the repo
+	 *
+	 * @param localId
+	 *            local repository item ID (not the precise mongoID)
+	 */
+	protected void removeExistingEntity(String localId) {
+		if (StringUtils.isEmpty(localId))
+			return;
+		List<Feature> listOfFeature = featureRepo.getFeatureIdById(localId);
 
-        if (CollectionUtils.isEmpty(listOfFeature))
-            return;
-        featureRepo.delete(listOfFeature);
-    }
+		if (CollectionUtils.isEmpty(listOfFeature))
+			return;
+		featureRepo.delete(listOfFeature);
+	}
 
-    @Override
-    public String getMaxChangeDate() {
-        Collector col = featureCollectorRepository.findByName(FeatureCollectorConstants.VERSIONONE);
-        if (col == null)
-            return "";
-        if (StringUtils.isEmpty(featureSettings.getDeltaStartDate()))
-            return "";
+	@Override
+	public String getMaxChangeDate() {
+		Collector col = featureCollectorRepository.findByName(FeatureCollectorConstants.VERSIONONE);
+		if (col == null)
+			return "";
+		if (StringUtils.isEmpty(featureSettings.getDeltaStartDate()))
+			return "";
 
-        List<Feature> response = featureRepo
-                .findTopByCollectorIdAndChangeDateGreaterThanOrderByChangeDateDesc(col.getId(),
-                        featureSettings.getDeltaStartDate());
-        if (!CollectionUtils.isEmpty(response))
-            return response.get(0).getChangeDate();
-        return "";
-    }
+		List<Feature> response = featureRepo
+				.findTopByCollectorIdAndChangeDateGreaterThanOrderByChangeDateDesc(col.getId(),
+						featureSettings.getDeltaStartDate());
+		if (!CollectionUtils.isEmpty(response))
+			return response.get(0).getChangeDate();
+		return "";
+	}
 }
--- versionone-feature-collector/src/main/java/com/capitalone/dashboard/util/DateUtil.java
+++ versionone-feature-collector/src/main/java/com/capitalone/dashboard/util/DateUtil.java
@@ -1,40 +1,42 @@
 package com.capitalone.dashboard.util;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.versionone.DB.DateTime;
 
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public final class DateUtil {
 	private static final Logger LOGGER = LoggerFactory.getLogger(DateUtil.class);
 
 	public static final String ISO_DATE_TIME_FORMAT = "yyyy-MM-dd'T'HH:mm:ss";
 
-
 	public static Date fromISODateTimeFormat(String isoString) {
 		String iString = isoString;
 		int charIndex = iString.indexOf(".");
-		if (charIndex != -1){
+		if (charIndex != -1) {
 			iString = iString.substring(0, charIndex);
 		}
 		try {
 			return new SimpleDateFormat(ISO_DATE_TIME_FORMAT).parse(iString);
 		} catch (ParseException e) {
-			LOGGER.error("Parse error of: "+ iString, e);
-            return null;
+			LOGGER.error("Parse error of: " + iString, e);
+			return null;
 		}
 	}
-
 
 	public static String toISODateRealTimeFormat(Date iDate) {
 		DateFormat df = new SimpleDateFormat(ISO_DATE_TIME_FORMAT);
 		return df.format(iDate);
 	}
-
 
 	public static Date getDatePriorToMinutes(Date fromDate, int minutes) {
 		Calendar calendar = Calendar.getInstance();
@@ -43,18 +45,71 @@ public final class DateUtil {
 		return calendar.getTime();
 	}
 
-    /**
-     * Generates and retrieves the change date that occurs a minute prior to the
-     * specified change date in ISO format.
-     *
-     * @param changeDateISO
-     *            A given change date in ISO format
-     * @return The ISO-formatted date/time stamp for a minute prior to the given
-     *         change date
-     */
-    public static String getChangeDateMinutePrior(String changeDateISO, int priorMinutes) {
-        return DateUtil.toISODateRealTimeFormat(DateUtil.getDatePriorToMinutes(
-                DateUtil.fromISODateTimeFormat(changeDateISO), priorMinutes));
-    }
+	/**
+	 * Generates and retrieves the change date that occurs a minute prior to the
+	 * specified change date in ISO format.
+	 *
+	 * @param changeDateISO
+	 *            A given change date in ISO format
+	 * @return The ISO-formatted date/time stamp for a minute prior to the given
+	 *         change date
+	 */
+	public static String getChangeDateMinutePrior(String changeDateISO, int priorMinutes) {
+		return DateUtil.toISODateRealTimeFormat(DateUtil.getDatePriorToMinutes(
+				DateUtil.fromISODateTimeFormat(changeDateISO), priorMinutes));
+	}
+
+	/**
+	 * Evaluates whether a sprint length appears to be kanban or scrum
+	 * 
+	 * @param startDate
+	 *            The start date of a sprint in ISO format
+	 * @param endDate
+	 *            The end date of a sprint in ISO format
+	 * @return True indicates a scrum sprint; False indicates a Kanban sprint
+	 */
+	public boolean evaluateSprintLength(String startDate, String endDate) {
+		boolean sprintIndicator = false;
+		Calendar startCalendar = Calendar.getInstance();
+		Calendar endCalendar = Calendar.getInstance();
+
+		if ((!StringUtils.isAnyEmpty(startDate) && !StringUtils.isAnyEmpty(endDate))
+				&& (this.isInteger(startDate.substring(0, 4), 10)
+						&& this.isInteger(endDate.substring(0, 4), 10)
+						&& this.isInteger(startDate.substring(8, 10), 10)
+						&& this.isInteger(endDate.substring(8, 10), 10)
+						&& this.isInteger(startDate.substring(8, 10), 10)
+						&& this.isInteger(endDate.substring(8, 10), 10))) {
+			startCalendar.set(Integer.valueOf(startDate.substring(0, 4)),
+					Integer.valueOf(startDate.substring(5, 7)),
+					Integer.valueOf(startDate.substring(8, 10)));
+			endCalendar.set(Integer.valueOf(endDate.substring(0, 4)),
+					Integer.valueOf(endDate.substring(5, 7)),
+					Integer.valueOf(endDate.substring(8, 10)));
+			long diffMill = endCalendar.getTimeInMillis() - startCalendar.getTimeInMillis();
+			long diffDays = TimeUnit.DAYS.convert(diffMill, TimeUnit.MILLISECONDS);
+
+			if (diffDays <= 28) {
+				// Scrum-enough
+				sprintIndicator = true;
+			}
+		} else {
+			// Default to kanban
+			sprintIndicator = false;
+
+		}
+
+		return sprintIndicator;
+	}
+
+	private boolean isInteger(String s, int radix) {
+		Scanner sc = new Scanner(s.trim());
+		if (!sc.hasNextInt(radix))
+			return false;
+		// we know it starts with a valid int, now make sure
+		// there's nothing left!
+		sc.nextInt(radix);
+		return !sc.hasNext();
+	}
 
 }
