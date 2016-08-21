fe82e19beab9034b96e2077d6d799f16199fbb51
Shamik Ray <shamik.ray@gdn-commerce.com>
2016-08-22 00:39:28 +0530

    Bamboo Build Collector

---
+++ Bamboo-build-collector/src/main/java/com/capitalone/dashboard/repository/BambooCollectorRepository.java
@@ -0,0 +1,9 @@
+package com.capitalone.dashboard.repository;
+
+import com.capitalone.dashboard.model.BambooCollector;
+
+/**
+ * Collector repository for the BambooCollector subclass
+ */
+public interface BambooCollectorRepository extends BaseCollectorRepository<BambooCollector> {
+}
