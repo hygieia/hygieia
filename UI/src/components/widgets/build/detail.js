88f1a3ba99b7b9348708e8657519d81329a6715a
satishc1 <satish.ramabhadran@capitalone.com>
2016-09-08 09:45:39 -0400

    Replace collectorName with collectorNiceName in build detail widget. #784

--- UI/src/components/widgets/build/detail.js
+++ UI/src/components/widgets/build/detail.js
@@ -8,15 +8,31 @@
         .module(HygieiaConfig.module)
         .controller('BuildWidgetDetailController', BuildWidgetDetailController);
 
-    BuildWidgetDetailController.$inject = ['$scope', '$modalInstance', 'build', 'collectorName'];
-    function BuildWidgetDetailController($scope, $modalInstance, build, collectorName) {
+    BuildWidgetDetailController.$inject = ['$scope', '$modalInstance', 'build', 'collectorName', 'collectorNiceName'];
+    function BuildWidgetDetailController($scope, $modalInstance, build, collectorName, collectorNiceName) {
         var ctrl = this;
 
         ctrl.build = build;
         ctrl.collectorName = collectorName;
+        ctrl.collectorNiceName = collectorNiceName;
 
+        ctrl.buildUrlNiceName = buildUrlNiceName;
         ctrl.buildPassed = buildPassed;
         ctrl.close = close;
+
+        function buildUrlNiceName() {
+            if (!isEmpty(collectorNiceName)) {
+                return collectorNiceName;
+            } else {
+                return collectorName;
+            }
+        }
+
+        function isEmpty(str) {
+            //!str returns true for uninitialized, null and empty strings
+            //the test checks if the string only contains whitespaces and returns true.
+            return !str || /^[\s]*$/.test(str);
+        }
 
         function buildPassed() {
             return ctrl.build.buildStatus === 'Success';
