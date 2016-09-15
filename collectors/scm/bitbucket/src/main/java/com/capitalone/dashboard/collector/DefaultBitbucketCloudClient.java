fd35a8167b9ecb34451d005650c682c0de4f6642
MarkRx <MarkRx@users.noreply.github.com>
2016-09-15 12:09:42 +0000

    Add scm parent revision numbers to Commits

--- collectors/scm/bitbucket/src/main/java/com/capitalone/dashboard/collector/DefaultBitbucketCloudClient.java
+++ collectors/scm/bitbucket/src/main/java/com/capitalone/dashboard/collector/DefaultBitbucketCloudClient.java
@@ -147,11 +147,19 @@ public class DefaultBitbucketCloudClient implements GitClient {
 					String message = str(jsonObject, "message");
 					String author = str(authorObject, "raw");
 					long timestamp = new DateTime(str(jsonObject, "date")).getMillis();
+					JSONArray parents = (JSONArray) jsonObject.get("parents");
+					List<String> parentShas = new ArrayList<>();
+					if (parents != null) {
+						for (Object parentObj : parents) {
+							parentShas.add(str((JSONObject)parentObj, "id"));
+						}
+					}
 					
 					Commit commit = new Commit();
 					commit.setTimestamp(System.currentTimeMillis());
 					commit.setScmUrl(repo.getRepoUrl());
 					commit.setScmRevisionNumber(sha);
+					commit.setScmParentRevisionNumbers(parentShas);
 					commit.setScmAuthor(author);
 					commit.setScmCommitLog(message);
 					commit.setScmCommitTimestamp(timestamp);
