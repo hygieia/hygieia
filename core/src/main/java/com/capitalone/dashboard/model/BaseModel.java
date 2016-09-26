9f89a968f5c536968956d7db3829e06bed5ad075
MarkRx <MarkRx@users.noreply.github.com>
2016-09-26 15:44:30 -0400

    Revert change to add hashcode+equals to BaseModel due to unintended side effects

--- core/src/main/java/com/capitalone/dashboard/model/BaseModel.java
+++ core/src/main/java/com/capitalone/dashboard/model/BaseModel.java
@@ -18,36 +18,10 @@ public class BaseModel {
         this.id = id;
     }
 
-	/* (non-Javadoc)
-	 * @see java.lang.Object#hashCode()
-	 */
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	/* (non-Javadoc)
-	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		BaseModel other = (BaseModel) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
-    
-    
+    /*
+     * Note:
+     * 
+     * Having hashcode + equals is more complicated than simply comparing ObjectIds since
+     * it does not provide a way to properly compare models that have not been saved yet.
+     */
 }
