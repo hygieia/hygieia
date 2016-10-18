583ad6d3e5cd705c7002bafef909bd7a2c7d4b2f
Rob Miller <rob.miller.deiw@statefarm.com>
2016-10-18 13:12:20 -0700

    extracted commit building to a private method and added code to trust self signed certs

--- collectors/scm/gitlab/src/main/java/com/capitalone/dashboard/collecteur/DefaultGitlabGitClient.java
+++ collectors/scm/gitlab/src/main/java/com/capitalone/dashboard/collecteur/DefaultGitlabGitClient.java
@@ -4,10 +4,14 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.List;
@@ -19,6 +23,7 @@ import javax.net.ssl.SSLSession;
 import javax.net.ssl.TrustManager;
 import javax.net.ssl.X509TrustManager;
 
+import org.apache.commons.codec.binary.Base64;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -28,6 +33,10 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestOperations;
 
@@ -49,7 +58,6 @@ public class DefaultGitlabGitClient implements  GitlabGitClient {
     private final RestOperations restOperations;
 
     private static final String SEGMENT_API = "/api/v3/projects/";
-    
     private static final String PUBLIC_GITLAB_HOST_NAME = "gitlab.company.com";
     
     @Autowired
@@ -57,17 +65,6 @@ public class DefaultGitlabGitClient implements  GitlabGitClient {
                                        Supplier<RestOperations> restOperationsSupplier) {
         this.gitlabSettings = gitlabSettings;
         this.restOperations = restOperationsSupplier.get();
-    }
-
-    private TrustManager[ ] get_trust_mgr() {
-        TrustManager[ ] certs = new TrustManager[ ] {
-                new X509TrustManager() {
-                    public X509Certificate[ ] getAcceptedIssuers() { return null; }
-                    public void checkClientTrusted(X509Certificate[ ] certs, String t) { }
-                    public void checkServerTrusted(X509Certificate[ ] certs, String t) { }
-                }
-        };
-        return certs;
     }
 
     @Override
@@ -123,27 +120,7 @@ public class DefaultGitlabGitClient implements  GitlabGitClient {
                                 JSONArray jsonArray = (JSONArray) obj;
                                 for (Object item : jsonArray) {
                                     JSONObject jsonObject = (JSONObject) item;
-                                    String author = (String) jsonObject.get("author_name");
-                                    String message = (String) jsonObject.get("message");
-                                    long timestamp = new DateTime(jsonObject.get("created_at"))
-                                            .getMillis();
-
-                                    Commit commit = new Commit();
-                                    commit.setTimestamp(System.currentTimeMillis());
-                                    commit.setScmUrl(repo.getOptions().get("url").toString());
-                                    commit.setScmAuthor(author);
-                                    commit.setScmCommitLog(message);
-                                    commit.setScmCommitTimestamp(timestamp);
-                                    commit.setNumberOfChanges(159753);
-                                /*
-                                    LOG.info("getTimestamp " + commit.getTimestamp());
-                                    LOG.info("getScmUrl " + commit.getScmUrl());
-                                    LOG.info("getScmBranch " + commit.getScmBranch());
-                                    LOG.info("getScmCommitLog " + commit.getScmCommitLog());
-                                    LOG.info("getScmCommitTimestamp " + commit.getScmCommitTimestamp());
-                                    LOG.info("getNumberOfChanges " + commit.getNumberOfChanges());
-                                */
-                                    commits.add(commit);
+									commits.add(buildCommit(jsonObject, repo.getRepoUrl(), repo.getBranch()));
                                 }
                             } catch (ParseException e) {
                                 LOG.error(e);
@@ -177,7 +154,7 @@ public class DefaultGitlabGitClient implements  GitlabGitClient {
 	 * @return url api format =
 	 *         https://gitlab.company.com/api/v3/projects/team%2Freponame/repository/commits/
 	 */
-    public String buildApiUrl(GitlabGitRepo repo){
+	private String buildApiUrl(GitlabGitRepo repo) {
         String repoUrl = (String) repo.getOptions().get("url");
         
         if (repoUrl.endsWith(".git")) {
@@ -207,4 +184,100 @@ public class DefaultGitlabGitClient implements  GitlabGitClient {
 
 		return apiUrl;
     }
+
+	private Commit buildCommit(JSONObject jsonObject, String repoUrl, String repoBranch) {
+		String author = str(jsonObject, "author_name");
+		String message = str(jsonObject, "message");
+		String id = str(jsonObject, "id");
+		long timestamp = new DateTime(str(jsonObject, "created_at")).getMillis();
+
+		Commit commit = new Commit();
+		commit.setTimestamp(System.currentTimeMillis());
+		commit.setScmUrl(repoUrl);
+		// TODO: figure out why branch name isn't getting through
+		commit.setScmBranch(repoBranch);
+		commit.setScmRevisionNumber(id);
+		commit.setScmAuthor(author);
+		commit.setScmCommitLog(message);
+		commit.setScmCommitTimestamp(timestamp);
+		commit.setNumberOfChanges(159753);
+		// TODO: figure out commit type and parents
+		return commit;
+	}
+
+	private String str(JSONObject json, String key) {
+		Object value = json.get(key);
+		return value == null ? null : value.toString();
+	}
+
+	private ResponseEntity<String> makeRestCall(String url, String userId, String password) {
+		trustSelfSignedSSL();
+		URI uri = null;
+		try {
+			uri = new URI(url);
+		} catch (URISyntaxException e) {
+			// e.printStackTrace();
+		}
+		// Basic Auth only.
+		if (!"".equals(userId) && !"".equals(password)) {
+			return restOperations.exchange(uri, HttpMethod.GET, new HttpEntity<>(createHeaders(userId, password)),
+					String.class);
+
+		} else {
+			return restOperations.exchange(uri, HttpMethod.GET, null, String.class);
+		}
+
+	}
+
+	private void trustSelfSignedSSL() {
+		try {
+			final SSLContext ctx = SSLContext.getInstance("TLS");
+			final X509TrustManager tm = new X509TrustManager() {
+				public void checkClientTrusted(final X509Certificate[] xcs, final String string)
+						throws CertificateException {
+					// do nothing
+				}
+
+				public void checkServerTrusted(final X509Certificate[] xcs, final String string)
+						throws CertificateException {
+					// do nothing
+				}
+
+				public X509Certificate[] getAcceptedIssuers() {
+					X509Certificate[] n = new X509Certificate[0];
+					return n;
+
+				}
+			};
+			ctx.init(null, new TrustManager[] { tm }, null);
+			SSLContext.setDefault(ctx);
+		} catch (final Exception ex) {
+			// ex.printStackTrace();
+		}
+	}
+
+	private HttpHeaders createHeaders(final String userId, final String password) {
+		String auth = userId + ":" + password;
+		byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(StandardCharsets.US_ASCII));
+		String authHeader = "Basic " + new String(encodedAuth);
+
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Authorization", authHeader);
+		return headers;
+	}
+
+	private TrustManager[] get_trust_mgr() {
+		TrustManager[] certs = new TrustManager[] { new X509TrustManager() {
+			public X509Certificate[] getAcceptedIssuers() {
+				return null;
+			}
+
+			public void checkClientTrusted(X509Certificate[] certs, String t) {
+			}
+
+			public void checkServerTrusted(X509Certificate[] certs, String t) {
+			}
+		} };
+		return certs;
+	}
 }
