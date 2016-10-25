f6433814bc754fb8b24809359fe3cea587f6faa9
MarkRx <MarkRx@users.noreply.github.com>
2016-10-25 12:35:28 -0400

    Add ability to configure API and UI system configurations

---
+++ api/src/main/java/com/capitalone/dashboard/service/SystemConfigService.java
@@ -0,0 +1,7 @@
+package com.capitalone.dashboard.service;
+
+import com.captialone.dashboard.response.SystemConfigResponse;
+
+public interface SystemConfigService {
+	SystemConfigResponse getSystemConfig();
+}
