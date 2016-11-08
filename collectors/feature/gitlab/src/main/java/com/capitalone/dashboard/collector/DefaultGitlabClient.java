b99795ecd0b34ca856e1954a570b2e2aee1faded
Rob Miller <rob.miller.deiw@statefarm.com>
2016-11-08 14:09:41 -0700

    adds teams to db

--- collectors/feature/gitlab/src/main/java/com/capitalone/dashboard/collector/DefaultGitlabClient.java
+++ collectors/feature/gitlab/src/main/java/com/capitalone/dashboard/collector/DefaultGitlabClient.java
@@ -2,13 +2,11 @@ package com.capitalone.dashboard.collector;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.json.simple.JSONArray;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
-import org.json.simple.parser.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestOperations;
+
+import com.capitalone.dashboard.model.GitlabTeam;
 
 @Component
 public class DefaultGitlabClient implements GitlabClient {
@@ -22,25 +20,8 @@ public class DefaultGitlabClient implements GitlabClient {
 	}
 
 	@Override
-	public JSONArray getTeams() {
-		String response = restOperations.getForObject("url to gitlab", String.class);
-		JSONArray teams = parseAsArray(response);
-
-		return teams;
-	}
-	
-	private JSONArray parseAsArray(String response) {
-		try {
-			return (JSONArray) new JSONParser().parse(response);
-		} catch (ParseException pe) {
-			LOG.error(pe.getMessage());
-		}
-		return new JSONArray();
-	}
-	
-	private String str(JSONObject json, String key) {
-		Object value = json.get(key);
-		return value == null ? null : value.toString();
+	public GitlabTeam[] getTeams() {
+		return restOperations.getForObject("", GitlabTeam[].class);
 	}
 
 }
