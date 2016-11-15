8edc72c3be529c776310b70ee0523a06466c55fb
Rob Miller <rob.miller.deiw@statefarm.com>
2016-11-15 07:40:31 -0700

    added closing curly brace which fixed bug in features being returned for kanban board

--- core/src/main/java/com/capitalone/dashboard/repository/FeatureRepository.java
+++ core/src/main/java/com/capitalone/dashboard/repository/FeatureRepository.java
@@ -74,7 +74,7 @@ public interface FeatureRepository extends CrudRepository<Feature, ObjectId>,
 	@Query(value = "{'sTeamID' : ?0 , 'isDeleted' : 'False', $and : [{'sSprintID' : {$ne : null}} , {'sSprintID' : {?2 : ?3}} , {'sSprintBeginDate' : {$lte : ?1}} , {'sSprintEndDate' : {$gte : ?1}}]}, $orderby: { 'sStatus' :-1 }", fields = "{'sSprintID' : 1, 'sSprintName' : 1,'sSprintBeginDate' : 1, 'sSprintEndDate' : 1}")
 	List<Feature> getCurrentSprintDetail(String sTeamID, String currentISODateTime, String agileTypeAggregate, String agileType);
 
-	@Query(value = " {'sTeamID' : ?0 , 'isDeleted' : 'False', $and : [{'sSprintID' : {$ne : null} , {'sSprintID' : {?2 : ?3}} , {'sSprintBeginDate' : {$lte : ?1}} , {'sSprintEndDate' : {$gte : ?1}}]}, $orderby: { 'sEpicID' :-1 }", fields = "{'sEpicID' : 1,'sEpicNumber' : 1,'sEpicName' : 1,'sEstimate' : 1,'sEstimateTime' : 1}")
+	@Query(value = " {'sTeamID' : ?0 , 'isDeleted' : 'False', $and : [{'sSprintID' : {$ne : null}} , {'sSprintID' : {?2 : ?3}} , {'sSprintBeginDate' : {$lte : ?1}} , {'sSprintEndDate' : {$gte : ?1}}]}, $orderby: { 'sEpicID' :-1 }", fields = "{'sEpicID' : 1,'sEpicNumber' : 1,'sEpicName' : 1,'sEstimate' : 1,'sEstimateTime' : 1}")
 	List<Feature> getInProgressFeaturesEstimatesByTeamId(String sTeamID, String currentISODateTime, String agileTypeAggregate, String agileType);
 
 	@Query(value = " {'sTeamID' : ?0 , 'isDeleted' : 'False', $and : [{'sSprintID' : {$ne : null}} , {'sSprintID' : {?2 : ?3}} , {'sSprintBeginDate' : {$lte : ?1}} , {'sSprintEndDate' : {$gte : ?1}}]}, $orderby: { 'sStatus' :-1 }", fields = "{'sStatus': 1,'sEstimate' : 1,'sEstimateTime' : 1}")
