d715ceda87db7e1f7da335934b2fdd5ee1712ec8
Rob Miller <rob.miller.deiw@statefarm.com>
2016-11-29 14:29:49 -0700

    updated properties builder

--- collectors/feature/gitlab/docker/gitlab-properties-builder.sh
+++ collectors/feature/gitlab/docker/gitlab-properties-builder.sh
@@ -8,7 +8,7 @@ then
         #for testing locally
         PROP_FILE=application.properties
 else 
-	PROP_FILE=hygieia-versionone-feature-collector.properties
+	PROP_FILE=gitlab-feature-collector.properties
 fi
   
 if [ "$MONGO_PORT" != "" ]; then
@@ -41,42 +41,20 @@ dbusername=${HYGIEIA_API_ENV_SPRING_DATA_MONGODB_USERNAME:-db}
 #Database Password - default is blank
 dbpassword=${HYGIEIA_API_ENV_SPRING_DATA_MONGODB_PASSWORD:-dbpass}
 
-#Page size for data calls (VersionOne recommended 2000)
-feature.pageSize=${VERSIONONE_PAGE_SIZE:-2000}
+#Collector schedule (required)
+gitlab.cron=${GITLAB_CRON:-0 0/5 * * * *}
 
-#In-built folder housing prepared REST queries (required)
-feature.queryFolder=${VERSIONONE_QUERY_FOLDER:-v1api-queries}
+#Gitlab host (optional, defaults to "gitlab.com")
+gitlab.host=${GITLAB_HOST:-}
 
-#Jira API Query file names (String template requires the files to have .st extension) (required)
-feature.storyQuery=${VERSIONONE_STORY_QUERY:-story}
-feature.epicQuery=${VERSIONONE_EPIC_QUERY:-epicinfo}
-feature.projectQuery=${VERSIONONE_PROJECT_QUERY:-projectinfo}
-feature.memberQuery=${VERSIONONE_MEMBBER_QUERY:-memberinfo}
-feature.sprintQuery=${VERSIONONE_SPRINT_QUERY:-sprintinfo}
-feature.teamQuery=${VERSIONONE_TEAM_QUERY:-teaminfo}
-feature.trendingQuery${{VERSIONONE_TRENDING_QUERY:-trendinginfo}
+#Gitlab protocol (optional, defaults to "http")
+gitlab.protocol=${GITLAB_PROTOCOL:-}
 
-# Trending Query:  Number of days in a sprint (not-required)
-feature.sprintDays=${VERSIONONE_SPRINT_DAYS:-60}
-# Trending Query:  Length of sprint week (not-required)
-feature.sprintEndPrior=${VERSIONONE_SPRINT_END_PRIOR:-7}
-
-#Scheduled Job prior minutes to recover data created during execution time (usually, 2 minutes is enough)
-feature.scheduledPriorMin=${VERSIONONE_SCHEDULED_PRIOR_MIN:-2}
-
-#Delta change date that modulates the collector item task - should be about as far back as possible, in ISO format (required)
-feature.deltaCollectorItemStartDate=${VERSIONONE_DELTA_COLLECTORITEM_START_DATE:-2008-01-01T00:00:00.000000}
-
-#VersionOne Connection Details
-#Proxy assumes a host:port syntax
-feature.versionOneProxyUrl=${VERSIONONE_PROXY_URL:-""}
-feature.versionOneBaseUri=${VERSIONONE_URL:-https://www.versionone.com/our-company-instance/}
-#Access token provided by VersionOne
-feature.versionOneAccessToken=${VERSIONONE_ACCESS_TOKEN:-accessToken}
-
-#Start dates from which to begin collector data, if no other data is present - usually, a month back is appropriate (required)
-feature.deltaStartDate=${VERSIONONE_DELTA_START_DATE:-2015-03-01T00:00:00.000000}
-feature.masterStartDate=${VERSIONONE_MASTER_START_DATE:-2008-01-01T00:00:00.000000}
+#Gitlab port (optional, defaults to protocol default port)
+gitlab.port=${GITLAB_PORT:-}
+  
+#Gitlab API Token (required, must be an admin account to retrieve all teams for the instance of gitlab.  If not admin, will only retrieve teams the user belongs to)
+gitlab.apiToken=${GITLAB_API_TOKEN:-}
 
 EOF
 
