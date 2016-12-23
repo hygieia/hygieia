8108d696bf05307a099043a8104937f0be169bee
Brennan Larson <brennan.larson.ffxs@statefarm.com>
2016-12-23 12:25:36 -0700

    removed auth scheme extractor

--- api/src/main/java/com/capitalone/dashboard/service/CustomAuthenticationProvider.java
+++ api/src/main/java/com/capitalone/dashboard/service/CustomAuthenticationProvider.java
@@ -1,7 +1,5 @@
 package com.capitalone.dashboard.service;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -10,26 +8,21 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Component;
 
 import com.capitalone.dashboard.auth.AuthenticationScheme;
-import com.capitalone.dashboard.auth.AuthenticationSchemeExtractor;
 import com.capitalone.dashboard.auth.AuthenticationServiceFactory;
 
 @Component
 public class CustomAuthenticationProvider implements AuthenticationProvider {
 
 	private AuthenticationServiceFactory authenticationServiceFactory;
-	private AuthenticationSchemeExtractor authenticationSchemeExtractor;
 
 	@Autowired
-	public CustomAuthenticationProvider(AuthenticationServiceFactory authenticationServiceFactory,
-										AuthenticationSchemeExtractor authenticationSchemeExtractor) {
+	public CustomAuthenticationProvider(AuthenticationServiceFactory authenticationServiceFactory) {
 		this.authenticationServiceFactory = authenticationServiceFactory;
-		this.authenticationSchemeExtractor = authenticationSchemeExtractor;
 	}
 
 	@Override
 	public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-		AuthenticationScheme scheme = authenticationSchemeExtractor.extract((HttpServletRequest) authentication.getDetails());
-		AuthenticationService authenticationService = authenticationServiceFactory.getAuthenticationService(scheme);
+		AuthenticationService authenticationService = authenticationServiceFactory.getAuthenticationService((AuthenticationScheme) authentication.getDetails());
 		return authenticationService.authenticate(authentication.getName(), authentication.getCredentials().toString());
 	}
 
