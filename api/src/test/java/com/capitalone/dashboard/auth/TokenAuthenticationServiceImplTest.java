a0fc58b4613a51687552bb92aa3869b8b6071859
Brennan Larson <brennan.larson.ffxs@statefarm.com>
2017-01-10 08:38:54 -0700

    refactored secret and exp time out of service, and created default values if none are provided

--- api/src/test/java/com/capitalone/dashboard/auth/TokenAuthenticationServiceImplTest.java
+++ api/src/test/java/com/capitalone/dashboard/auth/TokenAuthenticationServiceImplTest.java
@@ -6,7 +6,6 @@ import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Matchers.eq;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -16,18 +15,13 @@ import javax.servlet.http.HttpServletResponse;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.util.ReflectionTestUtils;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = TokenAuthenticationServiceImpl.class)
-@TestPropertySource(locations="classpath:jwt.properties")
+@RunWith(MockitoJUnitRunner.class)
 public class TokenAuthenticationServiceImplTest {
 
 	private static final String USERNAME = "username";
@@ -35,23 +29,23 @@ public class TokenAuthenticationServiceImplTest {
 	private static final String AUTH_PREFIX_W_SPACE = "Bearer ";
 	private static final String AUTH_RESPONSE_HEADER = "X-Authentication-Token";
 	
-	@Autowired
-	private TokenAuthenticationService service;
+	private TokenAuthenticationServiceImpl service;
 	
+	private TokenAuthProperties tokenAuthProperties;
+	
+	@Mock
 	private HttpServletResponse response;
-	private HttpServletRequest request;
 	
-	private String secret;
-	private long expirationTime;
+	@Mock
+	private HttpServletRequest request;
 	
 	@Before
 	public void setup() {
+		tokenAuthProperties = new TokenAuthProperties();
+		tokenAuthProperties.setExpirationTime(100000L);
+		tokenAuthProperties.setSecret("somesupersecretphrase");
+		service = new TokenAuthenticationServiceImpl(tokenAuthProperties);
 		SecurityContextHolder.clearContext();
-		response = mock(HttpServletResponse.class);
-		request = mock(HttpServletRequest.class);
-		
-		secret = (String) ReflectionTestUtils.getField(service, TokenAuthenticationServiceImpl.class, "secret");
-		expirationTime = (long) ReflectionTestUtils.getField(service, TokenAuthenticationServiceImpl.class, "expirationTime");
 	}
 	
 	@Test
@@ -68,13 +62,13 @@ public class TokenAuthenticationServiceImplTest {
 	
 	@Test
 	public void testGetAuthentication_expiredToken() {
-		when(request.getHeader(AUTHORIZATION)).thenReturn(AUTH_PREFIX_W_SPACE + AuthenticationFixture.getJwtToken(USERNAME, secret, 0));
+		when(request.getHeader(AUTHORIZATION)).thenReturn(AUTH_PREFIX_W_SPACE + AuthenticationFixture.getJwtToken(USERNAME, tokenAuthProperties.getSecret(), 0));
 		assertNull(service.getAuthentication(request));
 	}
 	
 	@Test
 	public void testGetAuthentication() {
-		when(request.getHeader(AUTHORIZATION)).thenReturn(AUTH_PREFIX_W_SPACE + AuthenticationFixture.getJwtToken(USERNAME, secret, expirationTime));
+		when(request.getHeader(AUTHORIZATION)).thenReturn(AUTH_PREFIX_W_SPACE + AuthenticationFixture.getJwtToken(USERNAME, tokenAuthProperties.getSecret(), tokenAuthProperties.getExpirationTime()));
 		Authentication result = service.getAuthentication(request);
 		
 		assertNotNull(result);
