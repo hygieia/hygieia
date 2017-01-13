d9fdd4e892ed1061825c2c4f381e5cd7030f445c
Rob Miller <rob.miller.deiw@statefarm.com>
2017-01-13 12:34:00 -0700

    added some documentation around how to add a new filter

--- api/src/main/java/com/capitalone/dashboard/auth/AuthenticationFilter.java
+++ api/src/main/java/com/capitalone/dashboard/auth/AuthenticationFilter.java
@@ -13,6 +13,11 @@ import org.springframework.security.web.util.matcher.RequestMatcher;
 
 import com.capitalone.dashboard.model.AuthType;
 
+/**
+ * This class should be extended for any other forms of authentication required.
+ * The AuthenticationFilter class handles retrieving application specific information
+ * and turning it all into a token which it puts in the response.
+ */
 public abstract class AuthenticationFilter extends AbstractAuthenticationProcessingFilter {
 	
 	private final SecurityService securityService;
@@ -22,12 +27,20 @@ public abstract class AuthenticationFilter extends AbstractAuthenticationProcess
 		this.securityService = securityService;
 	}
 
+	/**
+	 * On a successful authentication, this method will inflate your authentication object with application specific roles,
+	 * and turn them into a token to put in the response.
+	 */
 	@Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication)
     throws IOException, ServletException {
 		securityService.inflateResponse(response, authentication, getAuthType());
     }
 	
+	/**
+	 * When adding new authentication types, add it to the @AuthType enum, and return that auth type from your authentication filter.
+	 * @return The type of authentication this filter uses.
+	 */
 	public abstract AuthType getAuthType();
 	
 }
