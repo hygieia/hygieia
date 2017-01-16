4798deda878e8c223a6a2fe32b40cb8e310539bf
satishc1 <satish.ramabhadran@capitalone.com>
2017-01-16 10:20:45 -0500

    Fix for dashboard issues in Internet Explorer Windows. (#1092)
    * Fix for issues 852. MongoDB replicaset support readme.
    
    * Fix sprintIndicator check.
    
    * Fix auth for recursive get for PR-1038
    
    * Handle cases where changeSet is null.
    
    * Handle cases where changeSet is null.
    
    * Fix for dashboard issues in Internet Explorer Windows.

--- UI/src/app/chartist/plugins/gridBoundaries.js
+++ UI/src/app/chartist/plugins/gridBoundaries.js
@@ -13,7 +13,13 @@
             chart.on('draw', function(data) {
                 // remove any elements if showGrid is still on
                 if(data.type == 'grid') {
-                    data.element._node.remove();
+                    if(typeof data.element._node.remove=='function') {
+                        data.element._node.remove();
+                    } else {
+                        while(data.element._node.hasChildNodes()) {
+                            data.element._node.removeChild(_node.firstChild);
+                        }
+                    }
                 }
             });
 
