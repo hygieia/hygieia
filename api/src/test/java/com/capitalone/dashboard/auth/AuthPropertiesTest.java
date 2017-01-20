c05e13b9fb80160fb4841fee3027d607cd563930
Rob Miller <rob.miller.deiw@statefarm.com>
2017-01-20 10:14:14 -0700

    updated tests

--- api/src/test/java/com/capitalone/dashboard/auth/AuthPropertiesTest.java
+++ api/src/test/java/com/capitalone/dashboard/auth/AuthPropertiesTest.java
@@ -30,11 +30,15 @@ public class AuthPropertiesTest {
 	public void testApplyDefaultsNotNeeded() {
 		tokenAuthProperties.setExpirationTime(8L);
 		tokenAuthProperties.setSecret("secret");
+		tokenAuthProperties.setLdapServerUrl("url");
+		tokenAuthProperties.setLdapUserDnPattern("pattern");
 		
 		tokenAuthProperties.applyDefaultsIfNeeded();
 		
 		assertEquals(Long.valueOf(8), tokenAuthProperties.getExpirationTime());
 		assertEquals("secret", tokenAuthProperties.getSecret());
+		assertEquals("url", tokenAuthProperties.getLdapServerUrl());
+		assertEquals("pattern", tokenAuthProperties.getLdapUserDnPattern());
 	}
 
 }
