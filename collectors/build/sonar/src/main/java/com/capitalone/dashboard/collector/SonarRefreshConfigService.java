947d63a66d438182901eec8b3a8f220bf5fc5194
Mae Anne Large <mae.a.large.rm44@statefarm.com>
2017-01-27 14:30:02 -0700

    added component scan annotation on  SonarRefreshConfigService

--- collectors/build/sonar/src/main/java/com/capitalone/dashboard/collector/SonarRefreshConfigService.java
+++ collectors/build/sonar/src/main/java/com/capitalone/dashboard/collector/SonarRefreshConfigService.java
@@ -3,7 +3,9 @@ package com.capitalone.dashboard.collector;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.endpoint.RefreshEndpoint;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
+@Component
 public class SonarRefreshConfigService {
 	@Autowired
 	private RefreshEndpoint refreshEndpoint;
