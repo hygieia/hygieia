810308d4312656abc7280a93e14c79632f65723d
Mae Anne Large <mae.a.large.rm44@statefarm.com>
2017-01-28 06:14:47 -0700

    added  a mocked repo for test

--- collector-config-server/src/test/java/com/capitalone/dashboard/ConfigServiceApplicationTest.java
+++ collector-config-server/src/test/java/com/capitalone/dashboard/ConfigServiceApplicationTest.java
@@ -3,10 +3,12 @@ package com.capitalone.dashboard;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(ConfigServiceApplication.class)
+@TestPropertySource(locations = {"classpath:test.properties"})
 public class ConfigServiceApplicationTest {
 
 	@Test
