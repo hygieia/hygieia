c7d89f5f8270de2e096b5182fb76aaf627b11b0f
nireeshT <nireesh.thiruveedula@capitalone.com>
2017-02-08 16:17:58 -0500

    Changes to PR1020 - Configurable Pipeline Stage Names

--- UI/src/components/widgets/pipeline/config.js
+++ UI/src/components/widgets/pipeline/config.js
@@ -5,29 +5,35 @@
         .module(HygieiaConfig.module)
         .controller('pipelineConfigController', pipelineConfigController);
 
-    pipelineConfigController.$inject = ['modalData', 'deployData', 'systemConfigData', '$modalInstance', '$q'];
-    function pipelineConfigController(modalData, deployData, systemConfigData, $modalInstance, $q) {
+    pipelineConfigController.$inject = ['modalData', 'deployData', 'systemConfigData', '$modalInstance', '$q','$scope'];
+    function pipelineConfigController(modalData, deployData, systemConfigData, $modalInstance, $q,$scope) {
         /*jshint validthis:true */
         var ctrl = this;
 
         // make sure mappings property is available
         ctrl.environmentsDropdownDisabled = true;
         ctrl.environmentMappings = [ ];
+        ctrl.saveDisabled = false;
+        ctrl.saveDisabledDropDown = false;
+        ctrl.radioValue =[];
 
         ctrl.save = save;
+        ctrl.deleteMapping = deleteMapping;
+        ctrl.addMapping = addMapping;
+        ctrl.validateStage = validateStage;
+        ctrl.validateDropDown = validateDropDown;
 
         $q.all([systemConfigData.config(), deployData.details(modalData.dashboard.application.components[0].id)]).then(processResponse);
 
         function processResponse(dataA) {
-        	var systemConfig = dataA[0];
-        	var data = dataA[1];
-        	
-        	ctrl.environmentMappings = _(systemConfig.systemStages)
-        		.filter(function (stage) { return stage.type == 'DEPLOY' })
-	        	.map(function (stage) {
-	        		return { key: stage.name.toLowerCase(), value: null }
-	        	}).value();
-        	
+            var systemConfig = dataA[0];
+            var data = dataA[1];
+
+            for(var x in modalData.widgetConfig.options.mappings) {
+                var envName = modalData.widgetConfig.options.mappings[x];
+                ctrl.environmentMappings.push({key: x , value: envName});
+            }
+
             if(modalData.widgetConfig.options.mappings) {
                 _(ctrl.environmentMappings).forEach(function(env) {
                     if(modalData.widgetConfig.options.mappings[env.key]) {
@@ -35,7 +41,7 @@
                     }
                 });
             }
-        	
+
             ctrl.environments = _(data.result).map(function (env) {
                 return {
                     name: env.name,
@@ -51,17 +57,118 @@
                     ctrl.mappings[x] = envName;
                 }
             }
-
-
+            ctrl.radioValue = modalData.widgetConfig.options.prod;
             ctrl.environmentsDropdownDisabled = false;
         }
 
-        function save() {
-            modalData.widgetConfig.name = 'pipeline';
-            modalData.widgetConfig.options.mappings = ctrl.mappings;
-
-            var postObj = angular.copy(modalData.widgetConfig);
-            $modalInstance.close(postObj);
+        function save(form) {
+            if(form.$valid){
+                modalData.widgetConfig.name = 'pipeline';
+                ctrl.mappings = editMappings(ctrl.radioValue);
+                modalData.widgetConfig.options.prod = ctrl.radioValue;
+                modalData.widgetConfig.options.mappings = ctrl.mappings;
+                var postObj = angular.copy(modalData.widgetConfig);
+                $modalInstance.close(postObj);
+            }
         }
+
+        function editMappings(radio){
+            var mappingsTemp ={};
+            _(ctrl.environmentMappings).forEach(function (env) {
+                if( env.key != radio){
+                    mappingsTemp[env.key] =ctrl.mappings[env.key];
+                }
+            });
+            mappingsTemp[radio] = ctrl.mappings[radio];
+            return mappingsTemp;
+        }
+
+        function addMapping() {
+            var newItemNo = ctrl.environmentMappings.length + 1;
+            ctrl.radioValue ='';
+            ctrl.environmentMappings.push({key: 'Env' + newItemNo, value: null});
+        }
+
+        function deleteMapping(item) {
+            var index = ctrl.environmentMappings.indexOf(item);
+            ctrl.environmentMappings.splice(index, 1);
+            if (item.key == modalData.widgetConfig.options.prod) {
+                ctrl.radioValue = '';
+            }
+        }
+
+        function validateStage() {
+            var sortedMap = ctrl.environmentMappings.concat().sort(function (a, b) {
+                if (a.key > b.key) return 1;
+                if (a.key < b.key) return -1;
+                return 0;
+            });
+
+            var map = find_duplicates(sortedMap, false);
+
+            _(sortedMap).forEach(function (item) {
+                item.isDuplicate = false;
+            });
+
+            _(map).forEach(function (env) {
+                for (var i = 0; i < env.length; i++) {
+                    sortedMap[env[i]].isDuplicate = true;
+                }
+            });
+            for(var i=0;i<sortedMap.length;i++){
+                ctrl.saveDisabled = sortedMap[i].isDuplicate;
+                if(ctrl.saveDisabled) break;
+            }
+
+        }
+
+        function validateDropDown() {
+            var sortedMap;
+            sortedMap=ctrl.environmentMappings.concat().sort(function (a, b) {
+                if (a.key > b.key) return 1;
+                if (a.key < b.key) return -1;
+                return 0;
+            });
+
+            _(sortedMap).forEach(function (item) {
+                item.isDuplicateDropDown = false;
+            });
+
+            var map = find_duplicates(sortedMap, true);
+            _(map).forEach(function (env) {
+                for (var i = 0; i < env.length; i++) {
+                    sortedMap[env[i]].isDuplicateDropDown = true;
+                }
+            });
+            for(var i=0;i<sortedMap.length;i++){
+                ctrl.saveDisabledDropDown = sortedMap[i].isDuplicateDropDown;
+                if(ctrl.saveDisabledDropDown ) break;
+            }
+
+        }
+
+        function find_duplicates(sorted, value) {
+            var map = {};
+            var obj;
+            for (var i=0;i<sorted.length;i++) {
+                if(value){
+                    obj=ctrl.mappings[sorted[i].key];
+                }else{
+                    obj=sorted[i].key.toUpperCase();
+                }
+                if(!map[obj]){
+                    map[obj]=[i];
+                }else{
+                    map[obj].push(i);
+                }
+            }
+            for(var obj in map){
+                if(map[obj].length === 1){
+                    delete map[obj];
+                }
+            }
+            return map;
+        }
+
     }
 })();
