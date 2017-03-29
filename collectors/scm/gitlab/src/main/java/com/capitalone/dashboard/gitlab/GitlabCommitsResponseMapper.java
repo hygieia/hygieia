0d48535d960eb08f4f8fed966c88ce6bcfca1e87
Rob Miller <rob.miller.deiw@statefarm.com>
2017-03-29 10:36:55 -0700

    pulled method out to make cleaner

--- collectors/scm/gitlab/src/main/java/com/capitalone/dashboard/gitlab/GitlabCommitsResponseMapper.java
+++ collectors/scm/gitlab/src/main/java/com/capitalone/dashboard/gitlab/GitlabCommitsResponseMapper.java
@@ -17,25 +17,28 @@ public class GitlabCommitsResponseMapper {
     public List<Commit> map(GitlabCommit[] gitlabCommits, String repoUrl, String branch) {
         List<Commit> commits = new ArrayList<>();
         for (GitlabCommit gitlabCommit : gitlabCommits) {
-            long timestamp = new DateTime(gitlabCommit.getCreatedAt()).getMillis();
-            CommitType commitType = CollectionUtils.isEmpty(gitlabCommit.getParentIds()) ? CommitType.New : CommitType.Merge;
-            
-            Commit commit = new Commit();
-            commit.setTimestamp(System.currentTimeMillis());
-            commit.setScmUrl(repoUrl);
-            commit.setScmBranch(branch);
-            commit.setScmRevisionNumber(gitlabCommit.getId());
-            commit.setScmAuthor(gitlabCommit.getAuthorName());
-            commit.setScmCommitLog(gitlabCommit.getMessage());
-            commit.setScmCommitTimestamp(timestamp);
-            commit.setNumberOfChanges(1);
-            commit.setScmParentRevisionNumbers(gitlabCommit.getParentIds());
-            commit.setType(commitType);
-            
-            commits.add(commit);
+            commits.add(map(repoUrl, branch, gitlabCommit));     
         }
         
         return commits;
+    }
+
+    private Commit map(String repoUrl, String branch, GitlabCommit gitlabCommit) {
+        long timestamp = new DateTime(gitlabCommit.getCreatedAt()).getMillis();
+        CommitType commitType = CollectionUtils.isEmpty(gitlabCommit.getParentIds()) ? CommitType.New : CommitType.Merge;
+        
+        Commit commit = new Commit();
+        commit.setTimestamp(System.currentTimeMillis());
+        commit.setScmUrl(repoUrl);
+        commit.setScmBranch(branch);
+        commit.setScmRevisionNumber(gitlabCommit.getId());
+        commit.setScmAuthor(gitlabCommit.getAuthorName());
+        commit.setScmCommitLog(gitlabCommit.getMessage());
+        commit.setScmCommitTimestamp(timestamp);
+        commit.setNumberOfChanges(1);
+        commit.setScmParentRevisionNumbers(gitlabCommit.getParentIds());
+        commit.setType(commitType);
+        return commit;
     }
 
     
