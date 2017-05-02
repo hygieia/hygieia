7360b70f9c85c31202067d5a26205892dcdd985c
tabladrum <vennycb6w>
2017-05-02 10:41:51 -0400

    Renamed variable.

--- collectors/build/sonar/src/main/java/com/capitalone/dashboard/collector/SonarClientSelector.java
+++ collectors/build/sonar/src/main/java/com/capitalone/dashboard/collector/SonarClientSelector.java
@@ -7,16 +7,16 @@ import org.springframework.stereotype.Component;
 @Component
 public class SonarClientSelector {
 
-    private final DefaultSonar6Client sonar54Client;
+    private final DefaultSonar6Client sonar6Client;
     private final DefaultSonarClient sonarClient;
 
     @Autowired
-    public SonarClientSelector(DefaultSonar6Client sonar54Client, DefaultSonarClient sonarClient) {
-        this.sonar54Client = sonar54Client;
+    public SonarClientSelector(DefaultSonar6Client sonar6Client, DefaultSonarClient sonarClient) {
+        this.sonar6Client = sonar6Client;
         this.sonarClient = sonarClient;
     }
 
     public SonarClient getSonarClient(Double version) {
-        return ((version == null) || (version < 6.0)) ? sonarClient : sonar54Client;
+        return ((version == null) || (version < 6.0)) ? sonarClient : sonar6Client;
     }
 }
