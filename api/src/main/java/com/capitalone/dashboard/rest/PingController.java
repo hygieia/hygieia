51a2ba567e3dc5d90cb0781ee7505b9f9aba0f43
Rob Miller <rob.miller.deiw@statefarm.com>
2017-05-03 12:13:52 -0700

    put ping controller back

--- api/src/main/java/com/capitalone/dashboard/rest/PingController.java
+++ api/src/main/java/com/capitalone/dashboard/rest/PingController.java
@@ -1,14 +1,14 @@
 package com.capitalone.dashboard.rest;
 
-import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
-
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @PropertySource(ignoreResourceNotFound=true,value= "classpath:version.properties")
 
@@ -18,7 +18,10 @@ public class PingController {
     @Value("${version.number}")
     private String versionNumber;
 
-    
+    public PingController() {
+
+    }
+
     @RequestMapping(value = "/ping", method = GET, produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<Boolean> ping () {
         return ResponseEntity
@@ -31,5 +34,4 @@ public class PingController {
         String appVersion=versionNumber;
         return ResponseEntity.status(HttpStatus.OK).body(appVersion);
     }
-
 }
