7157e28c1b737a736ff5ae5010e230a839f5a008
cpurna1 <cpurna1@gmail.com>
2017-06-06 14:12:01 -0500

    Created Maturity Score in Product Dashboard (#1438)
    * created maturity score
    
    * fixed maturity acore API error
    
    * fixed maturityModel.java
    
    * CICD 16Gates - Testcase for MaturityModelService
    
    * CICD 16Gates - Testcase for MaturityModelController
    
    * CICD 16Gates - Testcase for MaturityModelController

--- api/src/test/java/com/capitalone/dashboard/config/TestConfig.java
+++ api/src/test/java/com/capitalone/dashboard/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.capitalone.dashboard.config;
 
 import com.capitalone.dashboard.service.ApiTokenService;
 import com.capitalone.dashboard.service.LibraryPolicyService;
+import com.capitalone.dashboard.service.MaturityModelService;
 import org.mockito.Mockito;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -42,7 +43,7 @@ public class TestConfig {
 	public AuthenticationService authenticationService() {
 		return Mockito.mock(AuthenticationService.class);
 	}
-	
+
 	@Bean
 	public AuthenticationResponseService authenticationResponseService() {
 		return Mockito.mock(AuthenticationResponseService.class);
@@ -102,7 +103,7 @@ public class TestConfig {
 	public ScopeService scopeService() {
 		return Mockito.mock(ScopeService.class);
 	}
-	
+
 	@Bean
 	public EncryptionService encryptionService() {
 		return Mockito.mock(EncryptionService.class);
@@ -117,7 +118,7 @@ public class TestConfig {
 	public PipelineService pipelineService() {
 		return Mockito.mock(PipelineService.class);
 	}
-	
+
 	@Bean
     public CloudInstanceService cloudInstanceService() {
         return Mockito.mock(CloudInstanceService.class);
@@ -137,7 +138,7 @@ public class TestConfig {
     public CloudVolumeService cloudVolumeService() {
         return Mockito.mock(CloudVolumeService.class);
     }
-    
+
     @Bean
     public PaginationHeaderUtility paginationHeaderUtility() {
     	return Mockito.mock(PaginationHeaderUtility.class);
@@ -151,6 +152,11 @@ public class TestConfig {
 	@Bean
 	public LibraryPolicyService libraryPolicyService() {
 		return Mockito.mock(LibraryPolicyService.class);
+	}
+
+	@Bean
+	public MaturityModelService maturityModelService() {
+		return Mockito.mock(MaturityModelService.class);
 	}
 
 	@Bean
