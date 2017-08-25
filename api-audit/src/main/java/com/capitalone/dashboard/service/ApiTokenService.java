b8b74792ff30b1b02ff9fd7e3783e093a3d9d6fe
Sbrenthughes <stephen.hughes@capitalone.com>
2017-08-25 13:18:03 -0400

    added ability to edit api token exp date and remove tokens (#1667)

--- api-audit/src/main/java/com/capitalone/dashboard/service/ApiTokenService.java
+++ api-audit/src/main/java/com/capitalone/dashboard/service/ApiTokenService.java
@@ -3,6 +3,7 @@ package com.capitalone.dashboard.service;
 import com.capitalone.dashboard.misc.HygieiaException;
 import com.capitalone.dashboard.model.ApiToken;
 import com.capitalone.dashboard.util.EncryptionException;
+import org.bson.types.ObjectId;
 
 import java.util.Collection;
 
@@ -10,4 +11,19 @@ public interface ApiTokenService {
     Collection<ApiToken> getApiTokens();
     String getApiToken(String apiUser, Long expirationDt) throws EncryptionException, HygieiaException;
     org.springframework.security.core.Authentication authenticate(String username, String password);
+    /**
+     * Deletes an existing Token .
+     *
+     * @param id unique identifier of Token to delete
+     */
+    void deleteToken(ObjectId id);
+
+    /**
+     * Updates expiration date of given token
+     * @param expirationDt
+     * @param id
+     * @return
+     * @throws HygieiaException
+     */
+    String updateToken(Long expirationDt, ObjectId id) throws HygieiaException;
 }
