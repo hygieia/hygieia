f419847204592e4f8df803226830932605a789db
MarkRx <MarkRx@users.noreply.github.com>
2017-09-05 11:05:21 -0400

    Update appd docker configs

--- collectors/performance/appdynamics/docker/appdynamics-properties-builder.sh
+++ collectors/performance/appdynamics/docker/appdynamics-properties-builder.sh
@@ -5,18 +5,32 @@ if [ "$SKIP_PROPERTIES_BUILDER" = true ]; then
   exit 0
 fi
 
-# if we are linked, use that info
-if [ "$MONGO_STARTED" != "" ]; then
-  # links now use hostnames
-  # todo: retrieve linked information such as hostname and port exposition
-  export SPRING_DATA_MONGODB_HOST=mongodb
-  export SPRING_DATA_MONGODB_PORT=27017
+# mongo container provides the HOST/PORT
+# api container provided DB Name, ID & PWD
+
+if [ "$TEST_SCRIPT" != "" ]
+then
+        #for testing locally
+        PROP_FILE=application.properties
+else 
+	PROP_FILE=config/hygieia-appdynamics-performance-collector.properties
+fi
+  
+if [ "$MONGO_PORT" != "" ]; then
+	# Sample: MONGO_PORT=tcp://172.17.0.20:27017
+	MONGODB_HOST=`echo $MONGO_PORT|sed 's;.*://\([^:]*\):\(.*\);\1;'`
+	MONGODB_PORT=`echo $MONGO_PORT|sed 's;.*://\([^:]*\):\(.*\);\2;'`
+else
+	env
+	echo "ERROR: MONGO_PORT not defined"
+	exit 1
 fi
 
-echo "SPRING_DATA_MONGODB_HOST: $SPRING_DATA_MONGODB_HOST"
-echo "SPRING_DATA_MONGODB_PORT: $SPRING_DATA_MONGODB_PORT"
+echo "MONGODB_HOST: $MONGODB_HOST"
+echo "MONGODB_PORT: $MONGODB_PORT"
 
-cat > appdynamics.properties <<EOF
+
+cat > $PROP_FILE <<EOF
 #Database Name - default is test
 database=${SPRING_DATA_MONGODB_DATABASE:-dashboard}
 
@@ -24,13 +38,13 @@ database=${SPRING_DATA_MONGODB_DATABASE:-dashboard}
 dbhost=${SPRING_DATA_MONGODB_HOST:-10.0.1.1}
 
 #Database Port - default is 27017
-dbport=${SPRING_DATA_MONGODB_PORT:-9999}
+dbport=${SPRING_DATA_MONGODB_PORT:-27017}
 
 #Database Username - default is blank
-dbusername=${SPRING_DATA_MONGODB_USERNAME:-db}
+dbusername=${HYGIEIA_API_ENV_SPRING_DATA_MONGODB_USERNAME:-db}
 
 #Database Password - default is blank
-dbpassword=${SPRING_DATA_MONGODB_PASSWORD:-dbpass}
+dbpassword=${HYGIEIA_API_ENV_SPRING_DATA_MONGODB_PASSWORD:-dbpass}
 
 #Logging File
 logging.file=${APPDYNAMICS_LOGFILE:-./logs/appd-collector.log}
@@ -39,15 +53,12 @@ logging.file=${APPDYNAMICS_LOGFILE:-./logs/appd-collector.log}
 appdynamics.cron=${APPDYNAMICS_CRON:-1 * * * * *}
 
 #Appdynamics server (required)
-appdynamics.instanceUrl=${APPDYNAMICS_INSTANCE_URL:-http://appdynamics}
+appdynamics.instanceUrls=${APPDYNAMICS_INSTANCE_URLS:-http://appdynamics}
 
 #Appdynamics Username (required)
 appdynamics.username=${APPDYNAMICS_USERNAME} # (if multi-tenancy APPD_USERNAME@TENANT)
 
 #Appdynamics Password (required)
 appdynamics.password=${APPDYNAMICS_PASSWORD}
-
-#Appdynamics Dashboard (required)
-appdynamics.dashboardUrl=${APPDYNAMICS_DASHBOARD_URL:-'http://appdynamics/controller/#/location=APP_DASHBOARD&timeRange=last_15_minutes.BEFORE_NOW.-1.-1.15&application=%s&dashboardMode=force'}
 
 EOF
