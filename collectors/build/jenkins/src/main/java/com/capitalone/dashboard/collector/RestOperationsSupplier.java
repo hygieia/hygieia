14b8ba03bfbe7f7f5019455f8bf39702060b514a
Bruce <wdquan1985@sina.com>
2017-09-27 11:00:17 +0800

    Support configuring a proxy to access the jenkins server, and please add a property jenkins.proxy= in the application.properties file.

--- collectors/build/jenkins/src/main/java/com/capitalone/dashboard/collector/RestOperationsSupplier.java
+++ collectors/build/jenkins/src/main/java/com/capitalone/dashboard/collector/RestOperationsSupplier.java
@@ -1,6 +1,14 @@
 package com.capitalone.dashboard.collector;
 
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.Proxy.Type;
+
 import com.capitalone.dashboard.util.Supplier;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestOperations;
 import org.springframework.web.client.RestTemplate;
@@ -10,8 +18,29 @@ import org.springframework.web.client.RestTemplate;
  */
 @Component
 public class RestOperationsSupplier implements Supplier<RestOperations> {
+	
+	private final HudsonSettings settings;
+	
+    @Autowired
+    public RestOperationsSupplier(HudsonSettings settings) {
+        this.settings = settings;
+    }
+    
     @Override
     public RestOperations get() {
-        return new RestTemplate();
+        if (StringUtils.isNotEmpty(this.settings.getProxy())) {
+        	String proxy = this.settings.getProxy();
+        	String[] proxyArray = proxy.split(":");
+        	SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+            
+            if (proxyArray.length == 1) {
+            	requestFactory.setProxy(new Proxy(Type.HTTP, new InetSocketAddress(proxyArray[0], 80)));
+			}else {
+				requestFactory.setProxy(new Proxy(Type.HTTP, new InetSocketAddress(proxyArray[0], Integer.parseInt(proxyArray[1]))));
+			}
+            return new RestTemplate(requestFactory);
+		}else {
+			return new RestTemplate();
+		}
     }
 }
