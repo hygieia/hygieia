d598dfcf8599fe2cebb2ff5b6e461e2a780cb5bb
satishc1 <satish.ramabhadran@capitalone.com>
2017-09-29 13:38:21 -0400

    Add ability to make authenticated calls using github personal access token.

--- collectors/scm/github/docker/github-properties-builder.sh
+++ collectors/scm/github/docker/github-properties-builder.sh
@@ -56,6 +56,13 @@ github.commitThresholdDays=${GITHUB_COMMIT_THRESHOLD_DAYS:-15}
 
 #Optional: Error threshold count after which collector stops collecting for a collector item. Default is 2.
 github.errorThreshold=${GITHUB_ERROR_THRESHOLD:-1}
+
+#This is the key generated using the Encryption class in core
+github.key=${GITHUB_KEY}
+
+#personal access token generated from github and used for making authentiated calls
+github.personalAccessToken=${PERSONAL_ACCESS_TOKEN}
+
 EOF
 
 echo "
