5d40fcc6c8dd66f21cd3a1c50bfdb868b627df10
Varun Mukka <kumarvarun.1252@gmail.com>
2017-09-29 14:43:33 -0700

    updating sauce credentials

--- UI-protractor-tests/protractor.conf.js
+++ UI-protractor-tests/protractor.conf.js
@@ -3,6 +3,14 @@ exports.config = {
 
     baseUrl: 'https://hygieia-qa.cloud.capitalone.com/',
 
+    sauceUser:'emb235',
+
+    sauceKey:'1e13701e-f9d2-4482-ba1c-fc592a863721',
+
+    sauceProxy:  'http://proxy.kdc.capitalone.com:8099',
+
+    webDriverProxy: 'http://proxy.kdc.capitalone.com:8099',
+
     allScriptsTimeout: 110000,
 
     disableChecks: true,
