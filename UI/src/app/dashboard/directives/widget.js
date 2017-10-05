d6683ed500fc2d757d25eef9ed80808038e54dcb
stephenhughes89 <stephen.hughes@capitalone.com>
2017-10-05 10:31:55 -0400

    Github widget now shows time when its collector item was updated

--- UI/src/app/dashboard/directives/widget.js
+++ UI/src/app/dashboard/directives/widget.js
@@ -245,7 +245,7 @@
                         var lastUpdated = angular.isArray(result) ? _.max(result) : result;
                         var collectorItems = result.collectorItem;
                         if(typeof lastUpdated === 'object'){
-                            lastUpdated = lastUpdated.lastUpdated;
+                            lastUpdated = lastUpdated.collectorItem[0].lastUpdated;
                         }
                         $scope.lastUpdatedDisplay = moment(lastUpdated).dash('ago');
                         $scope.collectorItems = collectorItems;
