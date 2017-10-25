ad9a5f89db4e145c8f6ab834fee2409f316eee4d
Varun Mukka <kumarvarun.1252@gmail.com>
2017-10-25 13:40:54 -0700

    updating config for jenkins run

--- UI-protractor-tests/protractor.conf.js
+++ UI-protractor-tests/protractor.conf.js
@@ -25,7 +25,8 @@ exports.config = {
     capabilities: {
         browserName: 'chrome',
         chromeOptions: {
-            args: [ "--headless", "--disable-gpu", "--window-size=1600,1200" ]
+            // args: [ "--headless", "--disable-gpu", "--window-size=1600,1200" ]
+            args: [ "--start-maximized" ]
         }
     }
 };
