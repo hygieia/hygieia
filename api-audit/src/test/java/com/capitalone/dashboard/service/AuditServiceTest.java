25b724e602af01ab8c070f8b354642477462642b
nireeshT <nireesh.thiruveedula@capitalone.com>
2017-11-02 10:23:35 -0400

    Audit-API - Match github url (case-insensitive)  (#1809)
    * Server side pagination - Hygieia Landing page
    
    * Server side pagination - Hygieia Landing page
    
    * Server side pagination - Hygieia Landing page
    
    * Set default page size to 10. Wrap text overflow for dashboard title exceeding max width
    
    * Pagination for my dashboards section
    
    * Audit-API - match github url (case-insensitive)
    
    * Audit-API - match github url (case-insensitive)

--- api-audit/src/test/java/com/capitalone/dashboard/service/AuditServiceTest.java
+++ api-audit/src/test/java/com/capitalone/dashboard/service/AuditServiceTest.java
@@ -133,7 +133,7 @@ public class AuditServiceTest {
 
         gitRequests.add(gitRequest);
 
-        when(gitRequestRepository.findByScmUrlAndScmBranchAndCreatedAtGreaterThanEqualAndMergedAtLessThanEqual(
+        when(gitRequestRepository.findByScmUrlIgnoreCaseAndScmBranchIgnoreCaseAndCreatedAtGreaterThanEqualAndMergedAtLessThanEqual(
                 request.getRepo(), request.getBranch(), request.getBeginDate(), request.getEndDate())).thenReturn(gitRequests);
         assertTrue(gitRequests.contains(gitRequest));
 
