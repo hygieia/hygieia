021c5d2fe74b4b0242dc6caab7a120c66284dc54
matthewbeasley <beardsley.ma@gmail.com>
2017-12-19 20:18:37 -0600

    UI conf-builder.sh now has sensible default for API_HOST (#1907)

--- UI/docker/conf-builder.sh
+++ UI/docker/conf-builder.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-
-# if we are linked, use that info
-#linked containers now use hostnames
-export API_HOST=hygieia-api
-export API_PORT=8080
-
-sed s:API_HOST:${API_HOST:-127.0.0.1}: /etc/nginx/conf.d/default.conf.templ |\
+sed s:API_HOST:${API_HOST:-hygieia-api}: /etc/nginx/conf.d/default.conf.templ |\
   sed s:API_PORT:${API_PORT:-8080}: > /etc/nginx/conf.d/default.conf
