5f15489c745f58f5f29e46ef7dd3042641e3b345
matthewbeasley <beardsley.ma@gmail.com>
2018-02-02 19:53:00 -0600

    Docker compose refactor (#1972)
    * squash this commit. stopping to work on something else
    
    * squash this
    
    * refactor of docker-compose.yml
    
    * undoing the UI change
    
    * squash this commit. stopping to work on something else
    
    * squash this
    
    * refactor of docker-compose.yml
    
    * undoing the UI change

--- db/db-setup.sh
+++ db/db-setup.sh
@@ -3,7 +3,7 @@
 # if we are linked, use that info
 # docker-compose uses depends_on, but while building the following will fail since it
 # assumes a hard-dependency on 'mongodb'
-if [ "$MONGO_STARTED" != "" ]; then
+# if [ "$MONGO_STARTED" != "" ]; then
   # Sample: MONGO_PORT=tcp://172.17.0.20:27017
-  mongo mongodb/admin < /tmp/db-setup.js
-fi
+  mongo db/admin < /tmp/db-setup.js
+# fi
