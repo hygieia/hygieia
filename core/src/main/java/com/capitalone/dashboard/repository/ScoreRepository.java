498c1f4655a35c5d296442eead6229702066841c
Siddharth Bhaskar <sbhaskar@walmartlabs.com>
2018-03-02 07:49:39 -0800

    Remove unused import

--- core/src/main/java/com/capitalone/dashboard/repository/ScoreRepository.java
+++ core/src/main/java/com/capitalone/dashboard/repository/ScoreRepository.java
@@ -2,7 +2,6 @@ package com.capitalone.dashboard.repository;
 
 import com.capitalone.dashboard.model.ScoreMetric;
 import org.bson.types.ObjectId;
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.data.repository.CrudRepository;
 
