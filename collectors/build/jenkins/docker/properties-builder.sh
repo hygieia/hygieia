ae6a4e4ccfcb63e77018dc47d6747e7bf6817a37
Sébastien Pennec <sebastien@pennec.ch>
2018-04-03 16:06:28 +0200

    Set DB properties to the correct defaults in all collectors (#2107)
    * Mismatch between Dockerfiles and properties builder script on file names
    
    * default DB names changed to dashboarddb
    
    * default DB usernames changed to dashboarduser
    
    * default DB passwords changed to dbpassword
    
    * revert modif on score dockerfile

--- collectors/build/jenkins/docker/properties-builder.sh
+++ collectors/build/jenkins/docker/properties-builder.sh
@@ -58,7 +58,7 @@ fi
 
 cat > $PROP_FILE <<EOF
 #Database Name
-dbname=${HYGIEIA_API_ENV_SPRING_DATA_MONGODB_DATABASE:-dashboard}
+dbname=${HYGIEIA_API_ENV_SPRING_DATA_MONGODB_DATABASE:-dashboarddb}
 
 #Database HostName - default is localhost
 dbhost=${MONGODB_HOST:-10.0.1.1}
@@ -67,10 +67,10 @@ dbhost=${MONGODB_HOST:-10.0.1.1}
 dbport=${MONGODB_PORT:-27017}
 
 #Database Username - default is blank
-dbusername=${HYGIEIA_API_ENV_SPRING_DATA_MONGODB_USERNAME:-db}
+dbusername=${HYGIEIA_API_ENV_SPRING_DATA_MONGODB_USERNAME:-dashboarduser}
 
 #Database Password - default is blank
-dbpassword=${HYGIEIA_API_ENV_SPRING_DATA_MONGODB_PASSWORD:-dbpass}
+dbpassword=${HYGIEIA_API_ENV_SPRING_DATA_MONGODB_PASSWORD:-dbpassword}
 
 #Collector schedule (required)
 jenkins.cron=${JENKINS_CRON:-0 0/5 * * * *}
