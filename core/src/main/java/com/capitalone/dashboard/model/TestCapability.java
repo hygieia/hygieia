0901606fd138affd16e19542085715622472c6bd
Ragha Vema <ragha.vema@capitalone.com>
2018-08-03 11:30:58 -0400

    Model Changes and UniqueCriteria (#2390)
    * Model Changes to have additional metadata
    
    * added unique field criteria as combination of jobName and jobUrl

--- core/src/main/java/com/capitalone/dashboard/model/TestCapability.java
+++ core/src/main/java/com/capitalone/dashboard/model/TestCapability.java
@@ -75,6 +75,21 @@ public class TestCapability {
      */
     private Collection<TestSuite> testSuites = new ArrayList<>();
 
+    /**
+     * Name of the Test Set
+     */
+    private String testSetName;
+
+    /**
+     * Tool Type for the Test Capability
+     */
+    private String toolType;
+
+    /**
+     * Tags of the Test Capability
+     */
+    private Collection<String> tags = new ArrayList<>();
+
     public TestCaseStatus getStatus() {
         return status;
     }
@@ -195,5 +210,16 @@ public class TestCapability {
         this.type = type;
     }
 
+    public String getTestSetName() { return testSetName; }
+
+    public void setTestSetName(String testSetName) { this.testSetName = testSetName; }
+
+    public Collection<String> getTags() { return tags; }
+
+    public void setTags(Collection<String> tags) { this.tags = tags; }
+
+    public String getToolType() { return toolType; }
+
+    public void setToolType(String toolType) { this.toolType = toolType; }
 
 }
