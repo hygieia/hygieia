fe0bee7d65ca6d7c21a2f35991619e32ff3ab119
Murugan, Aravindhan <Aravindhan.Murugan@capitalone.com>
2018-09-20 13:48:31 -0400

    NoFearRelease Implementation Updates

--- collectors/build/jenkins-audit/src/main/java/com/capitalone/dashboard/AuditApplication.java
+++ collectors/build/jenkins-audit/src/main/java/com/capitalone/dashboard/AuditApplication.java
@@ -2,13 +2,12 @@ package com.capitalone.dashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class AuditApplication {
 
     public static void main(String[] args) {
 
-        ApplicationContext applicationContext = SpringApplication.run(AuditApplication.class, args);
+        SpringApplication.run(AuditApplication.class, args);
     }
 }
