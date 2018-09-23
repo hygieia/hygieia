0dc51ebc1e975ae9daaeb2fe191c2d1f734f3ce2
stevegal <stephenjgalbraith@gmail.com>
2018-09-23 20:06:26 +0100

    pmd violation

--- collectors/cloud/cloudwatch/src/main/java/com/capitalone/dashboard/repository/AwsLogCollectorItemRepository.java
+++ collectors/cloud/cloudwatch/src/main/java/com/capitalone/dashboard/repository/AwsLogCollectorItemRepository.java
@@ -1,7 +1,6 @@
 package com.capitalone.dashboard.repository;
 
 import com.capitalone.dashboard.cloudwatch.model.AwsLogCollectorItem;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
