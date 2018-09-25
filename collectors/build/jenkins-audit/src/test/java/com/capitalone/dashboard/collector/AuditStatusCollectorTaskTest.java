40a835fb7dc9a2f1ae0b758767a0813248e5a410
Praveen-chaganti <chagantipraveen5g3@gmail.com>
2018-09-25 10:06:38 -0400

    unit tests for audit status collector

--- collectors/build/jenkins-audit/src/test/java/com/capitalone/dashboard/collector/AuditStatusCollectorTaskTest.java
+++ collectors/build/jenkins-audit/src/test/java/com/capitalone/dashboard/collector/AuditStatusCollectorTaskTest.java
@@ -1,108 +1,81 @@
 package com.capitalone.dashboard.collector;
 
+import com.capitalone.dashboard.collector.config.FongoConfig;
+import com.capitalone.dashboard.collector.config.TestConfig;
 import com.capitalone.dashboard.model.*;
-import com.capitalone.dashboard.repository.AuditStatusCollectorRepository;
-import com.capitalone.dashboard.repository.AuditStatusRepository;
-import com.capitalone.dashboard.repository.DashboardRepository;
+import com.capitalone.dashboard.repository.*;
 import com.capitalone.dashboard.response.DashboardReviewResponse;
 import com.capitalone.dashboard.service.DashboardAuditService;
-import org.assertj.core.api.AssertionsForInterfaceTypes;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.TaskScheduler;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import java.io.IOException;
+import java.util.*;
+
+import static junit.framework.TestCase.assertNotNull;
 import static org.mockito.Mockito.mock;
 
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {TestConfig.class, FongoConfig.class})
 public class AuditStatusCollectorTaskTest {
-    private AuditStatusCollectorTask testee;
-    private TaskScheduler mockScheduler;
     private AuditStatusRepository mockstatusRepository;
-    private DashboardAuditService mockDashboardService;
-    private DashboardRepository mockDashboardRepository;
-    private AuditStatusCollectorRepository mockCollectorRepository;
-    private AuditStatusCollector mockAudistatusCollector;
-    private Dashboard mockDashboard;
-    private DashboardReviewResponse mockDashboardReviewResponse;
+    @Autowired
+    private DashboardAuditService dashboardAuditService;
+    @Autowired
+   private DashboardRepository dashboardRepository;
+    @Autowired
+    private ComponentRepository componentRepository;
+    @Autowired
+    private CollectorRepository collectorRepository;
+    @Autowired
+    private CollectorItemRepository collectorItemRepository;
 
     @Before
-    public void setup(){
-        mockScheduler = mock(TaskScheduler.class);
+    public void setup() throws IOException {
         mockstatusRepository = mock(AuditStatusRepository.class);
-        mockDashboardService = mock(DashboardAuditService.class);
-        mockDashboardRepository = mock(DashboardRepository.class);
-        mockCollectorRepository = mock(AuditStatusCollectorRepository.class);
-        mockAudistatusCollector = mock(AuditStatusCollector.class);
-        mockDashboard = mock(Dashboard.class);
-        mockDashboardReviewResponse = mock(DashboardReviewResponse.class);
-        this.testee = new AuditStatusCollectorTask(mockScheduler, mockDashboardRepository,mockDashboardService, mockstatusRepository, mockCollectorRepository);
+        TestUtils.loadDashBoard(dashboardRepository);
+        TestUtils.loadCollector(collectorRepository);
+        TestUtils.loadComponent(componentRepository);
+        TestUtils.loadCollectorItems(collectorItemRepository);
+        AuditConfigSettings settings = new AuditConfigSettings();
+        settings.setServers(Arrays.asList("http://localhost:8081/"));
+        settings.setCron("*/2 * * * *");
+
     }
 
     @Test
-    public void getCollectorReturnsAuditStatusCollector(){
-        final AuditStatusCollector collector = testee.getCollector();
-        assertThat(collector).isNotNull().isInstanceOf(AuditStatusCollector.class);
-        assertThat(collector.isEnabled()).isTrue();
-        assertThat(collector.isOnline()).isTrue();
-        AssertionsForInterfaceTypes.assertThat(collector.getBuildServers()).contains("http://localhost:8081/");
-        AssertionsForInterfaceTypes.assertThat(collector.getCollectorType()).isEqualTo(CollectorType.Audit);
-        assertThat(collector.getName()).isEqualTo("JenkinsAuditCollector");
-        assertThat(collector.getAllFields().get("instanceUrl")).isEqualTo("");
-        assertThat(collector.getAllFields().get("jobName")).isEqualTo("");
-        assertThat(collector.getAllFields().get("jobUrl")).isEqualTo("");
-        assertThat(collector.getUniqueFields().get("instanceUrl")).isEqualTo("");
-        assertThat(collector.getUniqueFields().get("jobName")).isEqualTo("");
-        assertThat(collector.getUniqueFields().get("jobUrl")).isEqualTo("");
-    }
-
-    @Test
-    public void getCollectorRepositoryReturnsTheRepository() {
-        assertThat(testee.getCollectorRepository()).isNotNull().isInstanceOf(mockCollectorRepository.getClass());
-    }
-
-    @Test
-    public void getCollectAuditStatusData(){
-        long timestamp = mockAudistatusCollector.getLastExecuted();
-        Set<AuditType> auditTypes = new HashSet<>();
-        auditTypes.add(AuditType.ALL);
-        assertThat(auditTypes.contains("ALL"));
-        Iterable<Dashboard> newDashboards = mockDashboardRepository.findByTimestampAfter(timestamp);
-
+    public void getCollectAuditStatusData() throws IOException {
+        long timestamp = 1537824736000L;
+        Iterable<Dashboard> recentDashboards = dashboardRepository.findAll();
         List<AuditResult> auditResults = new ArrayList<>();
-        newDashboards.forEach(dashboard -> {
+        Set<AuditType> allAuditTypes = new HashSet<>();
+        allAuditTypes.add(AuditType.CODE_QUALITY);
+            recentDashboards.forEach(dashboard -> {
             try {
-                DashboardReviewResponse dashboardReviewResponse = mockDashboardService.getDashboardReviewResponse(
-                        dashboard.getTitle(),
-                        dashboard.getType(),
-                        "",
-                        "",
-                        timestamp,
-                        System.currentTimeMillis(),
-                        auditTypes
-                );
-                AuditResult auditResult = new AuditResult(dashboard.getId(), dashboard.getTitle(),
-                        dashboardReviewResponse.getAuditStatuses().iterator().next().toString());
+                long currentTimestamp = System.currentTimeMillis();
+
+                DashboardReviewResponse dashboardReviewResponse = dashboardAuditService.getDashboardReviewResponse(dashboard.getTitle(), dashboard.getType(), dashboard.getConfigurationItemBusServName(),dashboard.getConfigurationItemBusAppName(), timestamp, currentTimestamp, allAuditTypes);
+                AuditResult auditResult = new AuditResult(dashboard.getId(), dashboardReviewResponse,timestamp);
+                assert(auditResult.getDashboardTitle().equals("praveenDashboard"));
+                assertNotNull(auditResult.getDashboardId());
+                assertNotNull(auditResult.getDashboardReviewResponse());
                 auditResults.add(auditResult);
-
-
+                assertNotNull(auditResults);
             } catch (AuditException e) {
+                e.getLocalizedMessage();
             }
-            assertThat(!auditResults.isEmpty());
             if(!auditResults.isEmpty())
              { mockstatusRepository.save(auditResults); }
         });
-        this.testee.collect(mockAudistatusCollector);
     }
 
-    @Test
-    public  void getCron(){
-        assertThat(!testee.getCron().isEmpty());
-    }
 
 
 
