456f5bd18793aa85bcef9739f631280606a83964
tabladrum <tapabrata.pal@capitalone.com>
2018-09-27 13:46:56 -0400

    New features to sync dashboard based on collector item relations. New API to create Generic collector item - adding more missing files

--- core/src/main/java/com/capitalone/dashboard/repository/ComponentRepository.java
+++ core/src/main/java/com/capitalone/dashboard/repository/ComponentRepository.java
@@ -1,16 +1,21 @@
 package com.capitalone.dashboard.repository;
 
+import com.capitalone.dashboard.model.CollectorType;
 import com.capitalone.dashboard.model.Component;
+import com.mysema.query.BooleanBuilder;
+import com.mysema.query.types.path.PathBuilder;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * {@link Component} repository.
  */
-public interface ComponentRepository extends CrudRepository<Component, ObjectId> {
+public interface ComponentRepository extends CrudRepository<Component, ObjectId>,QueryDslPredicateExecutor<Component> {
 
 
     @Query(value = "{'collectorItems.SCM._id': ?0}")
@@ -24,4 +29,12 @@ public interface ComponentRepository extends CrudRepository<Component, ObjectId>
 
     @Query(value="{'collectorItems.Incident.enabled' : ?0}")
     List<Component> findByIncidentCollectorItems(boolean enabled);
+
+
+    default List<Component> findByCollectorTypeAndItemIdIn(CollectorType collectorType, List<ObjectId> collectorItemIds) {
+        BooleanBuilder builder = new BooleanBuilder();
+        PathBuilder<Component> path = new PathBuilder<>(Component.class, "component");
+        builder.and(path.get("collectorItems", Map.class).get(collectorType.toString(),List.class).get("id", ObjectId.class).in(collectorItemIds));
+        return (List<Component>) findAll(builder.getValue());
+    }
 }
