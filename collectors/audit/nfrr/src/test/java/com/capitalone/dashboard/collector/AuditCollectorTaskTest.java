6f5c51c6c7be523899aa63d946d74633d3e3633f
Murugan, Aravindhan <Aravindhan.Murugan@capitalone.com>
2018-10-05 17:27:12 -0400

    NFRR Audit Collector Unit test updates

--- collectors/audit/nfrr/src/test/java/com/capitalone/dashboard/collector/AuditCollectorTaskTest.java
+++ collectors/audit/nfrr/src/test/java/com/capitalone/dashboard/collector/AuditCollectorTaskTest.java
@@ -1,24 +1,21 @@
 package com.capitalone.dashboard.collector;
 
 import com.capitalone.dashboard.collector.config.FongoConfig;
+import com.capitalone.dashboard.collector.config.TestConfig;
 import com.capitalone.dashboard.model.Audit;
 import com.capitalone.dashboard.model.AuditResult;
 import com.capitalone.dashboard.model.AuditType;
 import com.capitalone.dashboard.model.Dashboard;
 import com.capitalone.dashboard.repository.*;
-import org.apache.commons.collections.CollectionUtils;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.io.IOException;
 import java.time.Instant;
-import java.util.Arrays;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Set;
@@ -48,7 +45,6 @@ public class AuditCollectorTaskTest {
     AuditSettings settings = new AuditSettings();
     private static final long BEGIN_DATE = 1537824736000L;
     private static final long END_DATE = Instant.now().toEpochMilli();
-    private HttpEntity<String> httpHeaders;
 
     @Before
     public void setup() throws IOException {
@@ -57,12 +53,10 @@ public class AuditCollectorTaskTest {
         TestUtils.loadCollector(collectorRepository);
         TestUtils.loadComponent(componentRepository);
         TestUtils.loadCollectorItems(collectorItemRepository);
-        settings.setServers(Arrays.asList("http://localhost:8081/"));
-        settings.setCron("*/2 * * * *");
-        httpHeaders = new HttpEntity<>(getHeaders());
+        settings = new TestConfig().settings();
     }
     @Test
-    public void getCollectAuditStatusData() throws IOException {
+    public void getCollectAuditStatusData(){
         Iterable<Dashboard> recentDashboards = dashboardRepository.findAll();
         List<AuditResult> auditResults = new ArrayList<>();
         Set<AuditType> allAuditTypes = new HashSet<>();
@@ -88,14 +82,5 @@ public class AuditCollectorTaskTest {
                 mockstatusRepository.save(auditResults);
             }
         });
-    }
-
-    private HttpHeaders getHeaders() {
-        HttpHeaders headers = new HttpHeaders();
-        if (!CollectionUtils.isEmpty(settings.getUsernames()) && !CollectionUtils.isEmpty(settings.getApiKeys())) {
-            headers.set("apiUser", settings.getUsernames().iterator().next());
-            headers.set("Authorization", "apiToken " + settings.getApiKeys().iterator().next());
-        }
-        return headers;
     }
 }
