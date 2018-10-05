b24c1bae79e7a400d86838222957b9c63de8aefa
Varun Mukka <varun.mukka@capitalone.com>
2018-10-05 08:05:46 -0700

    set SCA CODE_QUALITY_AUDIT_OK for Metric Status Ok/Warn

--- core/src/main/java/com/capitalone/dashboard/model/CodeQualityMetricStatus.java
+++ core/src/main/java/com/capitalone/dashboard/model/CodeQualityMetricStatus.java
@@ -4,5 +4,5 @@ package com.capitalone.dashboard.model;
  * Enumerates the possible {@link CodeQualityMetric} statuses.
  */
 public enum CodeQualityMetricStatus {
-    Ok, Warning, Alert
+    Ok, Warn, Alert, Error
 }
