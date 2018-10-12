e48c0383dd81ae95bd6e57cd1cc40496c728d12d
sao880 <matthew.beasley@capitalone.com>
2018-10-12 15:20:01 -0500

    move the decryption to the sh script and out of before_install

--- travis-utilities/release.sh
+++ travis-utilities/release.sh
@@ -2,6 +2,8 @@
 
 cp travis-utilities/.travis.settings.xml $HOME/.m2/settings.xml
 
+openssl aes-256-cbc -K $encrypted_229149f15c3f_key -iv $encrypted_229149f15c3f_iv -in travis-utilities/keys.gpg.enc -out keys.gpg -d
+
 gpg --fast-import keys.gpg
 
 shred keys.gpg
