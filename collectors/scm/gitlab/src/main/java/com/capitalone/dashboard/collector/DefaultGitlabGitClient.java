13beab0f4cc6bb11f580f20a9a67d3bebcdbd29b
Daniel Lydick <daniel.lydick.osv@fedex.com>
2018-10-15 16:13:26 -0500

    Socket connect/read/retry enhancements per discussion with Tapabrata 'Topo' Pal' on Monday, October 15, 2018

---
+++ collectors/scm/gitlab/src/main/java/com/capitalone/dashboard/collector/DefaultGitlabGitClient.java
@@ -0,0 +1,4 @@
+This file seems to have disappeared.  You might check the logs for such an action.
+In any event, the changes I originally made were for this 'DefaultGitlabGitClient.java'
+in bug report #1930.  Those deltas are repeated in numerous collectors so they should
+be easy to insert here.
