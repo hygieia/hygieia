a20f803e2b63a16bc96b7a394daac4ecb407496a
stephenhughes89 <stephen.hughes@capitalone.com>
2018-10-19 13:30:06 -0400

    fix build

--- core/src/main/java/com/capitalone/dashboard/model/Cmdb.java
+++ core/src/main/java/com/capitalone/dashboard/model/Cmdb.java
@@ -2,7 +2,6 @@ package com.capitalone.dashboard.model;
 
 
 import org.bson.types.ObjectId;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.validation.constraints.NotNull;
