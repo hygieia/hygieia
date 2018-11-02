4d778734492e12ae18e315946e2dac558c5af252
Tom George <tg82490@gmail.com>
2018-11-01 22:35:07 -0500

    Give absolute path to api.conf

--- UI/docker/substitute-nginx-proxy-pass.sh
+++ UI/docker/substitute-nginx-proxy-pass.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
  
-sed -i "s/API_HOST/${API_HOST:-api}/g" api.conf
-sed -i "s/API_PORT/${API_PORT:-8080}/g" api.conf
+CONF_DIR=/opt/app-root/etc/nginx.default.d
+sed -i "s/API_HOST/${API_HOST:-api}/g" ${CONF_DIR}/api.conf
+sed -i "s/API_PORT/${API_PORT:-8080}/g" ${CONF_DIR}/api.conf
