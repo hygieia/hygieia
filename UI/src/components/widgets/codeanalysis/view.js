9d377162de976ba6ab39d1a2e9456a6c180083b1
nireeshT <nireesh.thiruveedula@capitalone.com>
2018-11-08 15:45:15 -0500

    UI fix for Quality widget - open source tab

--- UI/src/components/widgets/codeanalysis/view.js
+++ UI/src/components/widgets/codeanalysis/view.js
@@ -137,7 +137,7 @@
                 }else{
                     calculatePercentage = knownComponentCount / totalComponentCount; 
                 }
-                if(libraryData.policyAlert.length>0 && !angular.isUndefined(libraryData.policyAlert)){
+                if(angular.isDefined(libraryData.policyAlert) && libraryData.policyAlert.length>0){
                     if (libraryData.policyAlert[0].policycriticalCount !== null && libraryData.policyAlert[0].policycriticalCount !== undefined) {
                         criticalCountPolicy = libraryData.policyAlert[0].policycriticalCount;
                     }
