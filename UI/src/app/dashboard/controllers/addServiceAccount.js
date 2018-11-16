bee5ec1a9438662f8b3ec3d0291c5d240658811c
nireeshT <nireesh.thiruveedula@capitalone.com>
2018-11-16 16:00:32 -0500

    SystemId Whitelist - Allowed service accounts

--- UI/src/app/dashboard/controllers/addServiceAccount.js
+++ UI/src/app/dashboard/controllers/addServiceAccount.js
@@ -6,46 +6,31 @@
         .controller('AddServiceAccountController', AddServiceAccountController);
 
     AddServiceAccountController.$inject = ['$uibModalInstance', 'serviceAccountData', '$scope'];
+
     function AddServiceAccountController($uibModalInstance, serviceAccountData, $scope) {
-
         var ctrl = this;
-
         // public methods
         ctrl.submit = submit;
 
-
-
         function submit(form) {
-
-
             if (form.$valid) {
                 console.log('val is ' + document.cdf.serviceAccount.value);
                 console.log('val is ' + document.cdf.fileNames.value);
-
                 var account = {
-                    "serviceAccount" : document.cdf.serviceAccount.value,
-                    "fileNames" : document.cdf.fileNames.value
+                    "serviceAccount": document.cdf.serviceAccount.value,
+                    "fileNames": document.cdf.fileNames.value
                 };
-
                 serviceAccountData
                     .createAccount(account)
                     .success(function (response) {
                         console.log(response);
-                        //$scope.apiKey = response;
-
                         $uibModalInstance.close();
                     })
-                    .error(function(response) {
+                    .error(function (response) {
                         console.log(response);
 
                     });
             }
-            else
-            {
-                //form.apiToken.$setValidity('apiTokenError', false);
-            }
-
         }
-
     }
 })();
