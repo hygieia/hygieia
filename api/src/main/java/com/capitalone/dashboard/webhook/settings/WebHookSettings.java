8a274dc70ab80036d02d9241a84503ce7bc54646
Ragha Vema <ragha.vema@capitalone.com>
2018-12-07 14:35:14 -0500

    NPE Fix and Exclude pattern for Repos in Build request (#2625)
    * Opensource icon fix (#2612)
    
    * project setup
    
    * project setup
    
    * project setup
    
    * project setup
    
    * project setup
    
    * project setup
    
    * project setup
    
    * Relate icon to violation type
    
    The violation summary values were not related to the specific violation type, making the icon not matched with the value displayed. This fix relates the icon to the specific status.
    
    * remove properties file
    
    * Fix for the NPE when component or collector items are abscent, returning empty response (#2617)
    
    * Fix the NPE when the component or the collector item is null
    
    * combining the null checks to make the code concise
    
    * Adding Unit Tests
    
    * Removed the wildcard on the model.* imports, and back to using specific imports
    
    * Removed the wildcard import on the model package
    
    * Update versions of core to [3.1.0-SNAPSHOT,) (#2623)
    
    * updated core to [3.1.0-SNAPSHOT,)
    
    * update core to [3.1.0-SNAPSHOT,)
    
    * update core to [3.1.0-SNAPSHOT,)
    
    * update core to [3.1.0-SNAPSHOT,)
    
    * Fix for NPE when bad collectorItemId is passed in and Add Exclude patterns for Repo branches passed in build request.

--- api/src/main/java/com/capitalone/dashboard/webhook/settings/WebHookSettings.java
+++ api/src/main/java/com/capitalone/dashboard/webhook/settings/WebHookSettings.java
@@ -8,7 +8,13 @@ import org.springframework.stereotype.Component;
 public class WebHookSettings {
     GitHubWebHookSettings gitHub;
 
+    JenkinsBuildWebHookSettings jenkinsBuild;
+
     public GitHubWebHookSettings getGitHub() { return gitHub; }
 
     public void setGitHub(GitHubWebHookSettings gitHub) { this.gitHub = gitHub; }
+
+    public JenkinsBuildWebHookSettings getJenkinsBuild() { return jenkinsBuild; }
+
+    public void setJenkinsBuild(JenkinsBuildWebHookSettings jenkinsBuild) { this.jenkinsBuild = jenkinsBuild; }
 }
