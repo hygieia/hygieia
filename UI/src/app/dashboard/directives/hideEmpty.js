870c9c9ce56e123c4d614a7c50dc400f2071545d
Kaitlyn Heinzmann <kaitlyn.heinzmann@capitalone.com>
2019-01-06 17:18:41 -0500

    move logic to directive
    still needs testing

--- UI/src/app/dashboard/directives/hideEmpty.js
+++ UI/src/app/dashboard/directives/hideEmpty.js
@@ -23,13 +23,27 @@
                 // }
 
                 link: function(scope,element, attrs, ctrl) {
+                    // Assume no data
+                    var isEmpty = true;
+                    var dataList = scope.$eval(attrs.hideEmpty);
+                    scope.data = dataList;
+
+                    // Check if any of the values have data (not empty)
+                    for (var i=0; i < dataList.length; i++) {
+                        // if data exists, widget is not empty
+                        if (dataList[i]) {
+                            isEmpty = false;
+                        }
+                    }
                     //if true (if widget is empty), hide the widget
-                    if(scope.$eval(attrs.hideEmpty)) {
+                    if(isEmpty) {
                         // element.hide();
+                        scope.result = "hide";
                         element.replaceWith('<div class="row"><div class="col-md-12"><div class="widget-body"><br>No data found.</div></div></div>');
                         // scope.dataMessage = "No data found.";
                     }
                     else {
+                        scope.result = "show";
                         element.show();
                     }
                 }
