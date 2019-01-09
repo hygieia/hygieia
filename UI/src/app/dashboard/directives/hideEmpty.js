989c07301fcd54b7febbcc8f2f7f5eda4af93da8
Kaitlyn Heinzmann <kaitlyn.heinzmann@capitalone.com>
2019-01-09 08:45:51 -0500

    adding watch/observe

--- UI/src/app/dashboard/directives/hideEmpty.js
+++ UI/src/app/dashboard/directives/hideEmpty.js
@@ -9,6 +9,9 @@
         .directive('hideEmpty', function () {
             return {
                 restrict: 'A',
+                scope: {
+                    hideEmpty: "="
+                },
 
                 // link: function(scope,element, attrs, ctrl) {
                 //     //if true (if widget is empty), hide the widget
@@ -23,29 +26,31 @@
                 // }
 
                 link: function(scope,element, attrs, ctrl) {
-                    // Assume no data
-                    var isEmpty = true;
-                    var dataList = scope.$eval(attrs.hideEmpty);
-                    scope.data = dataList;
+                    attrs.$observe(attrs.hideEmpty, function(){
+                        // Assume no data
+                        var isEmpty = true;
+                        var dataList = scope.$eval(attrs.hideEmpty);
+                        scope.data = dataList;
 
-                    // Check if any of the values have data (not empty)
-                    for (var i=0; i < dataList.length; i++) {
-                        // if data exists, widget is not empty
-                        if (dataList[i]) {
-                            isEmpty = false;
+                        // Check if any of the values have data (not empty)
+                        for (var i=0; i < dataList.length; i++) {
+                            // if data exists, widget is not empty
+                            if (dataList[i] || dataList[i] == '0') {
+                                isEmpty = false;
+                            }
                         }
-                    }
-                    //if true (if widget is empty), hide the widget
-                    if(isEmpty) {
-                        // element.hide();
-                        scope.result = "hide";
-                        element.replaceWith('<div class="row"><div class="col-md-12"><div class="widget-body"><br>No data found.</div></div></div>');
-                        // scope.dataMessage = "No data found.";
-                    }
-                    else {
-                        scope.result = "show";
-                        element.show();
-                    }
+                        //if true (if widget is empty), hide the widget
+                        if(isEmpty) {
+                            // element.hide();
+                            scope.result = "hide";
+                            // element.replaceWith('<div class="row"><div class="col-md-12"><div class="widget-body"><br>No data found.</div></div></div>');
+                            // scope.dataMessage = "No data found.";
+                        }
+                        else {
+                            scope.result = "show";
+                            element.show();
+                        }
+                    };
                 }
             };
         });
