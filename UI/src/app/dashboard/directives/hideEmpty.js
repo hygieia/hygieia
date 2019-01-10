ad826da07276b36bdc58d89fa59ea842a04577c6
Kaitlyn Heinzmann <kaitlyn.heinzmann@capitalone.com>
2019-01-10 12:29:45 -0500

    working hide functionality

--- UI/src/app/dashboard/directives/hideEmpty.js
+++ UI/src/app/dashboard/directives/hideEmpty.js
@@ -9,10 +9,6 @@
         .directive('hideEmpty', function () {
             return {
                 restrict: 'A',
-                scope: {
-                    hideEmpty: "="
-                },
-
                 // link: function(scope,element, attrs, ctrl) {
                 //     //if true (if widget is empty), hide the widget
                 //     if(scope.$eval(attrs.hideEmpty)) {
@@ -25,32 +21,43 @@
                 //     }
                 // }
 
-                link: function(scope,element, attrs, ctrl) {
-                    attrs.$observe(attrs.hideEmpty, function(){
+                link: function(scope,element, attrs, ctrl, $compile) {
+                    attrs.$observe('hideEmpty', function(){
                         // Assume no data
                         var isEmpty = true;
                         var dataList = scope.$eval(attrs.hideEmpty);
                         scope.data = dataList;
+                        var replacementText;
 
                         // Check if any of the values have data (not empty)
                         for (var i=0; i < dataList.length; i++) {
                             // if data exists, widget is not empty
-                            if (dataList[i] || dataList[i] == '0') {
+                            if ((dataList[i] || dataList[i] == '0') && dataList[i] != "") {
                                 isEmpty = false;
                             }
+
                         }
+
+                        attrs.$observe
                         //if true (if widget is empty), hide the widget
                         if(isEmpty) {
-                            // element.hide();
+                            element.hide();
                             scope.result = "hide";
-                            // element.replaceWith('<div class="row"><div class="col-md-12"><div class="widget-body"><br>No data found.</div></div></div>');
+                            element.html(getTemplate('<div class="row"><div class="col-md-12"><div class="widget-body"><br>No data found.</div></div></div>')).show();
+                            // element.replaceWith('<div class="row"><div class="col-md-12"><div class="widget-body"><br>No data found.</div></div></div>', (scope));
                             // scope.dataMessage = "No data found.";
+
                         }
                         else {
                             scope.result = "show";
                             element.show();
+                            // replacementText = '';
                         }
-                    };
+
+                        // element.create(replacementText);
+
+
+                    });
                 }
             };
         });
