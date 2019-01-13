419f17c607faaf440b6a0d4e1bee5f6e91d46008
Kaitlyn Heinzmann <kaitlyn.heinzmann@capitalone.com>
2019-01-13 13:48:33 -0500

    all logic in directive
    add all logic into the directive
    hide widget if there is no data
    make sure "No data found" is not visible if there is data

--- UI/src/app/dashboard/directives/hideEmpty.js
+++ UI/src/app/dashboard/directives/hideEmpty.js
@@ -9,55 +9,30 @@
         .directive('hideEmpty', function () {
             return {
                 restrict: 'A',
-                // link: function(scope,element, attrs, ctrl) {
-                //     //if true (if widget is empty), hide the widget
-                //     if(scope.$eval(attrs.hideEmpty)) {
-                //         // element.hide();
-                //         element.replaceWith('<div class="row"><div class="col-md-12"><div class="widget-body"><br>No data found.</div></div></div>');
-                //         // scope.dataMessage = "No data found.";
-                //     }
-                //     else {
-                //         element.show();
-                //     }
-                // }
 
-                link: function(scope,element, attrs, ctrl, $compile) {
-                    attrs.$observe('hideEmpty', function(){
-                        // Assume no data
-                        var isEmpty = true;
+                link: function(scope,element, attrs, ctrl, $compile, $document) {
+                    attrs.$observe('hideEmpty', function(newValue, oldValue){
+                       // Assume the widget should be visible
+                       //  var hideWidget = false;
                         var dataList = scope.$eval(attrs.hideEmpty);
-                        scope.data = dataList;
-                        var replacementText;
+                        // scope.data = dataList;
 
-                        // Check if any of the values have data (not empty)
-                        for (var i=0; i < dataList.length; i++) {
-                            // if data exists, widget is not empty
-                            if ((dataList[i] || dataList[i] == '0') && dataList[i] != "") {
-                                isEmpty = false;
-                            }
+                        var hideWidget = dataList.every(x => (!x && x != '0'));
 
-                        }
-
-                        attrs.$observe
                         //if true (if widget is empty), hide the widget
-                        if(isEmpty) {
-                            element.hide();
+                        if(hideWidget) {
+                            // element.hide();
+                            element.children().first().hide();
+                            element.append('<div id="noDataMsg" class="row"><div class="col-md-12"><div class="widget-body"><br>No data found.</div></div></div>');
                             scope.result = "hide";
-                            element.html(getTemplate('<div class="row"><div class="col-md-12"><div class="widget-body"><br>No data found.</div></div></div>')).show();
-                            // element.replaceWith('<div class="row"><div class="col-md-12"><div class="widget-body"><br>No data found.</div></div></div>', (scope));
-                            // scope.dataMessage = "No data found.";
 
                         }
                         else {
                             scope.result = "show";
-                            element.show();
-                            // replacementText = '';
+                            element.children().first().show();
+                            angular.element(document.getElementById('noDataMsg')).hide();
                         }
-
-                        // element.create(replacementText);
-
-
-                    });
+                    }, true);
                 }
             };
         });
