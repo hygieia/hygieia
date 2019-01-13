1fc3cc6ae18e1a2d1feca6bb7648ae168e59e5ef
Kaitlyn Heinzmann <kaitlyn.heinzmann@capitalone.com>
2019-01-13 16:22:32 -0500

    clean up code

--- UI/src/app/dashboard/directives/hideEmpty.js
+++ UI/src/app/dashboard/directives/hideEmpty.js
@@ -13,29 +13,23 @@
                 link: function(scope,element, attrs, ctrl, $compile, $document) {
                     attrs.$observe('hideEmpty', function(newValue, oldValue){
                        // Assume the widget should be visible
-                       //  var hideWidget = false;
                         var dataList = scope.$eval(attrs.hideEmpty);
-                        // scope.data = dataList;
-
                         var hideWidget = dataList.every(x => (!x && x != '0'));
 
                         //if true (if widget is empty), hide the widget
                         if(hideWidget) {
-                            // element.hide();
+                            // hide the widget data within the section (not the current element)
                             element.children().first().hide();
-                            scope.testElement = document.getElementById('noDataMsg');
-                            // if "no data found" already added, make sure it is shown. Otherwise, append it to the HTML
+
+                            // if "no data found" message already added, make sure it is shown. Otherwise, append it to the HTML
                             if (document.getElementById('noDataMsg')){
                                 angular.element(document.getElementById('noDataMsg')).show();
                             } else {
                                 element.append('<div id="noDataMsg" class="row"><div class="col-md-12"><div class="widget-body"><br>No data found.</div></div></div>');
-
                             }
-                            scope.result = "hide";
-
                         }
                         else {
-                            scope.result = "show";
+                            // display the widget and hide "No data found" message
                             element.children().first().show();
                             angular.element(document.getElementById('noDataMsg')).hide();
                         }
