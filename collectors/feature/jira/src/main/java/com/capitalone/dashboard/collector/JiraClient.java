c692842a393c6cc49feba4abef4823a5968d3e00
tabladrum <tapabrata.pal@capitalone.com>
2019-01-13 22:39:44 -0500

    Attempt to update Epic status.

--- collectors/feature/jira/src/main/java/com/capitalone/dashboard/collector/JiraClient.java
+++ collectors/feature/jira/src/main/java/com/capitalone/dashboard/collector/JiraClient.java
@@ -2,24 +2,25 @@ package com.capitalone.dashboard.collector;
 
 import com.capitalone.dashboard.model.Epic;
 import com.capitalone.dashboard.model.Feature;
+import com.capitalone.dashboard.model.FeatureEpicResult;
 import com.capitalone.dashboard.model.Scope;
 import com.capitalone.dashboard.model.Team;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public interface JiraClient {
 
-    List<Feature> getIssues(Team board);
+	FeatureEpicResult getIssues(Team board);
 
-    List<Feature> getIssues(Scope project);
+	FeatureEpicResult getIssues(Scope project);
 
 	Set<Scope> getProjects();
 
 	List<Team> getBoards();
 
 	List<Team> getTeams();
-	
-	Epic getEpic(String epicId);
 
+	Epic getEpic(String epicKey, Map<String, Epic> epicMap);
 }
