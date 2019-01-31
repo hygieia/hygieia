c73a5b7837b53e936f815bc2a3f50c2f4a8e94e3
stephenhughes89 <stephen.hughes@capitalone.com>
2019-01-31 14:01:48 -0500

    Changing value to force push change

--- collectors/feature/jira/src/main/java/com/capitalone/dashboard/collector/FeatureSettings.java
+++ collectors/feature/jira/src/main/java/com/capitalone/dashboard/collector/FeatureSettings.java
@@ -79,7 +79,7 @@ public class FeatureSettings {
 	/**
 	 * Defines the maximum number of features allow per board. If limit is reach collection will not happen for given board
 	 */
-	@Value("${feature.maxNumberOfFeaturesPerBoard:5000}")
+	@Value("${feature.maxNumberOfFeaturesPerBoard:2000}")
 	private int maxNumberOfFeaturesPerBoard;
 
 	public int getMaxNumberOfFeaturesPerBoard() {
