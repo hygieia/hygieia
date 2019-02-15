cd005c64d0235d40c29bd84cefe8adf7d2908dfe
stephenhughes89 <stephen.hughes@capitalone.com>
2019-02-15 15:25:26 -0500

    remove imports

--- collectors/feature/jira/src/test/java/com/capitalone/dashboard/utils/UtilitiesTest.java
+++ collectors/feature/jira/src/test/java/com/capitalone/dashboard/utils/UtilitiesTest.java
@@ -1,21 +1,15 @@
 package com.capitalone.dashboard.utils;
 
-import com.capitalone.dashboard.utils.Utilities;
-import com.capitalone.dashboard.model.Sprint;
 import org.junit.Test;
 
-import java.util.Date;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertNull;
 
 public class UtilitiesTest {
     @Test
     public void parseDateWithoutFractionEmpty() {
         String date = Utilities.parseDateWithoutFraction("");
         assertEquals( "", date);
-        //2018-05-16T11:21:26.000-0400
     }
     @Test
     public void parseDateWithoutFraction() {
