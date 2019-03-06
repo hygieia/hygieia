071b2f23a4d651ef2b668dfccfea3b71f6cf291c
stephenhughes89 <stephen.hughes@capitalone.com>
2019-03-06 15:47:27 -0500

    Updating to had some field tests and routing tests

--- src/app/user/login/login.component.spec.ts
+++ src/app/user/login/login.component.spec.ts
@@ -1,4 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
@@ -8,12 +10,13 @@ import { LoginComponent } from './login.component';
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let router: Router;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
         ReactiveFormsModule,
-        RouterTestingModule,
+        RouterTestingModule.withRoutes([]),
         HttpClientModule
       ],
       declarations: [ LoginComponent ]
@@ -23,6 +26,7 @@ describe('LoginComponent', () => {
 
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginComponent);
+    router = TestBed.get(Router);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
@@ -30,4 +34,18 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('username input should exist', () => {
+    const field = fixture.debugElement.query(By.css('#username'));
+    expect(field).toBeTruthy();
+  });
+  it('password input should exist', () => {
+    const field = fixture.debugElement.query(By.css('#password'));
+    expect(field).toBeTruthy();
+  });
+  it('should tell ROUTER to navigate when sign up clicked', () => {
+    const navigateSpy = spyOn(router, 'navigate');
+    component.signUp();
+    expect(navigateSpy).toHaveBeenCalledWith(['/user/signup']);
+  });
 });
+
--- src/app/user/signup/signup.component.spec.ts
+++ src/app/user/signup/signup.component.spec.ts
@@ -1,4 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
@@ -8,12 +10,13 @@ import { SignupComponent } from './signup.component';
 describe('SignupComponent', () => {
   let component: SignupComponent;
   let fixture: ComponentFixture<SignupComponent>;
+  let router: Router;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
         ReactiveFormsModule,
-        RouterTestingModule,
+        RouterTestingModule.withRoutes([]),
         HttpClientModule
       ],
       declarations: [ SignupComponent ]
@@ -24,10 +27,28 @@ describe('SignupComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(SignupComponent);
     component = fixture.componentInstance;
+    router = TestBed.get(Router);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('username input should exist', () => {
+    const field = fixture.debugElement.query(By.css('#username'));
+    expect(field).toBeTruthy();
+  });
+  it('password input should exist', () => {
+    const field = fixture.debugElement.query(By.css('#password'));
+    expect(field).toBeTruthy();
+  });
+  it('confirmPassword input should exist', () => {
+    const field = fixture.debugElement.query(By.css('#confirmPassword'));
+    expect(field).toBeTruthy();
+  });
+  it('should tell ROUTER to navigate when login clicked', () => {
+    const navigateSpy = spyOn(router, 'navigate');
+    component.login();
+    expect(navigateSpy).toHaveBeenCalledWith(['/user/login']);
   });
 });
