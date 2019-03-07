271244a5c48456b50fa2f2109bf15ea6148cec25
NOBRAINER <31477574+poojithkumar@users.noreply.github.com>
2019-03-07 22:09:04 +0530

    setting proxy to resttemplate
    it make work github under proxy server

--- collectors/scm/github/src/main/java/com/capitalone/dashboard/collector/RestOperationsSupplier.java
+++ collectors/scm/github/src/main/java/com/capitalone/dashboard/collector/RestOperationsSupplier.java
@@ -5,14 +5,38 @@ import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestOperations;
 import org.springframework.web.client.RestTemplate;
+import org.apache.http.HttpHost;
+import org.apache.http.impl.client.HttpClientBuilder;
+
 
 @Component
 public class RestOperationsSupplier implements Supplier<RestOperations> {
-    @Override
+     static String proxyValue;    //hold proxy 
+     static  int proxyPort;       //proxy port value
+
+     public static void set(String proxy,int port){    // get proxy settings
+        proxyValue=proxy;
+        proxyPort=port;
+      }
+  @Override
     public RestOperations get() {
-        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
-        requestFactory.setConnectTimeout(20000);
+
+    if(!"".equals(proxyValue)&&proxyValue!=null){                                      //check for proxy presence
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(
+        HttpClientBuilder.create()
+                .setProxy(new HttpHost(proxyValue,proxyPort, "http"))
+                .build());                                                             //set proxy
+         requestFactory.setConnectTimeout(20000);
         requestFactory.setReadTimeout(20000);
         return new RestTemplate(requestFactory);
-    }
+
+}else
+{
+ HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();   //no proxy condition
+ requestFactory.setConnectTimeout(20000);
+        requestFactory.setReadTimeout(20000);
+        return new RestTemplate(requestFactory);
+
+}
+     }
 }
