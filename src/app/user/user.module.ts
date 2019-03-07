045e68fa8d4715a889cdae63170c5f4ac8928251
stephenhughes89 <stephen.hughes@capitalone.com>
2019-03-07 16:58:08 -0500

    Moving SharedModule from lazyloaded User module

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
 import { UserRoutingModule } from './user-routing.module';
-import {SharedModule} from '../shared/shared.module';
 
 @NgModule({
   declarations: [UserRoutingModule.components],
   imports: [
-    ReactiveFormsModule,
-    SharedModule,
     UserRoutingModule
   ]
 })
