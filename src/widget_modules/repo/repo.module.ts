60c112cd67ef66b0023cae54afc47f74bd1dc720
nireeshT <nireesh.thiruveedula@capitalone.com>
2019-03-07 15:37:49 -0500

    Initial commit: screen and widget modules

--- src/widget_modules/repo/repo.module.ts
+++ src/widget_modules/repo/repo.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { SharedModule } from '../../app/shared/shared.module';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    SharedModule
   ]
 })
 export class RepoModule { }
