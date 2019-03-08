ec19026236ee343a1f1bb224fc1baa277e111b40
Murugan, Aravindhan <--replace-all>
2019-03-08 13:39:32 -0500

    ssa audit check with disposition

--- api-audit/src/main/java/com/capitalone/dashboard/evaluator/StaticSecurityAnalysisEvaluator.java
+++ api-audit/src/main/java/com/capitalone/dashboard/evaluator/StaticSecurityAnalysisEvaluator.java
@@ -6,11 +6,14 @@ import com.capitalone.dashboard.model.CodeQualityMetric;
 import com.capitalone.dashboard.model.CollectorItem;
 import com.capitalone.dashboard.model.CollectorType;
 import com.capitalone.dashboard.model.Dashboard;
+import com.capitalone.dashboard.model.SecurityPolicy;
 import com.capitalone.dashboard.repository.CodeQualityRepository;
 import com.capitalone.dashboard.response.SecurityReviewAuditResponse;
 import com.capitalone.dashboard.status.CodeQualityAuditStatus;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.base.Strings;
 import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -25,8 +28,6 @@ import java.util.stream.Collectors;
 public class StaticSecurityAnalysisEvaluator extends Evaluator<SecurityReviewAuditResponse> {
 
     private final CodeQualityRepository codeQualityRepository;
-    private static final String STR_CRITICAL = "Critical";
-    private static final String STR_HIGH = "High";
     private static final String STR_SCORE = "Score";
     private static final String STR_REPORT_URL = "reportUrl";
 
@@ -69,31 +70,46 @@ public class StaticSecurityAnalysisEvaluator extends Evaluator<SecurityReviewAud
             securityReviewAuditResponse.addAuditStatus(CodeQualityAuditStatus.STATIC_SECURITY_SCAN_MISSING);
             return securityReviewAuditResponse;
         }
-
         CodeQuality returnQuality = codeQualities.get(0);
         securityReviewAuditResponse.setUrl(collectorItem.getOptions().get(STR_REPORT_URL).toString());
         securityReviewAuditResponse.setCodeQuality(returnQuality);
         securityReviewAuditResponse.setLastExecutionTime(returnQuality.getTimestamp());
 
         Set<CodeQualityMetric> metrics = returnQuality.getMetrics();
-        boolean isSecurityCritical = metrics.stream().anyMatch(metric -> metric.getName().equalsIgnoreCase(STR_CRITICAL));
-        boolean isSecurityHigh = metrics.stream().anyMatch(metric -> metric.getName().equalsIgnoreCase(STR_HIGH));
+        boolean isSecurityCriticalAndOpen = metrics.stream()
+                .filter(metric -> metric.getName().equalsIgnoreCase(SecurityPolicy.SecurityLevel.Critical.name()))
+                .anyMatch(metric -> isSecurityStatusOpen(metric.getDisposition()));
+        boolean isSecurityHighAndOpen = metrics.stream()
+                .filter(metric -> metric.getName().equalsIgnoreCase(SecurityPolicy.SecurityLevel.High.name()))
+                .anyMatch(metric -> isSecurityStatusOpen(metric.getDisposition()));
 
-        if (isSecurityCritical){
+        if (isSecurityCriticalAndOpen){
             securityReviewAuditResponse.addAuditStatus(CodeQualityAuditStatus.STATIC_SECURITY_SCAN_FOUND_CRITICAL);
-        }else if (isSecurityHigh){
+        }else if (isSecurityHighAndOpen){
             securityReviewAuditResponse.addAuditStatus(CodeQualityAuditStatus.STATIC_SECURITY_SCAN_FOUND_HIGH);
         }else{
             CodeQualityMetric scoreMetric = metrics.stream().filter(metric -> metric.getName().equalsIgnoreCase(STR_SCORE)).findFirst().get();
-            Integer value = Integer.parseInt(scoreMetric.getValue());
-            if (value == 100) {
+            String scoreVal = scoreMetric.getValue();
+            Integer iScore = StringUtils.isNumeric(scoreVal)? Integer.parseInt(scoreVal) : 0;
+            if (iScore == 100) {
                 securityReviewAuditResponse.addAuditStatus(CodeQualityAuditStatus.STATIC_SECURITY_SCAN_NO_CLOSED_FINDINGS);
-            } else if (value > 0) {
+            } else if (iScore > 0) {
                 securityReviewAuditResponse.addAuditStatus(CodeQualityAuditStatus.STATIC_SECURITY_SCAN_OK);
             } else {
                 securityReviewAuditResponse.addAuditStatus(CodeQualityAuditStatus.STATIC_SECURITY_SCAN_FAIL);
             }
         }
         return securityReviewAuditResponse;
+    }
+
+    /***
+     * security status is considered open if its empty/null or exists in open list or not exists in closed list
+     * @param status
+     * @return boolean
+     */
+    private boolean isSecurityStatusOpen(String status) {
+        return (Strings.isNullOrEmpty(status)
+                || SecurityPolicy.DispositionOpenStatus.contains(status)
+                || !SecurityPolicy.DispositionClosedStatus.contains(status));
     }
 }
