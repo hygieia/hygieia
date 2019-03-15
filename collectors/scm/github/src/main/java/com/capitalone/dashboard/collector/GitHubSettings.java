c253623a6139a2783b4f9e6a0054d822727eba61
NOBRAINER <31477574+poojithkumar@users.noreply.github.com>
2019-03-15 16:04:29 +0530

    Update GitHubSettings.java

--- collectors/scm/github/src/main/java/com/capitalone/dashboard/collector/GitHubSettings.java
+++ collectors/scm/github/src/main/java/com/capitalone/dashboard/collector/GitHubSettings.java
@@ -19,108 +19,110 @@ public class GitHubSettings {
     private String cron;
     private String host;
     private String key;
-        @Value("${github.firstRunHistoryDays:14}")
+	@Value("${github.firstRunHistoryDays:14}")
     private int firstRunHistoryDays;
     private List<String> notBuiltCommits;
-        @Value("${github.errorThreshold:2}")
+	@Value("${github.errorThreshold:2}")
     private int errorThreshold;
-        @Value("${github.rateLimitThreshold:10}")
-    private int rateLimitThreshold;
-    private String personalAccessToken;
+	@Value("${github.rateLimitThreshold:10}")
+	private int rateLimitThreshold;
+	private String personalAccessToken;
+
+
+	public String getHost() {
+		return host;
+	}
+
+	public void setHost(String host) {
+		this.host = host;
+	}
+
+	public String getCron() {
+        return cron;
+    }
+
+    public void setCron(String cron) {
+        this.cron = cron;
+    }
+
+	public String getKey() {
+		return key;
+	}
+	
+	public void setKey(String key) {
+		this.key = key;
+	}
+	
+    public int getFirstRunHistoryDays() {
+		return firstRunHistoryDays;
+	}
+
+	public void setFirstRunHistoryDays(int firstRunHistoryDays) {
+		this.firstRunHistoryDays = firstRunHistoryDays;
+	}
+
+    public List<String> getNotBuiltCommits() {
+        return notBuiltCommits;
+    }
+
+    public void setNotBuiltCommits(List<String> notBuiltCommits) {
+        this.notBuiltCommits = notBuiltCommits;
+    }
+
+	public int getErrorThreshold() {
+		return errorThreshold;
+	}
+
+	public void setErrorThreshold(int errorThreshold) {
+		this.errorThreshold = errorThreshold;
+	}
+
+	public int getRateLimitThreshold() {
+		return rateLimitThreshold;
+	}
+
+	public void setRateLimitThreshold(int rateLimitThreshold) {
+		this.rateLimitThreshold = rateLimitThreshold;
+	}
+
+	public String getPersonalAccessToken() {
+		return personalAccessToken;
+	}
+
+	public void setPersonalAccessToken(String personalAccessToken) {
+		this.personalAccessToken = personalAccessToken;
+	}
     
-        public String getProxyUser(){               //getters and setters
+    public String getProxyUser(){               //getters and setters
                return proxyUser;
         }
         
-        public void setProxyUser(String proxyUser){
+    public void setProxyUser(String proxyUser){
                this.proxyUser=proxyUser;
         }
 
-        public String getProxyPassword(){
+    public String getProxyPassword(){
                 return proxyPassword;
         }
       
-        public void setProxyPassword(String proxyPassword){
+    public void setProxyPassword(String proxyPassword){
                 this.proxyPassword=proxyPassword;
         }
 
-        public String getProxyPort() {          
+    public String getProxyPort() {          
                 return proxyPort;
         } 
 
-        public void setProxyPort(String proxyPort){
+    public void setProxyPort(String proxyPort){
                 this.proxyPort=proxyPort;
         }
 
-        public String getProxy() {
+    public String getProxy() {
                 return proxy;
         }
     
-        public void setProxy(String proxy) {
+    public void setProxy(String proxy) {
                 this.proxy = proxy;
         }
-    
-        public String getHost() {
-                return host;
-        }
 
-        public void setHost(String host) {
-                this.host = host;
-        }
-
-        public String getCron() {
-                return cron;
-        }
-
-        public void setCron(String cron) {
-                this.cron = cron;
-        }
-
-        public String getKey() {
-                return key;
-        }
-
-        public void setKey(String key) {
-                this.key = key;
-        }
-
-        public int getFirstRunHistoryDays() {
-                return firstRunHistoryDays;
-        }
-
-        public void setFirstRunHistoryDays(int firstRunHistoryDays) {
-                this.firstRunHistoryDays = firstRunHistoryDays;
-        }
-
-        public List<String> getNotBuiltCommits() {
-                return notBuiltCommits;
-        }
-
-        public void setNotBuiltCommits(List<String> notBuiltCommits) {
-                this.notBuiltCommits = notBuiltCommits;
-        } 
-
-        public int getErrorThreshold() {
-                return errorThreshold;
-        }
-
-        public void setErrorThreshold(int errorThreshold) {
-                this.errorThreshold = errorThreshold;
-        }
-
-        public int getRateLimitThreshold() {
-                return rateLimitThreshold;
-        }
-
-        public void setRateLimitThreshold(int rateLimitThreshold) {
-                this.rateLimitThreshold = rateLimitThreshold;
-        }
-
-        public String getPersonalAccessToken() {
-                return personalAccessToken;
-        }
-
-        public void setPersonalAccessToken(String personalAccessToken) {
-                this.personalAccessToken = personalAccessToken;
-        }
 }
