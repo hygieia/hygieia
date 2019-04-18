0179e835ea1ade30d643a693b57bc16fb046d3b5
stevegal <stephenjgalbraith@gmail.com>
2019-04-18 20:02:35 +0100

    getting publisher working with different types (json only) (#2799)
    * getting piblisher working with different type supportd (json only)
    
    * remove * imports and bump core version in code quality collector
    
    * bump core reference in parent pom for jenkins plugins

--- collectors/build/jenkins-codequality/src/main/java/com/capitalone/dashboard/utils/CodeQualityService.java
+++ collectors/build/jenkins-codequality/src/main/java/com/capitalone/dashboard/utils/CodeQualityService.java
@@ -2,12 +2,12 @@ package com.capitalone.dashboard.utils;
 
 import com.capitalone.dashboard.jenkins.JenkinsJob;
 import com.capitalone.dashboard.jenkins.model.JenkinsCodeQualityJob;
-import com.capitalone.dashboard.model.quality.CodeQualityVisitee;
+import com.capitalone.dashboard.model.quality.QualityVisitee;
 
 import java.util.List;
 
 public interface CodeQualityService {
 
-    boolean storeJob(JenkinsJob job, JenkinsCodeQualityJob codeQualityJob, List<? extends CodeQualityVisitee> report);
+    boolean storeJob(JenkinsJob job, JenkinsCodeQualityJob codeQualityJob, List<? extends QualityVisitee> report);
 
 }
