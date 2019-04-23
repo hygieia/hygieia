4d6082c78d8ce08b9d26016be783bbe16016fac9
Kaitlyn Heinzmann <kaitlyn.heinzmann@capitalone.com>
2019-04-23 13:55:56 -0400

    remove code from widget to move to widget-header

--- src/app/shared/widget/widget.component.ts
+++ src/app/shared/widget/widget.component.ts
@@ -2,16 +2,10 @@ import { ChangeDetectorRef, Component, ComponentFactoryResolver, Input, Type } f
 import {LayoutComponent} from '../layouts/layout/layout.component';
 import {LayoutDirective} from '../layouts/layout.directive';
 import {Chart} from '../interfaces';
-import {NgbModal, NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
-import {DetailModalComponent} from '../modals/detail-modal/detail-modal.component';
-import {FormModalComponent} from '../modals/form-modal/form-modal.component';
-import {ConfirmationModalComponent} from '../modals/confirmation-modal/confirmation-modal.component';
-import {TestFormComponent} from '../../widget_modules/build/test-form/test-form.component';
 
 
 @Component({
-  selector: 'app-widget',
-  templateUrl: './widget.component.html',
+  template: '',
   styleUrls: ['./widget.component.scss']
 })
 export class WidgetComponent {
@@ -19,7 +13,7 @@ export class WidgetComponent {
 
   public charts: Chart[];
 
-  constructor(private componentFactoryResolver: ComponentFactoryResolver, private cdr: ChangeDetectorRef, private modalService: NgbModal) {
+  constructor(private componentFactoryResolver: ComponentFactoryResolver, private cdr: ChangeDetectorRef) {
   }
 
   loadComponent(layoutTag: LayoutDirective) {
@@ -31,23 +25,7 @@ export class WidgetComponent {
     this.cdr.detectChanges();
   }
 
-  openConfig() {
-    const modalRef = this.modalService.open(FormModalComponent);
-    modalRef.componentInstance.title = 'Configure';
-    modalRef.componentInstance.modalType = TestFormComponent;
-    modalRef.componentInstance.id = 1;
-    modalRef.result.then((result) => {
-      console.log(result);
-    }).catch((error) => {
-      console.log(error);
-    });
-  }
 
-  openConfirm() {
-    const modalRef = this.modalService.open(ConfirmationModalComponent);
-    modalRef.componentInstance.title = 'Are you sure want to delete this widget from your dashboard?';
-    // modalRef.componentInstance.modalType = ConfirmationModalComponent;
-  }
 }
 
 
