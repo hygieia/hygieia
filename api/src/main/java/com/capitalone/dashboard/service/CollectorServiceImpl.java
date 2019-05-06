bf9ee72189d4049a155e6805e3b0f4e084318663
battinasubodh <36087171+battinasubodh@users.noreply.github.com>
2019-05-05 23:47:46 -0500

    Updates to grab the latest updated collector item (#2622)
    * Updates to grab the latest updated collector item
    
    * Setting the lastUpdatedTimestamp for collector items when saved or updated
    
    * A minor update, to not set the timestamp for existing collector items of a given type
    
    * reverting back collector_item collection timestamp update changes
    
    * one more cleanup
    
    * Updated method associateCollectorToComponent() to detect the collector item that actually changed before setting the lastUpdated timestamp
    
    * Removed wild card usage on the imports
    
    * using java 8 for map options comaprison
    
    * Minor update on return values
    
    * Removed the import wild card

--- api/src/main/java/com/capitalone/dashboard/service/CollectorServiceImpl.java
+++ api/src/main/java/com/capitalone/dashboard/service/CollectorServiceImpl.java
@@ -169,7 +169,6 @@ public class CollectorServiceImpl implements CollectorService {
         //Flow is here because there is only one collector item with the same collector id and niceName. So, update with
         // the new info - keep the same collector item id. Save = Update or Insert.
         item.setId(existing.getId());
-
         return collectorItemRepository.save(item);
     }
 
@@ -184,7 +183,6 @@ public class CollectorServiceImpl implements CollectorService {
         //Flow is here because there is only one collector item with the same collector id and niceName. So, update with
         // the new info - keep the same collector item id. Save = Update or Insert.
         item.setId(existing.getId());
-
         return collectorItemRepository.save(item);
     }
 
