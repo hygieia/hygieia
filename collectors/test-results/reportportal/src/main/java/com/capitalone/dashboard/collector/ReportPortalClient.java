d1e290bb7fa47b9911a2a80d6155002d52ce5eea
gopiprasanthpotipireddy <gopi.prasanth007@gmail.com>
2019-05-08 23:35:39 +0530

    changed reportportal name

---
+++ collectors/test-results/reportportal/src/main/java/com/capitalone/dashboard/collector/ReportPortalClient.java
@@ -0,0 +1,27 @@
+package com.capitalone.dashboard.collector;
+
+
+
+import java.util.List;
+
+import org.bson.types.ObjectId;
+
+import com.capitalone.dashboard.model.ReportPortalCollector;
+import com.capitalone.dashboard.model.ReportPortalProject;
+import com.capitalone.dashboard.model.ReportResult;
+
+
+
+public interface ReportPortalClient {
+
+    List<ReportPortalProject> getProjectData(String instanceUrl ,String projectName);
+
+	
+
+	List<ReportResult> getTestData(ReportPortalCollector collector, String launchId, String instanceUrl, ObjectId collectorItemId);
+
+
+
+    
+
+}
