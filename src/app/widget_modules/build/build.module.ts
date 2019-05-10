92a313fa2acbd1766f7d65156124df13f3ab7a35
Kaitlyn Heinzmann <kaitlyn.heinzmann@capitalone.com>
2019-05-10 09:57:18 -0400

    duplicate import

--- src/app/widget_modules/build/build.module.ts
+++ src/app/widget_modules/build/build.module.ts
@@ -1,4 +1,5 @@
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { SharedModule } from 'src/app/shared/shared.module';
@@ -6,7 +7,7 @@ import { SharedModule } from 'src/app/shared/shared.module';
 import { BuildConfigFormComponent } from './build-config-form/build-config-form.component';
 import { BuildRoutingModule } from './build-routing-module';
 import { BuildWidgetComponent } from './build-widget/build-widget.component';
-import {FormsModule} from '@angular/forms';
+
 
 
 @NgModule({
