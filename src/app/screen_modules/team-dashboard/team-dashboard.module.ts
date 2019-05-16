d6104101fcfa2867370350e17442dafc8a30b52f
Kaitlyn Heinzmann <kaitlyn.heinzmann@capitalone.com>
2019-05-16 15:59:28 -0400

    unused component cleanup

--- src/app/screen_modules/team-dashboard/team-dashboard.module.ts
+++ src/app/screen_modules/team-dashboard/team-dashboard.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 
-import { BuildWidgetComponent} from '../../widget_modules/build/build-widget/build-widget.component';
 import { DashboardViewComponent } from './dashboard-view/dashboard-view.component';
 import { SharedModule } from '../../shared/shared.module';
 import { TeamDashboardRoutingModule } from './team-dashboard-routing.module';
-import {WidgetHeaderComponent} from '../../shared/widget-header/widget-header.component';
 
 @NgModule({
   declarations: [
-    // BuildWidgetComponent,
     DashboardViewComponent
   ],
   imports: [
@@ -16,8 +13,6 @@ import {WidgetHeaderComponent} from '../../shared/widget-header/widget-header.co
     TeamDashboardRoutingModule
   ],
   entryComponents: [
-    BuildWidgetComponent,
-    // WidgetHeaderComponent
   ]
 })
 export class TeamDashboardModule { }
