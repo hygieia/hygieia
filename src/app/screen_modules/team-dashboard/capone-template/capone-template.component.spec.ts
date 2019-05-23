e8b9da1799bbd71d656a7ad6a3c1cb4dfa8d2c29
Kaitlyn Heinzmann <kaitlyn.heinzmann@capitalone.com>
2019-05-23 14:48:39 -0400

    cleanup

--- src/app/screen_modules/team-dashboard/capone-template/capone-template.component.spec.ts
+++ src/app/screen_modules/team-dashboard/capone-template/capone-template.component.spec.ts
@@ -1,6 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CaponeTemplateComponent } from './capone-template.component';
+import {PlaceholderWidgetComponent} from '../../../shared/widget/placeholder-widget/placeholder-widget.component';
+import { WidgetHeaderComponent } from '../../../shared/widget-header/widget-header.component';
+import {Test} from 'tslint';
+import {BrowserDynamicTestingModule} from '@angular/platform-browser-dynamic/testing';
+import {Component, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA} from '@angular/core';
+import {BuildConfigFormComponent} from '../../../widget_modules/build/build-config-form/build-config-form.component';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
 
 describe('CaponeTemplateComponent', () => {
   let component: CaponeTemplateComponent;
@@ -8,9 +15,11 @@ describe('CaponeTemplateComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CaponeTemplateComponent ]
-    })
-    .compileComponents();
+      declarations: [
+        CaponeTemplateComponent
+      ],
+      schemas: [ NO_ERRORS_SCHEMA ]
+    });
   }));
 
   beforeEach(() => {
