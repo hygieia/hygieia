85964659d3324e7d043cf00c159e4ba662754a1e
ZhenweiDing <zhenwei.ding@gmail.com>
2019-06-10 21:48:25 +0800

    Update to the latest version of CollectionUtils.

--- collectors/scm/gitlab/src/main/java/com/capitalone/dashboard/gitlab/GitlabCommitsResponseMapper.java
+++ collectors/scm/gitlab/src/main/java/com/capitalone/dashboard/gitlab/GitlabCommitsResponseMapper.java
@@ -3,7 +3,7 @@ package com.capitalone.dashboard.gitlab;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections4.CollectionUtils;
 import org.joda.time.DateTime;
 import org.springframework.stereotype.Component;
 
