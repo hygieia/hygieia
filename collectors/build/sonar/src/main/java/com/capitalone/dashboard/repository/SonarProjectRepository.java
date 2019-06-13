c6bfc7d2ccaa6df286e38528fbf550a276fc48ad
Fidel Coria <fidel.coria@aa.com>
2019-06-13 15:46:54 -0500

    Add quotes to Sonar repository query

--- collectors/build/sonar/src/main/java/com/capitalone/dashboard/repository/SonarProjectRepository.java
+++ collectors/build/sonar/src/main/java/com/capitalone/dashboard/repository/SonarProjectRepository.java
@@ -8,9 +8,9 @@ import java.util.List;
 
 public interface SonarProjectRepository extends BaseCollectorItemRepository<SonarProject> {
 
-    @Query(value="{ 'collectorId' : ?0, options.instanceUrl : ?1, options.projectId : ?2}")
+    @Query(value="{ 'collectorId' : ?0, 'options.instanceUrl' : ?1, 'options.projectId' : ?2}")
     SonarProject findSonarProject(ObjectId collectorId, String instanceUrl, String projectId);
 
-    @Query(value="{ 'collectorId' : ?0, options.instanceUrl : ?1, enabled: true}")
+    @Query(value="{ 'collectorId' : ?0, 'options.instanceUrl' : ?1, enabled: true}")
     List<SonarProject> findEnabledProjects(ObjectId collectorId, String instanceUrl);
 }
