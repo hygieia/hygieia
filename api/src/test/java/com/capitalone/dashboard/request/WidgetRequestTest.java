a71c3c0e52f8b4b4612c2d9a51745b0f69fc11cb
Yeswanth Barapati <Yeswanth.Barapati@aa.com>
2019-06-14 10:36:16 -0500

    Rally backwards compatibility Collector Name

--- api/src/test/java/com/capitalone/dashboard/request/WidgetRequestTest.java
+++ api/src/test/java/com/capitalone/dashboard/request/WidgetRequestTest.java
@@ -35,4 +35,29 @@ public class WidgetRequestTest {
         Assert.assertEquals("TestTeam", options.get("teamName"));
         Assert.assertEquals("321", options.get("teamId"));
     }
+
+    @Test
+    public void widgetTest() {
+        Map<String, Object> allOptions = new HashMap<>();
+        allOptions.put("id", "feature0");
+        allOptions.put("featureTool", "Jira");
+        allOptions.put("projectName", "TestProject");
+        allOptions.put("projectId", "123");
+        allOptions.put("teamName", "TestTeam");
+        allOptions.put("teamId", "321");
+		
+		WidgetRequest widgetRequest = new WidgetRequest();
+        widgetRequest.setName("feature");
+        widgetRequest.setOptions(allOptions);
+        Widget widget = widgetRequest.widget();
+
+        Map<String, Object> options = widget.getOptions();
+        Assert.assertEquals(6, options.size());
+        Assert.assertEquals("feature0", options.get("id"));
+        Assert.assertEquals("Jira", options.get("featureTool"));
+        Assert.assertEquals("TestProject", options.get("projectName"));
+        Assert.assertEquals("123", options.get("projectId"));
+        Assert.assertEquals("TestTeam", options.get("teamName"));
+        Assert.assertEquals("321", options.get("teamId"));
+    }
 }
