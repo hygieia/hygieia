e265520dfe01f0d52008c4b64ec53938d138e84e
rvema <ragha.vema@capitalone.com>
2019-07-04 10:45:53 -0400

    Remove api as it has been migrated

--- api/src/main/java/com/capitalone/dashboard/service/GenericCollectorItemService.java
+++
@@ -1,9 +0,0 @@
-package com.capitalone.dashboard.service;
-
-import com.capitalone.dashboard.misc.HygieiaException;
-import com.capitalone.dashboard.request.GenericCollectorItemCreateRequest;
-
-public interface GenericCollectorItemService {
-    String create(GenericCollectorItemCreateRequest request) throws HygieiaException;
-    String createGenericBinaryArtifactData(GenericCollectorItemCreateRequest request) throws HygieiaException;
-}
