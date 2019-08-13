f521d00c6483816ae25056bc9ae99916ea09d608
Ragha Vema <ragha.vema@capitalone.com>
2019-08-13 19:52:01 -0400

    Remove Jenkins Codequality collector post Migration (#3095)

--- collectors/build/jenkins-codequality/src/main/java/com/capitalone/dashboard/repository/JenkinsCodeQualityCollectorRepository.java
+++
@@ -1,6 +0,0 @@
-package com.capitalone.dashboard.repository;
-
-import com.capitalone.dashboard.collector.JenkinsCodeQualityCollector;
-
-public interface JenkinsCodeQualityCollectorRepository extends BaseCollectorRepository<JenkinsCodeQualityCollector>{
-}
