93b6a7f71f300545cc604dc5704eead48201c06d
rvema <ragha.vema@capitalone.com>
2019-08-13 19:46:07 -0400

    Remove Jenkins Codequality collector post Migration

--- collectors/build/jenkins-codequality/src/main/java/com/capitalone/dashboard/utils/CodeQualityConverter.java
+++
@@ -1,9 +0,0 @@
-package com.capitalone.dashboard.utils;
-
-import com.capitalone.dashboard.model.CodeQuality;
-import com.capitalone.dashboard.model.quality.QualityVisitor;
-
-public interface CodeQualityConverter {
-
-  QualityVisitor<CodeQuality> produceVisitor();
-}
