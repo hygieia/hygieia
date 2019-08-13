61bfbd73a60b98931302ad2bd251f224807f8414
rvema <ragha.vema@capitalone.com>
2019-08-13 16:23:37 -0400

    delete hpsm collector

--- collectors/cmdb/hpsm/src/main/java/com/capitalone/dashboard/collector/HpsmIncidentUpdateClient.java
+++
@@ -1,8 +0,0 @@
-package com.capitalone.dashboard.collector;
-
-import com.capitalone.dashboard.misc.HygieiaException;
-import com.capitalone.dashboard.model.Incident;
-
-public interface HpsmIncidentUpdateClient {
-    Incident getIncident(String incidentId) throws HygieiaException;
-}
