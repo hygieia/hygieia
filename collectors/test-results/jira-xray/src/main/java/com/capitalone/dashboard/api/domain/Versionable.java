6fe3e33f9c2869a0953d6d8ba7f37260185d981a
Ragha Vema <ragha.vema@capitalone.com>
2019-08-13 15:42:41 -0400

    Cleanup of jira x-ray collector (#3092)
    * deleted jira xray build collector post migration
    
    * deleted jira xray build collector from pom.xml

--- collectors/test-results/jira-xray/src/main/java/com/capitalone/dashboard/api/domain/Versionable.java
+++
@@ -1,9 +0,0 @@
-package com.capitalone.dashboard.api.domain;
-
-public interface Versionable<T> {
-
-    T getOldVersion();
-    void setOldVersion(T oldVersion);
-
-    int getVersion();
-}
--- collectors/test-results/jira-xray/src/main/java/com/capitalone/dashboard/core/client/testexecution/TestExecutionClient.java
+++
@@ -1,6 +0,0 @@
-package com.capitalone.dashboard.core.client.testexecution;
-
-public interface TestExecutionClient {
-    int updateTestResultInformation();
-
-}
