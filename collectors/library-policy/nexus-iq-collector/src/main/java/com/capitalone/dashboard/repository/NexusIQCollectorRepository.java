e4e215d5d553eeaad436e26a530d23cb781cfef1
Ragha Vema <ragha.vema@capitalone.com>
2019-08-14 12:21:27 -0400

    remove nexus iq collector (#3099)

--- collectors/library-policy/nexus-iq-collector/src/main/java/com/capitalone/dashboard/repository/NexusIQCollectorRepository.java
+++
@@ -1,6 +0,0 @@
-package com.capitalone.dashboard.repository;
-
-import com.capitalone.dashboard.model.NexusIQCollector;
-
-public interface NexusIQCollectorRepository extends BaseCollectorRepository<NexusIQCollector> {
-}
