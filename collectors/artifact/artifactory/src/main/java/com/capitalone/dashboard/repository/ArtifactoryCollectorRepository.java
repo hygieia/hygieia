2678ed15e1df7c2c7576f47231a896baf56338d8
Ragha Vema <ragha.vema@capitalone.com>
2019-08-23 15:54:15 -0400

    Remove artifactory collector post Migration (#3124)

--- collectors/artifact/artifactory/src/main/java/com/capitalone/dashboard/repository/ArtifactoryCollectorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.capitalone.dashboard.repository;
-
-import com.capitalone.dashboard.model.ArtifactoryCollector;
-
-/**
- * Repository for {@link ArtifactoryCollector}.
- */
-public interface ArtifactoryCollectorRepository extends BaseCollectorRepository<ArtifactoryCollector> {
-}
