03412e22c2d6c1b1cec669212d15cbd69a32b3ca
Courtney Peng <courtney.peng@gmail.com>
2020-07-10 15:45:15 -0400

    unused import

--- src/app/shared/modals/edit-dashboard-modal/edit-dashboard-modal.component.ts
+++ src/app/shared/modals/edit-dashboard-modal/edit-dashboard-modal.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
 import { DashboardDataService } from 'src/app/shared/services/dashboard-data.service';
 import { AuthService } from 'src/app/core/services/auth.service';
 import { WidgetManagerService } from 'src/app/shared/services/widget-manager.service';
