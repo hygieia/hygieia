7f25b97217eb12765c59ebbbff6b878718de8f4f
Murugan, Aravindhan <aravindhan.murugan@capitalone.com>
2020-08-31 18:26:40 -0400

    unit test fix

--- src/app/viewer_modules/collector-item/collector-item-viewer/collector-item-details/collector-item-details.component.spec.ts
+++ src/app/viewer_modules/collector-item/collector-item-viewer/collector-item-details/collector-item-details.component.spec.ts
@@ -4,17 +4,16 @@ import { CollectorItemDetailsComponent } from './collector-item-details.componen
 import {HttpClientTestingModule} from '@angular/common/http/testing';
 import {ReactiveFormsModule} from '@angular/forms';
 import {NgbActiveModal, NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import {SharedModule} from '../../../../shared/shared.module';
 import {RouterTestingModule} from '@angular/router/testing';
 
-describe('CollectorItemDetailsComponent', () => {
+fdescribe('CollectorItemDetailsComponent', () => {
   let component: CollectorItemDetailsComponent;
   let fixture: ComponentFixture<CollectorItemDetailsComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        ReactiveFormsModule, NgbModule, SharedModule, HttpClientTestingModule,
+        ReactiveFormsModule, NgbModule, HttpClientTestingModule,
         RouterTestingModule.withRoutes([])
       ],
       declarations: [CollectorItemDetailsComponent],
