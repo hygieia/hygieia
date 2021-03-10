2b58bf67d58d0a4274e05e31779fb189ae0c1912
Ryan Montgomery <ryan.montgomery@capitalone.com>
2021-03-10 17:20:40 -0500

    Fix first few tests

--- src/app/widget_modules/build/build-widget/build-widget.component.spec.ts
+++ src/app/widget_modules/build/build-widget/build-widget.component.spec.ts
@@ -365,7 +365,7 @@ describe('BuildWidgetComponent', () => {
       expect(component.charts[0].data.dataPoints[0].series[0].value).toEqual(7);
       expect(component.charts[0].data.dataPoints[1].series[0].value).toEqual(6);
 
-      expect(component.charts[1].data.items[0].title).toEqual('708');
+      expect(component.charts[1].data.items[0].title).toEqual('Build: 708');
       expect(component.charts[3].data[0].value).toEqual(0);
       expect(component.charts[3].data[1].value).toEqual(0);
       expect(component.charts[3].data[2].value).toEqual(7);
