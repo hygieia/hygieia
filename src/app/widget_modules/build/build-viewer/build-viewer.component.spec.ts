fc1ed3b7b73a23703a2b222305c039889b01ff6e
SandhyaRajasabeson <sandhya.rajasabeson@capitalone.com>
2021-03-30 16:43:59 -0400

    unit tests

--- src/app/widget_modules/build/build-viewer/build-viewer.component.spec.ts
+++ src/app/widget_modules/build/build-viewer/build-viewer.component.spec.ts
@@ -1,17 +1,24 @@
+import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core"
 import { async, ComponentFixture, TestBed } from "@angular/core/testing"
+import { FormsModule } from "@angular/forms";
+import { Router } from "@angular/router";
+import { RouterTestingModule } from "@angular/router/testing";
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import { BuildViewerComponent } from './build-viewer.component';
 
 describe('BuildViewerComponent', () => {
     let component: BuildViewerComponent;
     let fixture: ComponentFixture<BuildViewerComponent>;
+    let router: Router;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [ BuildViewerComponent ],
-            providers: [],
-            schemas: [ CUSTOM_ELEMENTS_SCHEMA ],
-            imports: []
+            providers: [{ provide: Router, useValue: {
+                navigate: jasmine.createSpy('navigate')
+            }}],
+            imports: [ FormsModule ]
         })
         .compileComponents();
     }));
@@ -23,10 +30,17 @@ describe('BuildViewerComponent', () => {
     beforeEach(() => {
         fixture = TestBed.createComponent(BuildViewerComponent);
         component = fixture.componentInstance;
+        router = TestBed.get(Router);
         fixture.detectChanges();
     });
 
     it('should create', () => {
         expect(component).toBeTruthy();
     });
+
+    it('should navigate to standalone build page', () => {
+        component.buildId = "1234";
+        component.searchBuild();
+        expect(router.navigate).toHaveBeenCalledWith(['/build/1234']);
+    })  
 })
--- src/app/widget_modules/build/build-viewer/build-viewer.component.ts
+++ src/app/widget_modules/build/build-viewer/build-viewer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 import { Router } from "@angular/router";
 
 @Component({
