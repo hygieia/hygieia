176537cf7fb34ce14bffc9c91e9e01a85c509e27
balnibarbi <balnibarbi@github.com>
2021-04-22 15:02:00 +1000

    Make the listen port configurable

--- UI/docker/conf-builder.sh
+++ UI/docker/conf-builder.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-sed s:API_HOST:${API_HOST:-api}: /etc/nginx/conf.d/default.conf.templ |\
-  sed s:API_PORT:${API_PORT:-8080}: > /etc/nginx/conf.d/default.conf
+cat /etc/nginx/conf.d/default.conf.templ \
+  | sed s:API_HOST:${API_HOST:-api}:     \
+  | sed s:API_PORT:${API_PORT:-8080}:    \
+  | sed s:UI_PORT:${UI_PORT:-80}:        \
+  | sed s:UI_HOST:${UI_HOST:-localhost}: \
+> /etc/nginx/conf.d/default.conf
