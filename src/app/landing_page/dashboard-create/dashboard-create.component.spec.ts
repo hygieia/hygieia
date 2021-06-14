e631d6eed05ea1803fe35b65859755cc6c6b8444
SandhyaRajasabeson <sandhya.rajasabeson@capitalone.com>
2021-06-14 15:44:42 -0400

    dashboard

--- src/app/landing_page/dashboard-create/dashboard-create.component.spec.ts
+++ src/app/landing_page/dashboard-create/dashboard-create.component.spec.ts
@@ -7,19 +7,35 @@ import {FormsModule} from '@angular/forms';
 import {HttpClientTestingModule} from '@angular/common/http/testing';
 import {RouterTestingModule} from '@angular/router/testing';
 import {NgbTypeaheadModule} from '@ng-bootstrap/ng-bootstrap';
+import { DashboardService } from 'src/app/shared/dashboard.service';
+import { Observable, of } from 'rxjs';
+import { Router } from '@angular/router';
 
 class MockDialogRef {}
+
+class MockRouter {
+  navigate() {}
+}
+
+class MockDashboardService {
+  createDashboard: () => {}
+}
 
 describe('DashboardCreateComponent', () => {
   let component: DashboardCreateComponent;
   let fixture: ComponentFixture<DashboardCreateComponent>;
+  let service: DashboardService;
+  let router: Router;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [FormsModule, NbThemeModule.forRoot(), NbInputModule, NbButtonModule, HttpClientTestingModule,
         RouterTestingModule.withRoutes([]), NbDialogModule, NgbTypeaheadModule],
       declarations: [ DashboardCreateComponent ],
-      providers: [{provide: NbDialogRef, useClass: MockDialogRef}],
+      providers: [
+        { provide: NbDialogRef, useClass: MockDialogRef },
+        { provide: Router, useClass: MockRouter }
+      ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA]
     })
     .compileComponents();
@@ -28,10 +44,18 @@ describe('DashboardCreateComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(DashboardCreateComponent);
     component = fixture.componentInstance;
+    service = TestBed.get(DashboardService);
+    router = TestBed.get(Router);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should create dashboard', () => {
+    const spy = spyOn(service, 'createDashboard').and.returnValue({subscribe: () => {}})
+    component.createDashboard();
+    expect(spy).toHaveBeenCalled();
+  })
 });
