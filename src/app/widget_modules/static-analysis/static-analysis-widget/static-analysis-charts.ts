7e8f0ce41d310f5dca4878f5096a12eb19ea822d
VivekBuzruk <vivek_buzruk@yahoo.com>
2021-06-16 10:23:25 +0530

    Make project drop down optional and introduce new visual for Violations

--- src/app/widget_modules/static-analysis/static-analysis-widget/static-analysis-charts.ts
+++ src/app/widget_modules/static-analysis/static-analysis-widget/static-analysis-charts.ts
@@ -1,72 +1,198 @@
-import { IChart } from 'src/app/shared/interfaces';
+import { IChart } from "src/app/shared/interfaces";
 
-import {NumberCardChartComponent} from '../../../shared/charts/number-card-chart/number-card-chart.component';
-import {ClickListComponent} from '../../../shared/charts/click-list/click-list.component';
-import {PieGridChartComponent} from '../../../shared/charts/pie-grid-chart/pie-grid-chart.component';
-import {IPieGridData} from '../../../shared/charts/pie-grid-chart/pie-grid-interfaces';
-import {DataItem} from '../../../shared/ngx-charts/pie-grid/data-item.interfaces';
+import { NumberCardChartComponent } from "../../../shared/charts/number-card-chart/number-card-chart.component";
+import { ClickListComponent } from "../../../shared/charts/click-list/click-list.component";
+import { PieChartComponent } from "../../../shared/charts/pie-chart/pie-chart.component";
+import { PieGridChartComponent } from "../../../shared/charts/pie-grid-chart/pie-grid-chart.component";
+import { IPieGridData } from "../../../shared/charts/pie-grid-chart/pie-grid-interfaces";
+import { DataItem } from "../../../shared/ngx-charts/pie-grid/data-item.interfaces";
 
-export let STATICANALYSIS_CHARTS: IChart[] = [
+export enum ViolationsChart {
+  CARD_CHART,
+  CLICK_TABLE,
+  PIE_CHART,
+}
+
+export let CARD_STATICANALYSIS_CHARTS: IChart[] = [
   {
-    title: 'Project Details',
+    title: "Project Details",
     component: ClickListComponent,
     data: [],
-    xAxisLabel: '',
-    yAxisLabel: '',
-    colorScheme: '',
+    xAxisLabel: "",
+    yAxisLabel: "",
+    colorScheme: "",
   },
   {
-    title: 'Coverage',
+    title: "Coverage",
     component: PieGridChartComponent,
     data: {
       results: [
         {
-          name: '',
+          name: "",
           value: 0,
-        }
+        },
       ] as DataItem[],
       designatedTotal: 100.0,
-      label: 'Lines of Code',
+      label: "Lines of Code",
       useCustomLabelValue: true,
       customLabelValue: 0,
     } as IPieGridData,
-    xAxisLabel: '',
-    yAxisLabel: '',
+    xAxisLabel: "",
+    yAxisLabel: "",
     scaleFactor: 0.85,
-    colorScheme: 'forest',
+    colorScheme: "forest",
   },
   {
-    title: 'Issues',
+    title: "Issues",
     component: NumberCardChartComponent,
     data: [
       {
-        name: 'Blocker Violations',
+        name: "Blocker Violations",
         value: 0,
       },
       {
-        name: 'Critical Violations',
+        name: "Critical Violations",
         value: 0,
       },
       {
-        name: 'Major Violations',
+        name: "Major Violations",
         value: 0,
       },
       {
-        name: 'Total Issues',
+        name: "Total Issues",
         value: 0,
-      }
+      },
     ],
-    xAxisLabel: '',
-    yAxisLabel: '',
+    xAxisLabel: "",
+    yAxisLabel: "",
     scaleFactor: 0.55,
-    colorScheme: 'flame',
+    colorScheme: "flame",
   },
   {
-    title: 'Unit Test Metrics',
+    title: "Unit Test Metrics",
     component: ClickListComponent,
     data: [],
-    xAxisLabel: '',
-    yAxisLabel: '',
-    colorScheme: '',
+    xAxisLabel: "",
+    yAxisLabel: "",
+    colorScheme: "",
+  },
+];
+
+export let CLICKTAB_STATICANALYSIS_CHARTS: IChart[] = [
+  {
+    title: "Project Details",
+    component: ClickListComponent,
+    data: [],
+    xAxisLabel: "",
+    yAxisLabel: "",
+    colorScheme: "",
+  },
+  {
+    title: "Coverage",
+    component: PieGridChartComponent,
+    data: {
+      results: [
+        {
+          name: "",
+          value: 0,
+        },
+      ] as DataItem[],
+      designatedTotal: 100.0,
+      label: "Lines of Code",
+      useCustomLabelValue: true,
+      customLabelValue: 0,
+    } as IPieGridData,
+    xAxisLabel: "",
+    yAxisLabel: "",
+    scaleFactor: 0.85,
+    colorScheme: "forest",
+  },
+  {
+    title: "Issues",
+    component: ClickListComponent,
+    data: [],
+    xAxisLabel: "",
+    yAxisLabel: "",
+    colorScheme: [],
+  },
+  {
+    title: "Unit Test Metrics",
+    component: ClickListComponent,
+    data: [],
+    xAxisLabel: "",
+    yAxisLabel: "",
+    colorScheme: "",
+  },
+];
+
+export let PIE_STATICANALYSIS_CHARTS: IChart[] = [
+  {
+    title: "Project Details",
+    component: ClickListComponent,
+    data: [],
+    xAxisLabel: "",
+    yAxisLabel: "",
+    colorScheme: "",
+  },
+  {
+    title: "Coverage",
+    component: PieGridChartComponent,
+    data: {
+      results: [
+        {
+          name: "",
+          value: 0,
+        },
+      ] as DataItem[],
+      designatedTotal: 100.0,
+      label: "Lines of Code",
+      useCustomLabelValue: true,
+      customLabelValue: 0,
+    } as IPieGridData,
+    xAxisLabel: "",
+    yAxisLabel: "",
+    scaleFactor: 0.85,
+    colorScheme: "forest",
+  },
+  {
+    title: "Issues",
+    component: PieChartComponent,
+    data: {
+      results: [
+        {
+          name: "Blocker",
+          value: 0,
+        },
+        {
+          name: "Critical",
+          value: 0,
+        },
+        {
+          name: "Major",
+          value: 0,
+        },
+        {
+          name: "Others",
+          value: 0,
+        },
+      ] as DataItem[],
+      designatedTotal: 100.0,
+      label: "Violations",
+      advancedChart: true,
+      useCustomLabelValue: true,
+      customLabelValue: 0,
+    } as IPieGridData,
+    xAxisLabel: "",
+    yAxisLabel: "",
+    scaleFactor: 0.4,
+    colorScheme: "flame",
+  },
+  {
+    title: "Unit Test Metrics",
+    component: ClickListComponent,
+    data: [],
+    xAxisLabel: "",
+    yAxisLabel: "",
+    colorScheme: "",
   },
 ];
