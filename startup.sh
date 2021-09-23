2e38f59541df030b0c6635bf2d37f1d3aa8b7a38
Ryan Montgomery <ryan.montgomery@capitalone.com>
2021-09-23 11:49:17 -0400

    Fix typo

--- startup.sh
+++ startup.sh
@@ -9,7 +9,7 @@ sed -in 's/#LoadModule rewrite_module/LoadModule rewrite_module/' /usr/local/apa
 echo "SSLProxyEngine on" >> /usr/local/apache2/conf/httpd.conf
 echo "ProxyPass /api $API_URL/api retry=0" >> /usr/local/apache2/conf/httpd.conf
 echo "ProxyPass /apiaudit $API_URL/apiaudit retry=0" >> /usr/local/apache2/conf/httpd.conf
-echo "TraceEnable off" ?? /usr/local/apache2/conf/httpd.conf
+echo "TraceEnable off" >> /usr/local/apache2/conf/httpd.conf
 
 
 httpd-foreground
